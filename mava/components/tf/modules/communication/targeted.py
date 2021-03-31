128e2300228f0614d68c44f7ee681a62b9d26a45
arnupretorius <arnupretorius@gmail.com>
2021-03-31 13:18:11 +0200

    Skeleton code for modules.

---
+++ mava/components/tf/modules/communication/targeted.py
@@ -0,0 +1,33 @@
+# python3
+# Copyright 2021 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Broadcasted communication for multi-agent RL systems"""
+
+
+from mava.components.tf.architectures import BaseArchitecture
+from mava.components.tf.modules.communication import BaseCommunicationModule
+
+
+class TargetedCommunication(BaseCommunicationModule):
+    """Multi-agent broadcasted communication architecture."""
+
+    def __init__(
+        self,
+        architecture: BaseArchitecture,
+    ) -> None:
+        self._architecture = architecture
+
+    def some_communication_function(self) -> None:
+        """Perform some communication logic"""
--- mava/components/tf/modules/stabilising/__init__.py
+++ mava/components/tf/modules/stabilising/__init__.py
@@ -12,3 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+"""MARL system stabilising modules."""
+
+from mava.components.tf.modules.stabilising.base import BaseStabilisationModule
+from mava.components.tf.modules.stabilising.fingerprints import FingerPrintStabalisation
