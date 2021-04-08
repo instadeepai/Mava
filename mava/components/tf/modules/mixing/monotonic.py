6e1bc40b0002b4f21406a237143c1ce16b86a2a6
St John Grimbly <stjohngrimbly@protonmail.com>
2021-04-08 14:50:48 +0200

    Begin structuring monotonic mixing component.

--- mava/components/tf/modules/mixing/monotonic.py
+++ mava/components/tf/modules/mixing/monotonic.py
@@ -17,6 +17,11 @@
 
 """Mixing for multi-agent RL systems"""
 
+from typing import Dict
+
+import tensorflow as tf
+from tensorflow import Tensor
+
 from mava.components.tf.architectures import BaseArchitecture
 from mava.components.tf.modules.mixing import BaseMixingModule
 
@@ -27,8 +32,44 @@ class MonotonicMixing(BaseMixingModule):
     def __init__(
         self,
         architecture: BaseArchitecture,
+        mixer: str = "vdn",
     ) -> None:
+        """Initializes the mixer.
+        Args:
+            architecture: the BaseArchitecture used.
+            mixer: the type of monotonic mixing.
+        """
         self._architecture = architecture
+        self._mixer = mixer.lower()
 
-    def some_mixing_function(self) -> None:
-        """Perform some mixing logic"""
+    def forward(
+        self,
+        agent_qs: Dict[str, float],  # Check type
+        states: Dict[str, float],  # Check type
+        num_hypernet_layers: int = 1,
+    ) -> Tensor:
+
+        """Monotonic mixing logic."""
+        if self._mixer == "vdn":
+            # Not sure if this is the way to simply sum in tf.
+            # I'm looking for an equivalent to th.sum(...) in PyTorch.
+            return tf.math.accumulate_n(agent_qs)
+
+        elif self._mixer == "qmix":
+            # Set up hypernetwork configuration
+            if num_hypernet_layers == 1:
+                # Create 1-layer NN
+                pass
+            elif num_hypernet_layers == 2:
+                # Create 2-layer NN
+                pass
+            elif num_hypernet_layers > 2:
+                raise Exception("Sorry >2 hypernet layers is not implemented!")
+            else:
+                raise Exception("Error setting number of hypernet layers.")
+
+            # State dependent bias for hidden layer
+
+            # # V(s) instead of a bias for the last layers
+
+            # Forward pass
