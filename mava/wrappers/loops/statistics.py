f37ade0f187ff1a2098b902de024528c11c9f33a
arnupretorius <arnupretorius@gmail.com>
2021-04-14 10:49:59 +0200

    feat: base abstract class to capture per agent statistics from env loop

---
+++ mava/wrappers/loops/statistics.py
@@ -0,0 +1,76 @@
+# python3
+# Copyright 2021 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generic environment loop wrapper to track system statistics"""
+
+
+from mava.environment_loop import ParallelEnvironmentLoop
+
+
+class PerAgentStatisticsBase(ParallelEnvironmentLoop):
+    """A parallel MARL environment loop.
+    This takes `Environment` and `Executor` instances and coordinates their
+    interaction. Executors are updated if `should_update=True`. This can be used as:
+        loop = EnvironmentLoop(environment, executor)
+        loop.run(num_episodes)
+    A `Counter` instance can optionally be given in order to maintain counts
+    between different Mava components. If not given a local Counter will be
+    created to maintain counts between calls to the `run` method.
+    A `Logger` instance can also be passed in order to control the output of the
+    loop. If not given a platform-specific default logger will be used as defined
+    by utils.loggers.make_default_logger from acme. A string `label` can be passed
+    to easily change the label associated with the default logger; this is ignored
+    if a `Logger` instance is given.
+    """
+
+    def __init__(
+        self,
+        environment_loop: ParallelEnvironmentLoop,
+    ):
+        # Internalize agent and environment.
+        self._environment_loop = environment_loop
+
+    def _get_actions(self, timestep: dm_env.TimeStep) -> Any:
+        return self._environment_loop._get_actions(timestep)
+
+    def run_episode(self) -> loggers.LoggingData:
+        """Run one episode.
+        Each episode is a loop which interacts first with the environment to get a
+        dictionary of observations and then give those observations to the executor
+        in order to retrieve an action for each agent in the system.
+        Returns:
+            An instance of `loggers.LoggingData`.
+        """
+        raise NotImplementedError
+
+    def run(
+        self, num_episodes: Optional[int] = None, num_steps: Optional[int] = None
+    ) -> None:
+        """Perform the run loop.
+        Run the environment loop either for `num_episodes` episodes or for at
+        least `num_steps` steps (the last episode is always run until completion,
+        so the total number of steps may be slightly more than `num_steps`).
+        At least one of these two arguments has to be None.
+        Upon termination of an episode a new episode will be started. If the number
+        of episodes and the number of steps are not given then this will interact
+        with the environment infinitely.
+        Args:
+            num_episodes: number of episodes to run the loop for.
+            num_steps: minimal number of steps to run the loop for.
+        Raises:
+            ValueError: If both 'num_episodes' and 'num_steps' are not None.
+        """
+
+        self._environment_loop.run(num_episodes, num_steps)
