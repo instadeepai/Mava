f8b4fcca3949c83e2291397ad5f5d9504a8a23ce
arnupretorius <arnupretorius@gmail.com>
2021-04-14 15:02:01 +0200

    feat: step statistics computing in maddpg example

--- mava/wrappers/loops/statistics.py
+++ mava/wrappers/loops/statistics.py
@@ -17,9 +17,8 @@
 
 import operator
 import time
-from typing import Any, Dict, Optional
+from typing import Dict
 
-import dm_env
 import numpy as np
 import tree
 from acme import types
@@ -50,13 +49,11 @@ class EnvironmentLoopStatisticsBase(ParallelEnvironmentLoop):
         self,
         environment_loop: ParallelEnvironmentLoop,
     ) -> None:
-        # Internalize agent and environment.
-        self._environment_loop = environment_loop
-        self._environment = self._environment_loop._environment
-        self._executor = self._environment_loop._executor
-
-    def _get_actions(self, timestep: dm_env.TimeStep) -> Any:
-        return self._environment_loop._get_actions(timestep)
+        self._environment = environment_loop._environment
+        self._executor = environment_loop._executor
+        self._counter = environment_loop._counter
+        self._logger = environment_loop._logger
+        self._should_update = environment_loop._should_update
 
     def run_episode(self) -> loggers.LoggingData:
         """Run one episode.
@@ -67,26 +64,6 @@ class EnvironmentLoopStatisticsBase(ParallelEnvironmentLoop):
             An instance of `loggers.LoggingData`.
         """
         raise NotImplementedError
-
-    def run(
-        self, num_episodes: Optional[int] = None, num_steps: Optional[int] = None
-    ) -> None:
-        """Perform the run loop.
-        Run the environment loop either for `num_episodes` episodes or for at
-        least `num_steps` steps (the last episode is always run until completion,
-        so the total number of steps may be slightly more than `num_steps`).
-        At least one of these two arguments has to be None.
-        Upon termination of an episode a new episode will be started. If the number
-        of episodes and the number of steps are not given then this will interact
-        with the environment infinitely.
-        Args:
-            num_episodes: number of episodes to run the loop for.
-            num_steps: minimal number of steps to run the loop for.
-        Raises:
-            ValueError: If both 'num_episodes' and 'num_steps' are not None.
-        """
-
-        self._environment_loop.run(num_episodes, num_steps)
 
 
 class DetailedStatistics(EnvironmentLoopStatisticsBase):
@@ -114,12 +91,18 @@ class DetailedStatistics(EnvironmentLoopStatisticsBase):
             "std_steps_per_second": 0.0,
         }
 
-        agents_stats: Dict[str, Dict[str, RunningStatistics]] = {}
+        self._agents_stats: Dict[str, Dict[str, RunningStatistics]] = {
+            agent: {} for agent in self._environment.possible_agents
+        }
 
         # statistics dictionary
         for agent in self._environment.possible_agents:
-            agents_stats[agent]["return"] = RunningStatistics(f"{agent}_episode_return")
-            agents_stats[agent]["reward"] = RunningStatistics(f"{agent}_episode_reward")
+            self._agents_stats[agent]["return"] = RunningStatistics(
+                f"{agent}_episode_return"
+            )
+            self._agents_stats[agent]["reward"] = RunningStatistics(
+                f"{agent}_episode_reward"
+            )
             self._running_statistics.update(
                 {
                     f"{agent}_mean_return": 0.0,
@@ -132,6 +115,42 @@ class DetailedStatistics(EnvironmentLoopStatisticsBase):
                     f"{agent}_min_episode_reward": 0.0,
                     f"{agent}_var_episode_reward": 0.0,
                     f"{agent}_std_episode_reward": 0.0,
+                }
+            )
+
+    def _compute_step_statistics(self, rewards: Dict[str, float]) -> None:
+        for agent, reward in rewards.items():
+            self._agents_stats[agent]["reward"].push(reward)
+            mean_reward = self._agents_stats[agent]["reward"].mean()
+            max_reward = self._agents_stats[agent]["reward"].max()
+            min_reward = self._agents_stats[agent]["reward"].min()
+            var_reward = self._agents_stats[agent]["reward"].variance()
+            std_reward = self._agents_stats[agent]["reward"].standard_deviation()
+            self._running_statistics.update(
+                {
+                    f"{agent}_mean_episode_reward": mean_reward,
+                    f"{agent}_max_episode_reward": max_reward,
+                    f"{agent}_min_episode_reward": min_reward,
+                    f"{agent}_var_episode_reward": var_reward,
+                    f"{agent}_std_episode_reward": std_reward,
+                }
+            )
+
+    def _compute_episode_statistics(self, ep_return: Dict[str, float]) -> None:
+        for agent, reward in rewards.items():
+            self._agents_stats[agent]["reward"].push(reward)
+            mean_reward = self._agents_stats[agent]["return"].mean()
+            max_reward = self._agents_stats[agent]["return"].max()
+            min_reward = self._agents_stats[agent]["return"].min()
+            var_reward = self._agents_stats[agent]["return"].variance()
+            std_reward = self._agents_stats[agent]["return"].standard_deviation()
+            self._running_statistics.update(
+                {
+                    f"{agent}_mean_return": 0.0,
+                    f"{agent}_max_return": 0.0,
+                    f"{agent}_min_return": 0.0,
+                    f"{agent}_var_return": 0.0,
+                    f"{agent}_std_return": 0.0,
                 }
             )
 
@@ -192,6 +211,8 @@ class DetailedStatistics(EnvironmentLoopStatisticsBase):
                     for agent, spec in self._environment.reward_spec().items()
                 }
 
+            self._compute_step_statistics(rewards)
+
             # Equivalent to: episode_return += timestep.reward
             # We capture the return value because if timestep.reward is a JAX
             # DeviceArray, episode_return will not be mutated in-place. (In all other
@@ -206,11 +227,18 @@ class DetailedStatistics(EnvironmentLoopStatisticsBase):
 
         # Collect the results and combine with counts.
         steps_per_second = episode_steps / (time.time() - start_time)
-        result = {
-            "episode_length": episode_steps,
-            "episode_return": np.mean(episode_return),
-            "steps_per_second": steps_per_second,
-        }
-        result.update(counts)
+        # result = {
+        #     "episode_length": episode_steps,
+        #     "episode_return": np.mean(episode_return),
+        #     "steps_per_second": steps_per_second,
+        # }
+        self._running_statistics.update(
+            {
+                "episode_length": episode_steps,
+                "system_episode_return": np.mean(episode_return),
+                "steps_per_second": steps_per_second,
+            }
+        )
+        self._running_statistics.update(counts)
 
-        return result
+        return self._running_statistics
