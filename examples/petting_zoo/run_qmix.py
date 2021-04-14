1ca8ca1b186d73d207634fa795160a84213321c2
St John Grimbly <stjohngrimbly@protonmail.com>
2021-04-14 13:44:52 +0200

    Begin outlining Qmix example.

---
+++ examples/petting_zoo/run_qmix.py
@@ -0,0 +1,53 @@
+# python3
+# Copyright 2021 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example running Qmix on pettinzoo MPE environments."""
+
+from typing import Any, Dict, Mapping, Sequence, Union
+
+import dm_env
+from absl import flags
+
+from acme import types
+
+from mava import specs as mava_specs
+
+
+FLAGS = flags.FLAGS
+flags.DEFINE_integer("num_episodes", 100, "Number of training episodes to run for.")
+
+flags.DEFINE_integer(
+    "num_episodes_per_eval",
+    10,
+    "Number of training episodes to run between evaluation " "episodes.",
+)
+
+def make_environment(
+    env_class: str = "sisl", env_name: str = "simple_v2", **kwargs: int
+) -> dm_env.Environment:
+    """Creates a MPE environment."""
+    env_module = importlib.import_module(f"pettingzoo.{env_class}.{env_name}")
+    env = env_module.parallel_env(**kwargs)  # type: ignore
+    environment = PettingZooParallelEnvWrapper(env)
+    return environment
+
+def make_networks(
+    environment_spec: mava_specs.MAEnvironmentSpec,
+    networks_layer_sizes: Union[Dict[str, Sequence], Sequence] = (256, 256, 256),
+    
+) -> Mapping[str, types.TensorTransformation]:
+    """Creates networks used by the agents."""
+
+
