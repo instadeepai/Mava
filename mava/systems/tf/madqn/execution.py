0caf882703f5e7a0e010f14024548b687559273d
arnupretorius <arnupretorius@gmail.com>
2021-04-28 13:57:59 +0200

    feat: outline for MAQDN executor that can handle legal actions

--- mava/systems/tf/madqn/execution.py
+++ mava/systems/tf/madqn/execution.py
@@ -13,10 +13,116 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO (Claude): implement MADQN executor (if required)
-# Helper resources
-#   - single agent generic actors in acme:
-#           https://github.com/deepmind/acme/blob/master/acme/agents/tf/actors.py
-#   - single agent custom actor for Impala in acme:
-#           https://github.com/deepmind/acme/blob/master/acme/agents/tf/impala/acting.py
-#   - multi-agent generic executors in mava: mava/systems/tf/executors.py
+
+"""Executor implementations for MADQN."""
+from typing import Dict, Optional
+
+import dm_env
+import sonnet as snt
+import tensorflow as tf
+import tensorflow_probability as tfp
+from acme import types
+
+# Internal imports.
+from acme.tf import utils as tf2_utils
+from acme.tf import variable_utils as tf2_variable_utils
+
+from mava import adders, core
+from mava.types import OLT
+
+tfd = tfp.distributions
+
+
+class FeedForwardMADQNExecutor(core.Executor):
+    """A feed-forward executor.
+    An executor based on a feed-forward policy for each agent in the system
+    which takes non-batched observations and outputs non-batched actions.
+    It also allows adding experiences to replay and updating the weights
+    from the policy on the learner.
+    """
+
+    def __init__(
+        self,
+        q_networks: Dict[str, snt.Module],
+        action_selector: Dict[str, snt.Module],
+        shared_weights: bool = True,
+        adder: Optional[adders.ParallelAdder] = None,
+        variable_client: Optional[tf2_variable_utils.VariableClient] = None,
+    ):
+        """Initializes the executor.
+        Args:
+          policy_network: the policy to run for each agent in the system.
+          shared_weights: specify if weights are shared between agent networks.
+          adder: the adder object to which allows to add experiences to a
+            dataset/replay buffer.
+          variable_client: object which allows to copy weights from the trainer copy
+            of the policies to the executor copy (in case they are separate).
+        """
+
+        # Store these for later use.
+        self._adder = adder
+        self._variable_client = variable_client
+        self._q_networks = q_networks
+        self._action_selector = action_selector
+        self._shared_weights = shared_weights
+
+    @tf.function
+    def _policy(
+        self,
+        agent: str,
+        observation: types.NestedTensor,
+        legal_actions: types.NestedTensor,
+    ) -> types.NestedTensor:
+
+        # Add a dummy batch dimension and as a side effect convert numpy to TF.
+        batched_observation = tf2_utils.add_batch_dim(observation)
+
+        # index network either on agent type or on agent id
+        agent_key = agent.split("_")[0] if self._shared_weights else agent
+
+        # Compute the policy, conditioned on the observation.
+        q_values = self._q_networks[agent_key](batched_observation)
+
+        # select legal action
+        action = self._action_selector[agent_key](q_values, legal_actions)
+
+        return action
+
+    def observe_first(
+        self,
+        timestep: dm_env.TimeStep,
+        extras: Dict[str, types.NestedArray] = {},
+    ) -> None:
+        if self._adder:
+            self._adder.add_first(timestep, extras)
+
+    def observe(
+        self,
+        actions: Dict[str, types.NestedArray],
+        next_timestep: dm_env.TimeStep,
+        next_extras: Optional[Dict[str, types.NestedArray]] = {},
+    ) -> None:
+        if self._adder:
+            if next_extras:
+                self._adder.add(actions, next_timestep, next_extras)
+            else:
+                self._adder.add(actions, next_timestep)
+
+    def select_actions(
+        self, observations: Dict[str, OLT]
+    ) -> Dict[str, types.NestedArray]:
+        actions = {}
+        for agent, observation in observations.items():
+            # Pass the observation through the policy network.
+            action = self._policy(
+                agent, observation.observation, observation.legal_actions
+            )
+
+            actions[agent] = tf2_utils.to_numpy_squeeze(action)
+
+        # Return a numpy array with squeezed out batch dimension.
+        return actions
+
+    def update(self, wait: bool = False) -> None:
+        if self._variable_client:
+            self._variable_client.update(wait)
