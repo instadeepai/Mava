ac584dc5de3f1ddac299bfce0f4eb7e930ee59cf
Kale-ab <kaleabtessera@gmail.com>
2021-05-14 18:30:55 +0200

    feature: Updated and created PZ examples.

--- examples/petting_zoo/run_feedforward_mad4pg.py
+++ examples/petting_zoo/run_feedforward_mad4pg.py
@@ -13,24 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example running MADQN on the pettingzoo environment."""
+"""Example running MAD4PG on debug MPE environments."""
 
 import functools
 from datetime import datetime
-from typing import Any, Mapping
+from typing import Any, Dict, Mapping, Sequence, Union
 
 import launchpad as lp
+import numpy as np
 import sonnet as snt
-import tensorflow as tf
 from absl import app, flags
 from acme import types
-from acme.tf.networks import DQNAtariNetwork
+from acme.tf import networks
+from acme.tf import utils as tf2_utils
 from launchpad.nodes.python.local_multi_processing import PythonProcess
-from supersuit.aec_wrappers import black_death_v1
 
 from mava import specs as mava_specs
-from mava.components.tf.networks import epsilon_greedy_action_selector
-from mava.systems.tf import madqn
+from mava.systems.tf import mad4pg
 from mava.utils import lp_utils
 from mava.utils.environments import pettingzoo_utils
 from mava.utils.loggers import Logger
@@ -39,13 +38,13 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string(
     "env_class",
-    "atari",
+    "sisl",
     "Pettingzoo environment class, e.g. atari (str).",
 )
 
 flags.DEFINE_string(
     "env_name",
-    "maze_craze_v2",
+    "multiwalker_v6",
     "Pettingzoo environment name, e.g. pong (str).",
 )
 flags.DEFINE_string(
@@ -58,11 +57,19 @@ flags.DEFINE_string("base_dir", "~/mava/", "Base dir to store experiments.")
 
 def make_networks(
     environment_spec: mava_specs.MAEnvironmentSpec,
-    epsilon: tf.Variable = tf.Variable(0.05, trainable=False),
+    policy_networks_layer_sizes: Union[Dict[str, Sequence], Sequence] = (
+        256,
+        256,
+        256,
+    ),
+    critic_networks_layer_sizes: Union[Dict[str, Sequence], Sequence] = (512, 512, 256),
     shared_weights: bool = True,
+    sigma: float = 0.3,
+    vmin: float = -150.0,
+    vmax: float = 150.0,
+    num_atoms: int = 51,
 ) -> Mapping[str, types.TensorTransformation]:
     """Creates networks used by the agents."""
-
     specs = environment_spec.get_agent_specs()
 
     # Create agent_type specs
@@ -70,49 +77,80 @@ def make_networks(
         type_specs = {key.split("_")[0]: specs[key] for key in specs.keys()}
         specs = type_specs
 
-    def action_selector_fn(
-        q_values: types.NestedTensor, legal_actions: types.NestedTensor
-    ) -> types.NestedTensor:
-        return epsilon_greedy_action_selector(
-            action_values=q_values, legal_actions_mask=legal_actions
-        )
+    if isinstance(policy_networks_layer_sizes, Sequence):
+        policy_networks_layer_sizes = {
+            key: policy_networks_layer_sizes for key in specs.keys()
+        }
+    if isinstance(critic_networks_layer_sizes, Sequence):
+        critic_networks_layer_sizes = {
+            key: critic_networks_layer_sizes for key in specs.keys()
+        }
 
-    q_networks = {}
-    action_selectors = {}
+    observation_networks = {}
+    policy_networks = {}
+    critic_networks = {}
     for key in specs.keys():
 
         # Get total number of action dimensions from action spec.
-        num_dimensions = specs[key].actions.num_values
+        num_dimensions = np.prod(specs[key].actions.shape, dtype=int)
 
-        # Create the q-value network.
-        q_network = DQNAtariNetwork(num_dimensions)
+        # Create the shared observation network; here simply a state-less operation.
+        observation_network = tf2_utils.to_sonnet_module(tf2_utils.batch_concat)
 
-        # epsilon greedy action selector
-        action_selector = action_selector_fn
+        # Create the policy network.
+        policy_network = snt.Sequential(
+            [
+                networks.LayerNormMLP(
+                    policy_networks_layer_sizes[key], activate_final=True
+                ),
+                networks.NearZeroInitializedLinear(num_dimensions),
+                networks.TanhToSpec(specs[key].actions),
+                networks.ClippedGaussian(sigma),
+                networks.ClipToSpec(specs[key].actions),
+            ]
+        )
 
-        q_networks[key] = q_network
-        action_selectors[key] = action_selector
+        # Create the critic network.
+        critic_network = snt.Sequential(
+            [
+                # The multiplexer concatenates the observations/actions.
+                networks.CriticMultiplexer(),
+                networks.LayerNormMLP(
+                    critic_networks_layer_sizes[key], activate_final=False
+                ),
+                networks.DiscreteValuedHead(vmin, vmax, num_atoms),
+            ]
+        )
+        observation_networks[key] = observation_network
+        policy_networks[key] = policy_network
+        critic_networks[key] = critic_network
 
     return {
-        "q_networks": q_networks,
-        "action_selectors": action_selectors,
+        "policies": policy_networks,
+        "critics": critic_networks,
+        "observations": observation_networks,
     }
 
 
 def main(_: Any) -> None:
 
+    # TODO(Arnu): make logging optional, currently log_info
+    # is required for all systems
     # set loggers info
     log_info = (FLAGS.base_dir, f"{FLAGS.mava_id}/logs")
 
+    # environment
     environment_factory = functools.partial(
         pettingzoo_utils.make_environment,
-        env_class=FLAGS.env_class,
         env_name=FLAGS.env_name,
-        env_preprocess_wrappers=[(black_death_v1, None)],
+        env_class=FLAGS.env_class,
+        remove_on_fall=False,
     )
 
+    # networks
     network_factory = lp_utils.partial_kwargs(make_networks)
 
+    # distributed program
     # Checkpointer appends "Checkpoints" to checkpoint_dir
     checkpoint_dir = f"{FLAGS.base_dir}/{FLAGS.mava_id}"
 
@@ -145,19 +183,20 @@ def main(_: Any) -> None:
         time_delta=log_every,
     )
 
-    program = madqn.MADQN(
+    program = mad4pg.MAD4PG(
         environment_factory=environment_factory,
         network_factory=network_factory,
         num_executors=2,
         log_info=log_info,
-        policy_optimizer=snt.optimizers.Adam(learning_rate=1e-3),
+        policy_optimizer=snt.optimizers.Adam(learning_rate=1e-4),
+        critic_optimizer=snt.optimizers.Adam(learning_rate=1e-4),
         checkpoint_subpath=checkpoint_dir,
         trainer_logger=trainer_logger,
         exec_logger=exec_logger,
         eval_logger=eval_logger,
     ).build()
 
-    # Launch gpu config - let trainer use gpu.
+    # launch
     gpu_id = -1
     env_vars = {"CUDA_VISIBLE_DEVICES": str(gpu_id)}
     local_resources = {
@@ -165,7 +204,6 @@ def main(_: Any) -> None:
         "evaluator": PythonProcess(env=env_vars),
         "executor": PythonProcess(env=env_vars),
     }
-
     lp.launch(
         program,
         lp.LaunchType.LOCAL_MULTI_PROCESSING,
--- examples/petting_zoo/run_feedforward_mappo.py
+++ examples/petting_zoo/run_feedforward_mappo.py
@@ -13,41 +13,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example running MADQN on the pettingzoo environment."""
+"""Example running MAPPO on pettingzoo environment."""
 
 import functools
 from datetime import datetime
-from typing import Any, Mapping
+from typing import Any, Dict, Sequence, Union
 
+import acme.tf.networks as networks
+import dm_env
 import launchpad as lp
+import numpy as np
 import sonnet as snt
 import tensorflow as tf
+import tensorflow_probability as tfp
 from absl import app, flags
-from acme import types
-from acme.tf.networks import DQNAtariNetwork
+from acme.tf import utils as tf2_utils
 from launchpad.nodes.python.local_multi_processing import PythonProcess
-from supersuit.aec_wrappers import black_death_v1
+from supersuit import black_death_v1
 
-from mava import specs as mava_specs
-from mava.components.tf.networks import epsilon_greedy_action_selector
-from mava.systems.tf import madqn
+import mava.specs as mava_specs
+from mava.systems.tf import mappo
 from mava.utils import lp_utils
 from mava.utils.environments import pettingzoo_utils
 from mava.utils.loggers import Logger
 
 FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    "env_name",
+    "multiwalker_v6",
+    "Debugging environment name (str).",
+)
 
 flags.DEFINE_string(
     "env_class",
-    "atari",
+    "sisl",
     "Pettingzoo environment class, e.g. atari (str).",
 )
 
-flags.DEFINE_string(
-    "env_name",
-    "maze_craze_v2",
-    "Pettingzoo environment name, e.g. pong (str).",
-)
 flags.DEFINE_string(
     "mava_id",
     str(datetime.now()),
@@ -58,44 +60,89 @@ flags.DEFINE_string("base_dir", "~/mava/", "Base dir to store experiments.")
 
 def make_networks(
     environment_spec: mava_specs.MAEnvironmentSpec,
-    epsilon: tf.Variable = tf.Variable(0.05, trainable=False),
+    policy_networks_layer_sizes: Union[Dict[str, Sequence], Sequence] = (
+        256,
+        256,
+        256,
+    ),
+    critic_networks_layer_sizes: Union[Dict[str, Sequence], Sequence] = (512, 512, 256),
     shared_weights: bool = True,
-) -> Mapping[str, types.TensorTransformation]:
+) -> Dict[str, snt.Module]:
+
     """Creates networks used by the agents."""
 
+    # Create agent_type specs.
     specs = environment_spec.get_agent_specs()
-
-    # Create agent_type specs
     if shared_weights:
         type_specs = {key.split("_")[0]: specs[key] for key in specs.keys()}
         specs = type_specs
 
-    def action_selector_fn(
-        q_values: types.NestedTensor, legal_actions: types.NestedTensor
-    ) -> types.NestedTensor:
-        return epsilon_greedy_action_selector(
-            action_values=q_values, legal_actions_mask=legal_actions
-        )
+    if isinstance(policy_networks_layer_sizes, Sequence):
+        policy_networks_layer_sizes = {
+            key: policy_networks_layer_sizes for key in specs.keys()
+        }
+    if isinstance(critic_networks_layer_sizes, Sequence):
+        critic_networks_layer_sizes = {
+            key: critic_networks_layer_sizes for key in specs.keys()
+        }
 
-    q_networks = {}
-    action_selectors = {}
+    observation_networks = {}
+    policy_networks = {}
+    critic_networks = {}
+    # logging.info("*" * 100)
+    # logging.info(specs)
     for key in specs.keys():
 
-        # Get total number of action dimensions from action spec.
-        num_dimensions = specs[key].actions.num_values
+        # Create the shared observation network; here simply a state-less operation.
+        observation_network = tf2_utils.to_sonnet_module(tf.identity)
 
-        # Create the q-value network.
-        q_network = DQNAtariNetwork(num_dimensions)
+        # Note: The discrete case must be placed first as it inherits from BoundedArray.
+        if isinstance(specs[key].actions, dm_env.specs.DiscreteArray):  # discrete
+            num_actions = specs[key].actions.num_values
+            # logging.info(f"action: {num_actions} discrete")
+            policy_network = snt.Sequential(
+                [
+                    networks.LayerNormMLP(
+                        tuple(policy_networks_layer_sizes[key]) + (num_actions,),
+                        activate_final=False,
+                    ),
+                    tf.keras.layers.Lambda(
+                        lambda logits: tfp.distributions.Categorical(logits=logits)
+                    ),
+                ]
+            )
+        elif isinstance(specs[key].actions, dm_env.specs.BoundedArray):  # continuous
+            num_actions = np.prod(specs[key].actions.shape, dtype=int)
+            # logging.info(f"action: {num_actions} cont")
+            policy_network = snt.Sequential(
+                [
+                    networks.LayerNormMLP(
+                        policy_networks_layer_sizes[key], activate_final=True
+                    ),
+                    networks.MultivariateNormalDiagHead(num_dimensions=num_actions),
+                    networks.TanhToSpec(specs[key].actions),
+                ]
+            )
+        else:
+            raise ValueError(f"Unknown action_spec type, got {specs[key].actions}.")
 
-        # epsilon greedy action selector
-        action_selector = action_selector_fn
+        critic_network = snt.Sequential(
+            [
+                networks.LayerNormMLP(
+                    critic_networks_layer_sizes[key], activate_final=True
+                ),
+                networks.NearZeroInitializedLinear(1),
+            ]
+        )
 
-        q_networks[key] = q_network
-        action_selectors[key] = action_selector
-
+        observation_networks[key] = observation_network
+        policy_networks[key] = policy_network
+        critic_networks[key] = critic_network
+    # print("*" * 100)
     return {
-        "q_networks": q_networks,
-        "action_selectors": action_selectors,
+        "policies": policy_networks,
+        "critics": critic_networks,
+        "observations": observation_networks,
     }
 
 
@@ -104,6 +151,7 @@ def main(_: Any) -> None:
     # set loggers info
     log_info = (FLAGS.base_dir, f"{FLAGS.mava_id}/logs")
 
+    # environment
     environment_factory = functools.partial(
         pettingzoo_utils.make_environment,
         env_class=FLAGS.env_class,
@@ -111,8 +159,10 @@ def main(_: Any) -> None:
         env_preprocess_wrappers=[(black_death_v1, None)],
     )
 
+    # networks
     network_factory = lp_utils.partial_kwargs(make_networks)
 
+    # distributed program
     # Checkpointer appends "Checkpoints" to checkpoint_dir
     checkpoint_dir = f"{FLAGS.base_dir}/{FLAGS.mava_id}"
 
@@ -145,19 +195,20 @@ def main(_: Any) -> None:
         time_delta=log_every,
     )
 
-    program = madqn.MADQN(
+    program = mappo.MAPPO(
         environment_factory=environment_factory,
         network_factory=network_factory,
         num_executors=2,
         log_info=log_info,
-        policy_optimizer=snt.optimizers.Adam(learning_rate=1e-3),
+        policy_optimizer=snt.optimizers.Adam(learning_rate=5e-4),
+        critic_optimizer=snt.optimizers.Adam(learning_rate=1e-5),
         checkpoint_subpath=checkpoint_dir,
         trainer_logger=trainer_logger,
         exec_logger=exec_logger,
         eval_logger=eval_logger,
     ).build()
 
-    # Launch gpu config - let trainer use gpu.
+    # launch
     gpu_id = -1
     env_vars = {"CUDA_VISIBLE_DEVICES": str(gpu_id)}
     local_resources = {
@@ -165,7 +216,6 @@ def main(_: Any) -> None:
         "evaluator": PythonProcess(env=env_vars),
         "executor": PythonProcess(env=env_vars),
     }
-
     lp.launch(
         program,
         lp.LaunchType.LOCAL_MULTI_PROCESSING,
