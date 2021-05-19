f00bff0946f70d96bd15f62f8a3015d25ca74bad
arnupretorius <arnupretorius@gmail.com>
2021-05-19 16:29:11 +0200

    resolve merge conflicts

--- mava/systems/tf/mad4pg/system.py
+++ mava/systems/tf/mad4pg/system.py
@@ -111,12 +111,8 @@ class MAD4PG(MADDPG):
             period: Overlapping period of sequences used in recurrent
             training (if using recurrence).
             sigma: standard deviation of zero-mean, Gaussian exploration noise.
-<<<<<<< HEAD
-            clipping: whether to clip gradients by global norm.
-=======
             max_gradient_norm:
             logger: logger object to be used by trainers.
->>>>>>> faebbc14bf962468577340df02ce44c1296490bb
             counter: counter object used to keep track of steps.
             checkpoint: boolean indicating whether to checkpoint the trainers.
             checkpoint_subpath: directory for checkpoints.
@@ -124,247 +120,10 @@ class MAD4PG(MADDPG):
             train_loop_fn: loop for training.
             eval_loop_fn: loop for evaluation.
         """
-<<<<<<< HEAD
-
-        if not environment_spec:
-            environment_spec = mava_specs.MAEnvironmentSpec(
-                environment_factory(evaluation=False)  # type: ignore
-            )
-
-        # set default logger if no logger provided
-        if not logger_factory:
-            logger_factory = functools.partial(
-                logger_utils.make_logger,
-                directory="~/mava",
-                to_terminal=True,
-                time_delta=10,
-            )
-
-        self._architecture = architecture
-        self._environment_factory = environment_factory
-        self._network_factory = network_factory
-        self._logger_factory = logger_factory
-        self._environment_spec = environment_spec
-        self._shared_weights = shared_weights
-        self._num_exectors = num_executors
-        self._num_caches = num_caches
-        self._max_executor_steps = max_executor_steps
-        self._checkpoint_subpath = checkpoint_subpath
-        self._checkpoint = checkpoint
-        self._train_loop_fn = train_loop_fn
-        self._train_loop_fn_kwargs = train_loop_fn_kwargs
-        self._eval_loop_fn = eval_loop_fn
-        self._eval_loop_fn_kwargs = eval_loop_fn_kwargs
-
-        if executor_fn == executors.RecurrentExecutor:
-            extra_specs = self._get_extra_specs()
-        else:
-            extra_specs = {}
-
-        self._builder = builder.MAD4PGBuilder(
-            builder.MAD4PGConfig(
-                environment_spec=environment_spec,
-                shared_weights=shared_weights,
-                discount=discount,
-                batch_size=batch_size,
-                prefetch_size=prefetch_size,
-                target_update_period=target_update_period,
-                executor_variable_update_period=executor_variable_update_period,
-                min_replay_size=min_replay_size,
-                max_replay_size=max_replay_size,
-                samples_per_insert=samples_per_insert,
-                n_step=n_step,
-                sequence_length=sequence_length,
-                period=period,
-                sigma=sigma,
-                max_gradient_norm=max_gradient_norm,
-                checkpoint=checkpoint,
-                policy_optimizer=policy_optimizer,
-                critic_optimizer=critic_optimizer,
-                checkpoint_subpath=checkpoint_subpath,
-            ),
-            trainer_fn=trainer_fn,
-            executor_fn=executor_fn,
-            extra_specs=extra_specs,
-        )
-
-    def _get_extra_specs(self) -> Any:
-        agents = self._environment_spec.get_agent_ids()
-        core_state_specs = {}
-        networks = self._network_factory(  # type: ignore
-            environment_spec=self._environment_spec
-        )
-        for agent in agents:
-            agent_type = agent.split("_")[0]
-            core_state_specs[agent] = (
-                tf2_utils.squeeze_batch_dim(
-                    networks["policies"][agent_type].initial_state(1)
-                ),
-            )
-        extras = {"core_states": core_state_specs}
-        return extras
-
-    def replay(self) -> Any:
-        """The replay storage."""
-        return self._builder.make_replay_tables(self._environment_spec)
-
-    def counter(self) -> Any:
-        return tf2_savers.CheckpointingRunner(
-            counting.Counter(),
-            time_delta_minutes=15,
-            directory=self._checkpoint_subpath,
-            subdirectory="counter",
-        )
-
-    def coordinator(self, counter: counting.Counter) -> Any:
-        return lp_utils.StepsLimiter(counter, self._max_executor_steps)  # type: ignore
-
-    def trainer(
-        self,
-        replay: reverb.Client,
-        counter: counting.Counter,
-    ) -> mava.core.Trainer:
-        """The Trainer part of the system."""
-
-        # Create the networks to optimize (online)
-        networks = self._network_factory(  # type: ignore
-            environment_spec=self._environment_spec
-        )
-
-        # Create system architecture with target networks.
-        system_networks = self._architecture(
-            environment_spec=self._environment_spec,
-            observation_networks=networks["observations"],
-            policy_networks=networks["policies"],
-            critic_networks=networks["critics"],
-            shared_weights=self._shared_weights,
-        ).create_system()
-
-        # create logger
-        trainer_logger = self._logger_factory("trainer")
-
-        dataset = self._builder.make_dataset_iterator(replay)
-        counter = counting.Counter(counter, "trainer")
-
-        return self._builder.make_trainer(
-            networks=system_networks,
-            dataset=dataset,
-            counter=counter,
-            logger=trainer_logger,
-        )
-
-    def executor(
-        self,
-        executor_id: str,
-        replay: reverb.Client,
-        variable_source: acme.VariableSource,
-        counter: counting.Counter,
-    ) -> mava.ParallelEnvironmentLoop:
-        """The executor process."""
-
-        # Create the behavior policy.
-        networks = self._network_factory(  # type: ignore
-            environment_spec=self._environment_spec
-        )
-
-        # Create system architecture with target networks.
-        system = self._architecture(
-            environment_spec=self._environment_spec,
-            observation_networks=networks["observations"],
-            policy_networks=networks["policies"],
-            critic_networks=networks["critics"],
-            shared_weights=self._shared_weights,
-        )
-
-        # create variables
-        _ = system.create_system()
-
-        # behaviour policy networks (obs net + policy head)
-        behaviour_policy_networks = system.create_behaviour_policy()
-
-        # Create the executor.
-        executor = self._builder.make_executor(
-            policy_networks=behaviour_policy_networks,
-            adder=self._builder.make_adder(replay),
-            variable_source=variable_source,
-        )
-
-        # TODO (Arnu): figure out why factory function are giving type errors
-        # Create the environment.
-        environment = self._environment_factory(evaluation=False)  # type: ignore
-
-        # Create logger and counter; actors will not spam bigtable.
-        counter = counting.Counter(counter, "executor")
-
-        # Create executor logger
-        exec_logger = self._logger_factory(f"executor_{executor_id}")
-
-        # Create the loop to connect environment and executor.
-        train_loop = self._train_loop_fn(
-            environment,
-            executor,
-            counter=counter,
-            logger=exec_logger,
-            **self._train_loop_fn_kwargs,
-        )
-
-        train_loop = DetailedPerAgentStatistics(train_loop)
-
-        return train_loop
-
-    def evaluator(
-        self,
-        variable_source: acme.VariableSource,
-        counter: counting.Counter,
-        logger: loggers.Logger = None,
-    ) -> Any:
-        """The evaluation process."""
-
-        # Create the behavior policy.
-        networks = self._network_factory(  # type: ignore
-            environment_spec=self._environment_spec
-        )
-
-        # Create system architecture with target networks.
-        system = self._architecture(
-            environment_spec=self._environment_spec,
-            observation_networks=networks["observations"],
-            policy_networks=networks["policies"],
-            critic_networks=networks["critics"],
-            shared_weights=self._shared_weights,
-        )
-
-        # create variables
-        _ = system.create_system()
-
-        # behaviour policy networks (obs net + policy head)
-        behaviour_policy_networks = system.create_behaviour_policy()
-
-        # Create the agent.
-        executor = self._builder.make_executor(
-            policy_networks=behaviour_policy_networks,
-            variable_source=variable_source,
-        )
-
-        # Make the environment.
-        environment = self._environment_factory(evaluation=True)  # type: ignore
-
-        # Create logger and counter.
-        counter = counting.Counter(counter, "evaluator")
-        eval_logger = self._logger_factory("evaluator")
-
-        # Create the run loop and return it.
-        # Create the loop to connect environment and executor.
-        eval_loop = self._eval_loop_fn(
-            environment,
-            executor,
-            counter=counter,
-            logger=eval_logger,
-            **self._eval_loop_fn_kwargs,
-=======
         super().__init__(
             environment_factory=environment_factory,
             network_factory=network_factory,
+            logger_factory=logger_factory,
             architecture=architecture,
             trainer_fn=trainer_fn,
             executor_fn=executor_fn,
@@ -390,12 +149,8 @@ class MAD4PG(MADDPG):
             max_executor_steps=max_executor_steps,
             checkpoint=checkpoint,
             checkpoint_subpath=checkpoint_subpath,
-            trainer_logger=trainer_logger,
-            exec_logger=exec_logger,
-            eval_logger=eval_logger,
             train_loop_fn=train_loop_fn,
             eval_loop_fn=eval_loop_fn,
             train_loop_fn_kwargs=train_loop_fn_kwargs,
             eval_loop_fn_kwargs=eval_loop_fn_kwargs,
->>>>>>> faebbc14bf962468577340df02ce44c1296490bb
         )
