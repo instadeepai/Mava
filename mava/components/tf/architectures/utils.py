a04ae14a6886c8bff2ba198ee1808f329298084f
arnupretorius <arnupretorius@gmail.com>
2021-05-20 13:24:32 +0200

    update types in networked utils

--- mava/components/tf/architectures/utils.py
+++ mava/components/tf/architectures/utils.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Dict, List
+
 
 def fully_connected_network_spec(
     agents_by_type: Dict[str, List[str]]
