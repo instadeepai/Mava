17cf862020a69b77fb0540892676b820f932648e
Claude Formanek <formanekjc@gmail.com>
2021-05-20 14:11:31 +0200

    fix: logger factory.

--- examples/debugging_envs/run_feedforward_madqn_with_fingerprints.py
+++ examples/debugging_envs/run_feedforward_madqn_with_fingerprints.py
@@ -35,7 +35,7 @@ from mava.components.tf.networks import (
 from mava.systems.tf import madqn
 from mava.utils import lp_utils
 from mava.utils.environments import debugging_utils
-from mava.utils.loggers import Logger
+from mava.utils.loggers import logger_utils
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string(
@@ -133,28 +133,10 @@ def main(_: Any) -> None:
     # Checkpointer appends "Checkpoints" to checkpoint_dir
     checkpoint_dir = f"{FLAGS.base_dir}/{FLAGS.mava_id}"
 
+    # loggers
     log_every = 10
-    trainer_logger = Logger(
-        label="system_trainer",
-        directory=FLAGS.base_dir,
-        to_terminal=True,
-        to_tensorboard=True,
-        time_stamp=FLAGS.mava_id,
-        time_delta=log_every,
-    )
-
-    exec_logger = Logger(
-        # _{executor_id} gets appended to label in system.
-        label="train_loop_executor",
-        directory=FLAGS.base_dir,
-        to_terminal=True,
-        to_tensorboard=True,
-        time_stamp=FLAGS.mava_id,
-        time_delta=log_every,
-    )
-
-    eval_logger = Logger(
-        label="eval_loop",
+    logger_factory = functools.partial(
+        logger_utils.make_logger,
         directory=FLAGS.base_dir,
         to_terminal=True,
         to_tensorboard=True,
@@ -173,9 +155,7 @@ def main(_: Any) -> None:
         epsilon_decay=5e-4,
         optimizer=snt.optimizers.Adam(learning_rate=1e-4),
         checkpoint_subpath=checkpoint_dir,
-        trainer_logger=trainer_logger,
-        exec_logger=exec_logger,
-        eval_logger=eval_logger,
+        logger_factory=logger_factory,
     ).build()
 
     # launch
