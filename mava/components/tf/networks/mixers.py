6749a61efd8b2a2d5c8a3d7e1579e28ccb933b9b
arnupretorius <arnupretorius@gmail.com>
2021-05-24 19:23:01 +0200

    debug/small changes

--- mava/components/tf/networks/mixers.py
+++ mava/components/tf/networks/mixers.py
@@ -37,7 +37,7 @@ class QMixer(snt.Module):
         )
 
         self.linear_layer = snt.Sequential(
-            [snt.Linear(self.n_agents)]#, tf.keras.layers.ReLU()]
+            [snt.Linear(self.n_agents)]  # , tf.keras.layers.ReLU()]
         )
 
     def __call__(self, agent_qs: Any, states: Any) -> tf.Tensor:
@@ -48,37 +48,47 @@ class QMixer(snt.Module):
         """
         bs = agent_qs.shape[0]
         states = tf.reshape(states, (-1, self.state_dim))
-        agent_qs = tf.reshape(agent_qs, (-1, 1, self.n_agents))
+        print("states reshape: ", states.shape)
+        agent_qs = tf.reshape(agent_qs, (-1, self.n_agents))
+        print("agent_qs: ", agent_qs.shape)
         # First layer
         w1 = tf.math.abs(self.hyper_w_1(states))
+        print("w1: ", w1.shape)
         b1 = self.hyper_b_1(states)
+        print("b1: ", b1.shape)
         w1 = tf.reshape(w1, (-1, self.n_agents, self.embed_dim))
-        b1 = tf.reshape(b1, (-1, 1, self.embed_dim))
+        print("w1: ", w1.shape)
+        b1 = tf.reshape(b1, (-1, self.embed_dim))
+        print("b1: ", b1.shape)
         hidden = tf.keras.activations.elu(tf.linalg.matmul(agent_qs, w1) + b1)
+        print("hidden: ", hidden.shape)
+        hidden = tf.reshape(hidden, (-1, self.embed_dim))
         # Second layer
         w_final = tf.math.abs(self.hyper_w_final(states))
-        w_final = tf.reshape(w_final, (-1, self.embed_dim, 1))
+        print("w_final: ", w_final.shape)
+        w_final = tf.reshape(w_final, (-1, self.embed_dim))
+        print("w_final: ", w_final.shape)
         # State-dependent bias
-        v = tf.reshape(self.V(states), (-1, 1, 1))
+        v = tf.reshape(self.V(states), (-1, 1))
+        print("v: ", v.shape)
         # Compute final output
 
         # print("hidden: ", hidden.shape)
         # print("w_final: ", w_final.shape)
         # print("v: ", v.shape)
 
-        y = tf.linalg.matmul(hidden, w_final) + v
+        y = tf.linalg.matmul(hidden, w_final, transpose_b=True) + v
         # print("tf.linalg.matmul(hidden, w_final): ", tf.linalg.matmul(hidden, w_final).shape)
-        # print("y: ", y.shape)
+        print("y: ", y.shape)
 
         # Reshape and return
-        q_tot = tf.reshape(y, (bs, -1, 1))
+        q_tot = tf.reshape(y, (bs, -1))
         # print("q_tot: ", q_tot.shape)
         return q_tot
 
     # hidden: (256, 1, 32)
     # w_final: (256, 32, 1)
     # tf.linalg.matmul(hidden, w_final): (256, 1, 1)
-
 
     # v: (256, 1, 1)
 
@@ -116,4 +126,3 @@ class QMixer(snt.Module):
     #     y = tf.linalg.matmul(hidden, w_final) # + v
     #     q_tot = tf.reshape(y, (bs, -1, 1))
     #     return q_tot
-
