7965de275a7b7fa5873dac40fb9a9cf72d33f950
St John Grimbly <stjohngrimbly@protonmail.com>
2021-05-25 16:34:55 +0200

    fix: VDN and update files.

--- mava/systems/tf/qmix/training.py
+++ mava/systems/tf/qmix/training.py
@@ -15,7 +15,7 @@
 
 """Qmix trainer implementation."""
 
-from typing import Any, Dict, List, Sequence
+from typing import Any, Dict, List, Sequence, Optional
 
 import numpy as np
 import sonnet as snt
@@ -24,6 +24,7 @@ from acme.tf import utils as tf2_utils
 from acme.utils import counting, loggers
 from trfl.indexing_ops import batched_index
 
+from mava.components.tf.modules.communication import BaseCommunicationModule
 from mava.components.tf.modules.exploration.exploration_scheduling import (
     LinearExplorationScheduler,
 )
@@ -53,8 +54,10 @@ class QMIXTrainer(MADQNTrainer):
         discount: float,
         shared_weights: bool,
         exploration_scheduler: LinearExplorationScheduler,
+        communication_module: Optional[BaseCommunicationModule] = None,
         clipping: bool = True,
         counter: counting.Counter = None,
+        fingerprint: bool = False,
         logger: loggers.Logger = None,
         checkpoint: bool = True,
         checkpoint_subpath: str = "~/mava/",
@@ -74,8 +77,10 @@ class QMIXTrainer(MADQNTrainer):
             discount=discount,
             shared_weights=shared_weights,
             exploration_scheduler=exploration_scheduler,
+            communication_module=communication_module,
             clipping=clipping,
             counter=counter,
+            fingerprint=fingerprint,
             logger=logger,
             checkpoint=checkpoint,
             checkpoint_subpath=checkpoint_subpath,
@@ -104,34 +109,40 @@ class QMIXTrainer(MADQNTrainer):
         # )
 
     def _update_target_networks(self) -> None:
-        online_variables = []
-        target_variables = []
+        # Update target networks (incl. mixing networks).
         for key in self.unique_net_keys:
-            # Update target networks (incl. mixing networks).
-            online_variables += self._q_networks[key].variables
-            target_variables += self._target_q_networks[key].variables
+            online_variables = [*self._q_networks[key].variables,]
+            target_variables = [*self._target_q_networks[key].variables,]
 
-        online_variables += self._mixing_network.variables
-        target_variables += self._target_mixing_network.variables
+            # NOTE These shouldn't really be in the agent for loop.
+            online_variables += self._mixing_network.variables
+            target_variables += self._target_mixing_network.variables
 
-        # Make online -> target network update ops.
-        if tf.math.mod(self._num_steps, self._target_update_period) == 0:
-            for src, dest in zip(online_variables, target_variables):
-                dest.assign(src)
+            # Make online -> target network update ops.
+            if tf.math.mod(self._num_steps, self._target_update_period) == 0:
+                for src, dest in zip(online_variables, target_variables):
+                    dest.assign(src)
 
         self._num_steps.assign_add(1)
 
     @tf.function
-    def _step(self) -> Dict[str, Dict[str, Any]]:
+    def _step(
+        self,
+    ) -> Dict[str, Dict[str, Any]]:
+
         # Update the target networks
         self._update_target_networks()
 
+        # Get data from replay (dropping extras if any). Note there is no
+        # extra data here because we do not insert any into Reverb.
         inputs = next(self._iterator)
 
         self._forward(inputs)
+
         self._backward()
 
-        return {"system": {"q_value_loss": self.loss}}  # Return total system loss
+        # Log losses per agent
+        return {agent: {"q_value_loss": self.loss} for agent in self._agents}
 
     def _forward(self, inputs: Any) -> None:
         # Unpack input data as follows:
@@ -145,68 +156,56 @@ class QMIXTrainer(MADQNTrainer):
         # o_t = dictionary of next observations or next observation sequences
         # e_t = [Optional] = extra data that the agents persist in replay.
         o_tm1, a_tm1, e_tm1, r_t, d_t, o_t, e_t = inputs.data
-
-        # Global state (for hypernetwork)
-        # TODO Switch between one-hot for discrete state space and no one-hot
-        # for continuous state spaces. `depth` param should be taken from
-        # state specs - or somewhere else in a cleaner way?
-
-        # One-hot if discrete states
-        # s_tm1 = tf.one_hot(e_tm1["s_t"], depth=3)
-        # s_t = tf.one_hot(e_t["s_t"], depth=3)
-
-        # Don't one-hot for continuous states
         s_tm1 = e_tm1["s_t"]
         s_t = e_t["s_t"]
 
         # Do forward passes through the networks and calculate the losses
         with tf.GradientTape(persistent=True) as tape:
-            q_tm1 = []  # Q vals
-            q_t = []  # Target Q vals
+            q_acts = []  # Q vals
+            q_targets = []  # Target Q vals
             for agent in self._agents:
                 agent_key = self.agent_net_keys[agent]
 
                 o_tm1_feed, o_t_feed, a_tm1_feed = self._get_feed(
                     o_tm1, o_t, a_tm1, agent
                 )
+                q_tm1 = self._q_networks[agent_key](o_tm1_feed)
+                q_t_value = self._target_q_networks[agent_key](o_t_feed)
+                q_t_selector = self._q_networks[agent_key](o_t_feed)
+                best_action = tf.argmax(q_t_selector, axis=1, output_type=tf.int32)
 
-                q_tm1_agent = self._q_networks[agent_key](o_tm1_feed)  # [B, n_actions]
-                q_act = batched_index(q_tm1_agent, a_tm1_feed, keepdims=True)  # [B, 1]
+                # TODO Make use of q_t_selector for fingerprinting. Speak to Claude.
+                q_act = batched_index(q_tm1, a_tm1_feed, keepdims=True)  # [B, 1]
+                q_target = batched_index(
+                    q_t_value, best_action, keepdims=True
+                )  # [B, 1]
 
-                q_t_agent = self._target_q_networks[agent_key](
-                    o_t_feed
-                )  # [B, n_actions]
-                q_target_max = tf.reduce_max(q_t_agent, axis=1, keepdims=True)  # [B, 1]
+                q_acts.append(q_act)
+                q_targets.append(q_target)
 
-                q_tm1.append(q_act)
-                q_t.append(q_target_max)
+            rewards = tf.concat(
+                [tf.reshape(val, (-1, 1)) for val in list(r_t.values())], axis=1
+            )
+            rewards = tf.reduce_mean(rewards, axis=1)  # [B]
 
-            num_agents = len(self._agents)
+            pcont = tf.concat(
+                [tf.reshape(val, (-1, 1)) for val in list(d_t.values())], axis=1
+            )
+            pcont = tf.reduce_mean(pcont, axis=1)
+            discount = tf.cast(self._discount, list(d_t.values())[0].dtype)
+            pcont = discount * pcont  # [B]
 
-            rewards = [tf.reshape(val, (-1, 1)) for val in list(r_t.values())]
-            rewards = tf.reshape(
-                tf.concat(rewards, axis=1), (-1, 1, num_agents)
-            )  # [B, 1, num_agents]
+            q_acts = tf.concat(q_acts, axis=1)  # [B, num_agents]
+            q_targets = tf.concat(q_targets, axis=1)  # [B, num_agents]
 
-            dones = [tf.reshape(val.terminal, (-1, 1)) for val in list(o_tm1.values())]
-            dones = tf.reshape(
-                tf.concat(dones, axis=1), (-1, 1, num_agents)
-            )  # [B, 1, num_agents]
-
-            q_tm1 = tf.concat(q_tm1, axis=1)  # [B, num_agents]
-            q_t = tf.concat(q_t, axis=1)  # [B, num_agents]
-
-            q_tot_mixed = self._mixing_network(q_tm1, s_tm1)  # [B, 1, 1]
-            q_tot_target_mixed = self._target_mixing_network(q_t, s_t)  # [B, 1, 1]
+            q_tot_mixed = self._mixing_network(q_acts, s_tm1)  # [B, 1, 1]
+            q_tot_target_mixed = self._target_mixing_network(q_targets, s_t)  # [B, 1, 1]
 
             # Calculate Q loss.
-            targets = (
-                rewards
-                + self._discount * (tf.constant(1.0) - dones) * q_tot_target_mixed
-            )
+            targets = rewards + pcont * q_tot_target_mixed
             targets = tf.stop_gradient(targets)
             td_error = targets - q_tot_mixed
-
+            
             # Loss is MSE scaled by 0.5, so the gradient is equal to the TD error.
             self.loss = 0.5 * tf.reduce_mean(tf.square(td_error))
             self.tape = tape
