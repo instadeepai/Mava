17d2d9d4bf013b6a8f080da7a6cd757462a46047
Dries <dries.epos@gmail.com>
2021-05-26 16:03:25 +0200

    fix: Small fix.

--- mava/systems/tf/madqn/execution.py
+++ mava/systems/tf/madqn/execution.py
@@ -154,7 +154,7 @@ class MADQNFeedForwardExecutor(FeedForwardExecutor):
             if not self._evaluator:
                 epsilon = self._trainer.get_epsilon()
             else:
-                # Note (dries): For some reason 0 epsilon breaks on StarCraft
+                # Note (dries): For some reason 0 epsilon breaks on StarCraft.
                 epsilon = 1e-10
 
             epsilon = tf.convert_to_tensor(epsilon)
