6c167d81a1024d2b88b7a93fb02443e844b3e2ca
Dries <dries.epos@gmail.com>
2021-05-26 15:54:13 +0200

    fix: Epsilon value in MADQNFeedForwardExecutor.

--- mava/systems/tf/madqn/execution.py
+++ mava/systems/tf/madqn/execution.py
@@ -154,7 +154,8 @@ class MADQNFeedForwardExecutor(FeedForwardExecutor):
             if not self._evaluator:
                 epsilon = self._trainer.get_epsilon()
             else:
-                epsilon = 0.0
+                # Note (dries): For some reason 0 epsilon breaks on StarCraft
+                epsilon = 1e-10
 
             epsilon = tf.convert_to_tensor(epsilon)
 
@@ -175,6 +176,8 @@ class MADQNFeedForwardExecutor(FeedForwardExecutor):
             )
 
             actions[agent] = tf2_utils.to_numpy_squeeze(action)
+
+        print("actions: ", actions, ". Evaluator: ", self._evaluator)
 
         # Return a numpy array with squeezed out batch dimension.
         return actions
