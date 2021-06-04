b67749b6f9e0ad8b47c544b6e2ea5fd191310130
arnupretorius <arnupretorius@gmail.com>
2021-06-04 15:48:59 +0200

    correct copyright

--- mava/wrappers/robocup.py
+++ mava/wrappers/robocup.py
@@ -1,5 +1,5 @@
 # python3
-# Copyright 2018 DeepMind Technologies Limited. All rights reserved.
+# Copyright 2021 [...placeholder...]. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# type: ignore
 from typing import Tuple
 
 import dm_env
