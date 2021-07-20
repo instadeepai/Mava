2abb2f8d25b0a167533531ee26155913b4f1e75d
Kale-ab <kaleabtessera@gmail.com>
2021-07-20 15:08:39 +0200

    feat(adders): New acme adders and tests.

--- tests/systems/vdn_system_test.py
+++ tests/systems/vdn_system_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for MADQN."""
+"""Tests for VDN."""
 
 import functools
 
@@ -22,16 +22,15 @@ import sonnet as snt
 from launchpad.nodes.python.local_multi_processing import PythonProcess
 
 import mava
-from mava.systems.tf import madqn
+from mava.systems.tf import vdn
 from mava.utils import lp_utils
-from mava.utils.enums import ArchitectureType
 from mava.utils.environments import debugging_utils
 
 
-class TestMADQN:
-    """Simple integration/smoke test for MADQN."""
+class TestVdn:
+    """Simple integration/smoke test for Vdn."""
 
-    def test_madqn_on_debugging_env(self) -> None:
+    def test_vdn_on_debugging_env(self) -> None:
         """Tests that the system can run on the simple spread
         debugging environment without crashing."""
 
@@ -40,13 +39,14 @@ class TestMADQN:
             debugging_utils.make_environment,
             env_name="simple_spread",
             action_space="discrete",
+            return_state_info=True,
         )
 
         # networks
-        network_factory = lp_utils.partial_kwargs(madqn.make_default_networks)
+        network_factory = lp_utils.partial_kwargs(vdn.make_default_networks)
 
         # system
-        system = madqn.MADQN(
+        system = vdn.VDN(
             environment_factory=environment_factory,
             network_factory=network_factory,
             num_executors=1,
@@ -79,58 +79,4 @@ class TestMADQN:
         trainer: mava.Trainer = trainer_node.create_handle().dereference()
 
         for _ in range(5):
-            trainer.step()
-
-    def test_recurrent_madqn_on_debugging_env(self) -> None:
-        """Tests that the system can run on the simple spread
-        debugging environment without crashing."""
-
-        # environment
-        environment_factory = functools.partial(
-            debugging_utils.make_environment,
-            env_name="simple_spread",
-            action_space="discrete",
-        )
-
-        # networks
-        network_factory = lp_utils.partial_kwargs(
-            madqn.make_default_networks, archecture_type=ArchitectureType.recurrent
-        )
-
-        # system
-        system = madqn.MADQN(
-            environment_factory=environment_factory,
-            network_factory=network_factory,
-            num_executors=1,
-            batch_size=16,
-            min_replay_size=16,
-            max_replay_size=1000,
-            optimizer=snt.optimizers.Adam(learning_rate=1e-3),
-            checkpoint=False,
-            trainer_fn=madqn.training.MADQNRecurrentTrainer,
-            executor_fn=madqn.execution.MADQNRecurrentExecutor,
-        )
-
-        program = system.build()
-
-        (trainer_node,) = program.groups["trainer"]
-        trainer_node.disable_run()
-
-        # Launch gpu config - don't use gpu
-        gpu_id = -1
-        env_vars = {"CUDA_VISIBLE_DEVICES": str(gpu_id)}
-        local_resources = {
-            "trainer": PythonProcess(env=env_vars),
-            "evaluator": PythonProcess(env=env_vars),
-            "executor": PythonProcess(env=env_vars),
-        }
-        lp.launch(
-            program,
-            launch_type="test_mt",
-            local_resources=local_resources,
-        )
-
-        trainer: mava.Trainer = trainer_node.create_handle().dereference()
-
-        for _ in range(2):
             trainer.step()
