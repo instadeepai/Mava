4af6cfcc70a8142006be2fc054d122bdd631e4b6
arnupretorius <arnupretorius@gmail.com>
2021-09-06 17:19:38 +0200

    Use components to make MADDPG builder

--- mava/systems/tf/maddpg/building.py
+++ mava/systems/tf/maddpg/building.py
@@ -15,7 +15,86 @@
 
 """MADDPG system builder implementation."""
 
+import dataclasses
+from typing import Any, Dict, List, Optional, Union
+
+import sonnet as snt
+from acme.utils import counting, loggers
+
+from mava import specs
 from mava.components import building
 
 
+@dataclasses.dataclass
+class MADDPGConfig:
+    """Configuration options for the MADDPG system.
+    Args:
+        environment_spec: description of the action and observation spaces etc. for
+            each agent in the system.
+        policy_optimizer: optimizer(s) for updating policy networks.
+        critic_optimizer: optimizer for updating critic networks.
+        agent_net_keys: (dict, optional): specifies what network each agent uses.
+            Defaults to {}.
+        checkpoint_minute_interval (int): The number of minutes to wait between
+            checkpoints.
+        discount: discount to use for TD updates.
+        batch_size: batch size for updates.
+        prefetch_size: size to prefetch from replay.
+        target_averaging: whether to use polyak averaging for target network updates.
+        target_update_period: number of steps before target networks are updated.
+        target_update_rate: update rate when using averaging.
+        executor_variable_update_period: the rate at which executors sync their
+            paramters with the trainer.
+        min_replay_size: minimum replay size before updating.
+        max_replay_size: maximum replay size.
+        samples_per_insert: number of samples to take from replay for every insert
+            that is made.
+        n_step: number of steps to include prior to boostrapping.
+        sequence_length: recurrent sequence rollout length.
+        period: consecutive starting points for overlapping rollouts across a sequence.
+        max_gradient_norm: value to specify the maximum clipping value for the gradient
+            norm during optimization.
+        sigma: Gaussian sigma parameter.
+
+        checkpoint: boolean to indicate whether to checkpoint models.
+        checkpoint_subpath: subdirectory specifying where to store checkpoints.
+        replay_table_name: string indicating what name to give the replay table."""
+
+    environment_spec: specs.MAEnvironmentSpec
+    policy_optimizer: Union[snt.Optimizer, Dict[str, snt.Optimizer]]
+    critic_optimizer: snt.Optimizer
+    num_executors: int
+    agent_net_keys: Dict[str, str]
+    trainer_networks: Dict[str, List]
+    table_network_config: Dict[str, List]
+    executor_samples: List
+    net_to_ints: Dict[str, int]
+    unique_net_keys: List[str]
+    checkpoint_minute_interval: int
+    discount: float = 0.99
+    batch_size: int = 256
+    prefetch_size: int = 4
+    target_averaging: bool = False
+    target_update_period: int = 100
+    target_update_rate: Optional[float] = None
+    executor_variable_update_period: int = 1000
+    min_replay_size: int = 1000
+    max_replay_size: int = 1000000
+    samples_per_insert: Optional[float] = 32.0
+    n_step: int = 5
+    sequence_length: int = 20
+    period: int = 20
+    bootstrap_n: int = 10
+    max_gradient_norm: Optional[float] = None
+    sigma: float = 0.3
+    logger: loggers.Logger = None
+    counter: counting.Counter = None
+    checkpoint: bool = True
+    checkpoint_subpath: str = "~/mava/"
+    replay_table_name: str = "trainer"  # reverb_adders.DEFAULT_PRIORITY_TABLE
+
+
 # construct default system components
+######################################
+
+# Make replay table and dataset
