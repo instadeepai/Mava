2330a964f1dc3a5adf0617b9b77c0f297a641d01
ldfrancis <lawrencedikeu@gmail.com>
2021-09-24 14:57:50 +0100

    feat: add rendering functionalities to meltingpot wrapper

--- mava/wrappers/meltingpot.py
+++ mava/wrappers/meltingpot.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 """Wraps a PettingZoo MARL environment to be used as a dm_env environment."""
+import os
 from typing import Any, Dict, List, Union
 
 import dm_env
@@ -25,6 +26,7 @@ from mava import types
 from mava.wrappers.env_wrappers import ParallelEnvWrapper
 
 try:
+    import pygame  # type: ignore
     from meltingpot.python.scenario import Scenario  # type: ignore
     from meltingpot.python.substrate import Substrate  # type: ignore
 except ModuleNotFoundError:
@@ -42,12 +44,14 @@ class MeltingpotEnvWrapper(ParallelEnvWrapper):
         """Constructor for Melting pot wrapper.
 
         Args:
-            environment (Substrate or Scenario): parallel PZ env.
+            environment (Substrate or Scenario): Melting pot substrate or scenario.
         """
         self._environment = environment
         self._reset_next_step = True
         self._env_done = False
         self._num_agents = len(self._environment.action_spec)
+        self._env_image: np.ndarray = None
+        self._screen = None
 
     def reset(self) -> dm_env.TimeStep:
         """Resets the env.
@@ -60,7 +64,20 @@ class MeltingpotEnvWrapper(ParallelEnvWrapper):
         self._reset_next_step = False
         self._step_type = timestep.steptype
 
+        self._obtain_env_image(timestep)
+
         return self._refine_timestep(timestep)
+
+    def _set_env_image(self, timestep: dm_env.TimeStep) -> None:
+        """Sets an image of the environment from a timestep
+
+        The image is from the observation key 'WORLD.RGB'
+
+        Args:
+            timestep (dm_env.TimeStep): timestep from the meltinpot env
+
+        """
+        self._env_image = timestep.observation[0]["WORLD.RGB"]
 
     def _to_olt(
         self, observation: Dict[str, NestedArray], num_values: int, is_terminal: bool
@@ -72,6 +89,8 @@ class MeltingpotEnvWrapper(ParallelEnvWrapper):
 
         Args:
             observation (TimeStep): the observation
+            num_values (int): the number of actions
+            is_terminal (bool): whether its a terminal observation
 
         Returns:
             types.OLT: observation, legal actions, and terminal
@@ -85,20 +104,68 @@ class MeltingpotEnvWrapper(ParallelEnvWrapper):
     def _to_dict_observation(
         self, observation: List[Dict[str, NestedArray]], is_terminal: bool
     ) -> Dict[str, types.OLT]:
+        """Observation list to dict
+
+        Transforms a list of observations into a dictionary of observations
+        with keys corresponding to the agent ids
+
+        Args:
+            observation (List[Dict[str, NestedArray]]): List observation
+            is_terminal (bool): whether the observations corresponds to a
+             terminal timestep
+
+        Returns:
+            Dict[str, types.OLT]: Dictionary of observations
+        """
         return {
             f"agent_{i}": self._to_olt(obs, self._num_actions, is_terminal)
             for i, obs in enumerate(observation)
         }
 
     def _to_dict_rewards(self, rewards: List[NestedArray]) -> Dict[str, NestedArray]:
+        """List of rewards to Dict of rewards
+
+        Transforms a list of rewards to a dictionary of rewards with keys corresponding
+        to the agent ids
+
+        Args:
+            rewards (List[NestedArray]): List of rewards
+
+        Returns:
+            Dict[str, NestedArray]: Dictionary of reward
+        """
         return {f"agent_{i}": rew for i, rew in enumerate(rewards)}
 
     def _to_dict_discounts(
         self, discounts: List[NestedArray]
     ) -> Dict[str, NestedArray]:
+        """List of dicounts to Dict of discounts
+
+        Transforms a list of discounts into a dictionary of dixcounts with keys
+        corresponding to the agent ids
+
+        Args:
+            discounts (List[NestedArray]): List of discounts
+
+        Returns:
+            Dict[str, NestedArray]: Dictionary of discounts
+        """
         return {f"agent_{i}": disc for i, disc in enumerate(discounts)}
 
     def _refine_timestep(self, timestep: dm_env.TimeStep) -> dm_env.TimeStep:
+        """Converts a melting pot timestep into one compatiple with Mava
+
+        The difference between the timestep from melting pot and that of Mava is
+        that for the observation, reward, and discount, mava expects dictionaries
+        with keys corresponding to the agent ids while melting pot simply uses a
+        list for this.
+
+        Args:
+            timestep (dm_env.TimeStep): a timestep from melting pot
+
+        Returns:
+            dm_env.TimeStep: a timestep compatible with Mava
+        """
         is_terminal = timestep.last()
         timestep.observation = self._to_dict_observation(
             timestep.observation, is_terminal
@@ -122,6 +189,7 @@ class MeltingpotEnvWrapper(ParallelEnvWrapper):
 
         actions_ = [actions[f"agent_{i}"] for i in range(self._num_agents)]
         timestep = self._environment.step(actions_)
+        self._set_env_image(timestep)
         timestep = self._refine_timestep(timestep)
 
         if timestep.last():
@@ -129,6 +197,53 @@ class MeltingpotEnvWrapper(ParallelEnvWrapper):
             self._env_done = True
 
         return timestep
+
+    def render(
+        self, mode: str = "human", screen_width: int = 800, screen_height: int = 600
+    ) -> np.ndarray:
+        """Renders the environment in a pygame window or returns an image
+
+        Args:
+            mode (str, optional): mode for the display either rgb_array or human.
+            Defaults to "human".
+            screen_width (int, optional): the screen width. Defaults to 800.
+            screen_height (int, optional): the screen height. Defaults to 600.
+
+        Raises:
+            ValueError: for invalid mode
+
+        Returns:
+            [np.ndarray]: an image array for mode, rgb_array
+        """
+        image = self._env_image
+        height, width, _ = image.shape
+        scale = min(screen_height // height, screen_width // width)
+        if mode == "human":
+            os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+            if self._screen is None:
+                pygame.init()
+                self._screen = pygame.display.set_mode(
+                    (screen_width * scale, screen_height * scale)
+                )
+            image = np.transpose(image, (1, 0, 2))  # PyGame is column major!
+            surface = pygame.surfarray.make_surface(image)
+            rect = surface.get_rect()
+            surf = pygame.transform.scale(surface, (rect[2] * scale, rect[3] * scale))
+            self._screen.blit(surf, dest=(0, 0))  # type: ignore
+            pygame.display.update()
+
+        elif mode == "rgb_array":
+            return image
+        else:
+            raise ValueError("bad value for render mode")
+
+    def close(self) -> None:
+        """Closes the rendering screen"""
+        if self._screen is not None:
+            import pygame
+
+            pygame.quit()
+            self._screen = None
 
     def env_done(self) -> bool:
         """Check if env is done.
