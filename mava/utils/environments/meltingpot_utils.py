a4bc51c55eac3c768d60dc5853b781e904a1a473
ldfrancis <lawrencedikeu@gmail.com>
2021-09-24 14:53:30 +0100

    feat: include environment factory for melting pot

--- mava/utils/environments/meltingpot_utils.py
+++ mava/utils/environments/meltingpot_utils.py
@@ -13,16 +13,94 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any
+from typing import Any, Union
 
 try:
     from meltingpot.python import scenario, substrate  # type: ignore
-    from meltingpot.python.scenario import Scenario  # type: ignore
-    from meltingpot.python.substrate import Substrate  # type: ignore
+    from meltingpot.python.scenario import AVAILABLE_SCENARIOS, Scenario  # type: ignore
+    from meltingpot.python.substrate import (  # type: ignore
+        AVAILABLE_SUBSTRATES,
+        Substrate,
+    )
     from ml_collections import config_dict  # type: ignore
 except ModuleNotFoundError:
     Scenario = Any
     Substrate = Any
+
+
+class EnvironmentFactory:
+    def __init__(
+        self, substrate: str = None, scenario: str = None, mode: str = "substrate"
+    ):
+        """Initializes the env factory object
+
+        Args:
+            substrate (str, optional): what substrate to use. Defaults to None.
+            scenario (str, optional): what scenario to use. Defaults to None.
+            mode (str, optional): what mode to use either 'scenario' or 'substrate'.
+            Defaults to "substrate".
+        """
+        assert mode in [
+            "substrate",
+            "scenario",
+        ], f"mode can either be 'substrate' or 'scenario' not {mode}"
+        if mode == "substrate":
+            substrates = [*AVAILABLE_SUBSTRATES, "all"]
+            assert (
+                substrate in substrates
+            ), f"substrate cannot be f{substrate}, use any of {substrates}"
+
+            if substrate == "all":
+                self.available_substrates = AVAILABLE_SUBSTRATES
+            else:
+                self.available_substrates = [substrate]
+            self._env_fn = self._substrate
+
+        if mode == "scenario":
+            scenarios = [*[k for k in AVAILABLE_SCENARIOS], "all"]
+            assert (
+                scenario in scenarios
+            ), f"substrate cannot be f{substrate}, use any of {scenarios}"
+            if scenario == "all":
+                self.available_scenarios = AVAILABLE_SCENARIOS
+            else:
+                self.available_scenarios = [scenario]
+            self._env_fn = self._scenario
+
+    def _substrate(self) -> Substrate:
+        """Return a substrate as an environment
+
+        Returns:
+            [Substrate]: A substrate or None
+        """
+        if self.available_substrates:
+            name = self.available_substrates.pop()
+            env = load_substrate(name)
+            return env
+        else:
+            return None
+
+    def _scenario(self) -> Scenario:
+        """Return a scenario as an environment
+
+        Returns:
+            [Scenario]: A scenario or None
+        """
+        if self.available_scenarios:
+            name = self.available_scenarios.pop()
+            env = load_scenario(name)
+            return env
+        else:
+            return None
+
+    def __call__(self, evaluation: bool = False) -> Union[Substrate, Scenario]:
+        """Creates an environment
+
+        Returns:
+            [type]: The created environment
+        """
+        env = self._env_fn()  # type: ignore
+        return env
 
 
 def load_substrate(substrate_name: str) -> Substrate:
