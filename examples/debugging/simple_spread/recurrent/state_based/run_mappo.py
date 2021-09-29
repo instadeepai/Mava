1e4aaae3cd0d8cea841f2f89acdca8cf859d504f
Dries <dries.epos@gmail.com>
2021-09-29 10:14:52 +0200

    Small fix.

--- examples/debugging/simple_spread/recurrent/state_based/run_mappo.py
+++ examples/debugging/simple_spread/recurrent/state_based/run_mappo.py
@@ -47,8 +47,7 @@ flags.DEFINE_string(
     str(datetime.now()),
     "Experiment identifier that can be used to continue experiments.",
 )
-print("Dries change this back to ~/mava.")
-flags.DEFINE_string("base_dir", "/home/mava_logs", "Base dir to store experiments.")
+flags.DEFINE_string("base_dir", "~/mava", "Base dir to store experiments.")
 
 
 def main(_: Any) -> None:
