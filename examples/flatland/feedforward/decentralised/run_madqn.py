b190fa143019fea92d89f0a780d3fd72195e74a2
Paul Caron <paulcaron96@hotmail.fr>
2021-11-30 16:43:30 +0100

    fix: Revert to the former scheduler

--- examples/flatland/feedforward/decentralised/run_madqn.py
+++ examples/flatland/feedforward/decentralised/run_madqn.py
@@ -87,43 +87,37 @@ def main(_: Any) -> None:
         time_delta=log_every,
     )
 
-    num_executors = 8
-
-    # exploration_scheduler_fn={
-    #     "executor_0": {
-    #         "train_0": LinearExplorationScheduler(
-    #             epsilon_start=1.0, epsilon_min=0.05, epsilon_decay=1e-4
-    #         ),
-    #         "train_1": LinearExplorationScheduler(
-    #             epsilon_start=1.0, epsilon_min=0.06, epsilon_decay=2e-4
-    #         ),
-    #         "train_2": LinearExplorationScheduler(
-    #             epsilon_start=1.0, epsilon_min=0.07, epsilon_decay=3e-4
-    #         ),
-    #     },
-    #     "executor_1": {
-    #         "train_0": LinearExplorationScheduler(
-    #             epsilon_start=1.0, epsilon_min=0.08, epsilon_decay=4e-4
-    #         ),
-    #         "train_1": LinearExplorationScheduler(
-    #             epsilon_start=1.0, epsilon_min=0.09, epsilon_decay=5e-4
-    #         ),
-    #         "train_2": LinearExplorationScheduler(
-    #             epsilon_start=1.0, epsilon_min=0.10, epsilon_decay=6e-4
-    #         ),
-    #     },
-    # },
-
-    exploration_scheduler_fn = LinearExplorationScheduler(
-        epsilon_start=1.0, epsilon_min=0.05, epsilon_decay=1e-4
-    )
+    exploration_scheduler_fn = {
+        "executor_0": {
+            "train_0": LinearExplorationScheduler(
+                epsilon_start=1.0, epsilon_min=0.05, epsilon_decay=1e-4
+            ),
+            "train_1": LinearExplorationScheduler(
+                epsilon_start=1.0, epsilon_min=0.06, epsilon_decay=2e-4
+            ),
+            "train_2": LinearExplorationScheduler(
+                epsilon_start=1.0, epsilon_min=0.07, epsilon_decay=3e-4
+            ),
+        },
+        "executor_1": {
+            "train_0": LinearExplorationScheduler(
+                epsilon_start=1.0, epsilon_min=0.08, epsilon_decay=4e-4
+            ),
+            "train_1": LinearExplorationScheduler(
+                epsilon_start=1.0, epsilon_min=0.09, epsilon_decay=5e-4
+            ),
+            "train_2": LinearExplorationScheduler(
+                epsilon_start=1.0, epsilon_min=0.10, epsilon_decay=6e-4
+            ),
+        },
+    }
 
     # distributed program
     program = madqn.MADQN(
         environment_factory=environment_factory,
         network_factory=network_factory,
         logger_factory=logger_factory,
-        num_executors=num_executors,
+        num_executors=2,
         exploration_scheduler_fn=exploration_scheduler_fn,
         optimizer=snt.optimizers.Adam(learning_rate=1e-4),
         max_executor_steps=100_000,
