d968964aa94a0dc8dac06b05f53f6f03ca8b3639
Kale-ab <kaleabtessera@gmail.com>
2021-12-13 11:34:25 +0200

    chore: Added debian frontend config to bash scrips.

--- bash_scripts/check_format.sh
+++ bash_scripts/check_format.sh
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export DEBIAN_FRONTEND=noninteractive
 # Bash settings: fail on any error and display all commands being run.
 set -e
 set -x
