ef891f28a6d2f71a9ba39013eecfd40d7fbc3113
arnupretorius <arnupretorius@gmail.com>
2022-02-17 14:31:02 +0200

    chore: add todo to remove mypy ignore when system is running

--- examples/jax/run_maddpg_test.py
+++ examples/jax/run_maddpg_test.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO(Arnu): remove mypy ignore when the system is running
 # type: ignore
 
 """Test for running Jax MADDPG on debug MPE environment."""
