a828fb93d32ebe5737de11aee9de872147d41f62
arnupretorius <arnupretorius@gmail.com>
2022-02-17 21:11:14 +0200

    feat: first take on general jax system class

--- mava/systems/jax/system.py
+++ mava/systems/jax/system.py
@@ -1,106 +1,120 @@
-# # python3
-# # Copyright 2021 InstaDeep Ltd. All rights reserved.
-# #
-# # Licensed under the Apache License, Version 2.0 (the "License");
-# # you may not use this file except in compliance with the License.
-# # You may obtain a copy of the License at
-# #
-# #     http://www.apache.org/licenses/LICENSE-2.0
-# #
-# # Unless required by applicable law or agreed to in writing, software
-# # distributed under the License is distributed on an "AS IS" BASIS,
-# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# # See the License for the specific language governing permissions and
-# # limitations under the License.
+# python3
+# Copyright 2021 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# """Mava system implementation."""
+# TODO(Arnu): remove later
+# type: ignore
 
-# import abc
-# from types import SimpleNamespace
-# from typing import Any, List
+"""Mava system implementation."""
 
-# from mava.callbacks import Callback
-# from mava.components import building
-# from mava.core import BaseSystem
-# from mava.systems import Builder
+from types import SimpleNamespace
+from typing import Any, List
+
+from mava.core_jax import BaseSystem
+from mava.systems.jax import Builder, Config, Design
 
 
-# class System(BaseSystem):
-#     def update(self, component: Callback, name: str) -> None:
-#         """Update a component that has already been added to the system.
-#         Args:
-#             component : system callback component
-#             name : component name
-#         """
+# TODO(Arnu): replace component types with Callback when class is ready.
+class System(BaseSystem):
+    def __init__(self, design: Design) -> None:
+        """_summary_
 
-#         if name in list(self.system_components.__dict__.keys()):
-#             self.system_components.__dict__[name] = component
-#         else:
-#             raise Exception(
-#                 "The given component is not part of the current system.
-# Perhaps try adding it instead using .add()."
-#             )
+        Args:
+            design : _description_
+        """
+        self._design = design
+        self.config = Config()  # Mava config
+        self.components: List = []
 
-#     def add(self, component: Callback, name: str) -> None:
-#         """Add a new component to the system.
-#         Args:
-#             component : system callback component
-#             name : component name
-#         """
+        # make config from build
+        self._make_config()
 
-#         if name in list(self.system_components.__dict__.keys()):
-#             raise Exception(
-#                 "The given component is already part of the current system.
-# Perhaps try updating it instead using .update()."
-#             )
-#         else:
-#             self.system_components.__dict__[name] = component
+    def _make_config(self) -> None:
+        """_summary_"""
+        for component in self._design.components:
+            comp = component()
+            # note this requires default dataclasses for components
+            # note this requires "name" properties for components
+            input = {comp.name: comp.config}
+            self.config.add(**input)
+        self.config.build()
 
-#     @abc.abstractmethod
-#     def build(self, config: SimpleNamespace) -> SimpleNamespace:
-#         """Build system by constructing system components.
-#         Args:
-#             config : system configuration including
-#         Returns:
-#             System components
-#         """
+    def update(self, component: Any, name: str) -> None:
+        """Update a component that has already been added to the system.
 
-#     def distribute(
-#         self,
-#         num_executors: int = 1,
-#         nodes_on_gpu: List[str] = ["trainer"],
-#         distributor: Callback = None,
-#     ):
-#         """Distribute system across multiple processes.
+        Args:
+            component : system callback component
+            name : component name
+        """
 
-#         Args:
-#             num_executors : number of executor processes to run in parallel
-#             multi_process : whether to run locally or distributed
-#             nodes_on_gpu : which processes to run on gpu
-#             distributor : custom distributor component
-#         """
+        if name in list(self._design.components.__dict__.keys()):
+            self._design.components.__dict__[name] = component
+            self.config.update(name=component().config)
+        else:
+            raise Exception(
+                "The given component is not part of the current system.\
+                Perhaps try adding it instead using .add()."
+            )
 
-#         # Distributor
-#         distributor_fn = distributor if distributor else building.Distributor
-#         distribute = distributor_fn(
-#             num_executors=num_executors,
-#             nodes_on_gpu=nodes_on_gpu,
-#             run_evaluator="evaluator" in list(self.system_components.__dict__.keys()),
-#         )
-#         self.add(component=distribute, name="distributor")
+    def add(self, component: Any, name: str) -> None:
+        """Add a new component to the system.
 
-#     def launch(
-#         self,
-#         name: str = "system",
-#     ):
-#         """Run the system.
+        Args:
+            component : system callback component
+            name : component name
+        """
 
-#         Args:
-#             name : name of the system
-#         """
+        if name in list(self._design.components.__dict__.keys()):
+            raise Exception(
+                "The given component is already part of the current system.\
+                Perhaps try updating it instead using .update()."
+            )
+        else:
+            self._design.components.__dict__[name] = component
+            self.config.add(name=component().config)
 
-#         component_feed = list(self.system_components.__dict__.values())
+    def launch(
+        self,
+        config: SimpleNamespace,
+        num_executors: int,
+        nodes_on_gpu: List[str],
+        multi_process: bool = True,
+        name: str = "system",
+    ) -> None:
+        """Run the system.
 
-#         # Builder
-#         self._builder = Builder(components=component_feed)
-#         self._builder.build()
+        Args:
+            config : system configuration including
+            num_executors : number of executor processes to run in parallel
+            nodes_on_gpu : which processes to run on gpu
+            multi_process : whether to run locally or distributed, local runs are
+                for debugging
+            name : name of the system
+        """
+        # update distributor config
+        config.set(
+            num_executors=num_executors,
+            nodes_on_gpu=nodes_on_gpu,
+            multi_process=multi_process,
+            name=name,
+        )
+        system_config = config.get()
+
+        # update default system component configs
+        for component in self._design.components:
+            self.components.append(component(system_config))
+
+        # Build system
+        self._builder = Builder(components=self.components)
+        self._builder.build()
