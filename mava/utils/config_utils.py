8fad7868a4f85871353e3cb95a15d1a319d21ab5
arnupretorius <arnupretorius@gmail.com>
2022-02-24 13:30:07 +0200

    fix: remove unneeded pass in config utils

--- mava/utils/config_utils.py
+++ mava/utils/config_utils.py
@@ -35,7 +35,6 @@ def flatten_dict(
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
         if isinstance(v, MutableMapping):
-            pass
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         else:
             items.append((new_key, v))
