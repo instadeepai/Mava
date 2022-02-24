24344167e300f916ee7d78113250c0d5edac56db
RuanJohn <ruanjdekock@gmail.com>
2022-02-24 16:40:46 +0200

    Removed testing class and added pytest fixture

--- tests/utils/config_utils_test.py
+++ tests/utils/config_utils_test.py
@@ -13,38 +13,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import MutableMapping
+
+import pytest
+
 from mava.utils.config_utils import flatten_dict
 
-test_mock_nested_dict = {"a": 1, "c": {"a": 2, "b": {"x": 5, "y": 10}}, "d": [1, 2, 3]}
+
+@pytest.fixture
+def test_mock_nested_dict() -> MutableMapping:
+    """Mock nested dictionary."""
+    return {"a": 1, "c": {"a": 2, "b": {"x": 5, "y": 10}}, "d": [1, 2, 3]}
 
 
-class TestConfigUtils:
-    def test_flatten_dict(self) -> None:
-        """Tests flatten_dict."""
-        assert flatten_dict(d=test_mock_nested_dict) == {
-            "a": 1,
-            "c.a": 2,
-            "c.b.x": 5,
-            "c.b.y": 10,
-            "d": [1, 2, 3],
-        }
+def test_flatten_dict(test_mock_nested_dict: MutableMapping) -> None:
+    """Tests flatten_dict."""
+    assert flatten_dict(d=test_mock_nested_dict) == {
+        "a": 1,
+        "c.a": 2,
+        "c.b.x": 5,
+        "c.b.y": 10,
+        "d": [1, 2, 3],
+    }
 
-    def test_flatten_dict_different_sep(self) -> None:
-        """Tests flatten dict with specified sep token."""
-        assert flatten_dict(d=test_mock_nested_dict, sep="_") == {
-            "a": 1,
-            "c_a": 2,
-            "c_b_x": 5,
-            "c_b_y": 10,
-            "d": [1, 2, 3],
-        }
 
-    def test_flatten_dict_with_parent_key(self) -> None:
-        """Tests flatten dict with specified parent key."""
-        assert flatten_dict(d=test_mock_nested_dict, parent_key="test_parent") == {
-            "test_parent.a": 1,
-            "test_parent.c.a": 2,
-            "test_parent.c.b.x": 5,
-            "test_parent.c.b.y": 10,
-            "test_parent.d": [1, 2, 3],
-        }
+def test_flatten_dict_different_sep(test_mock_nested_dict: MutableMapping) -> None:
+    """Tests flatten dict with specified sep token."""
+    assert flatten_dict(d=test_mock_nested_dict, sep="_") == {
+        "a": 1,
+        "c_a": 2,
+        "c_b_x": 5,
+        "c_b_y": 10,
+        "d": [1, 2, 3],
+    }
+
+
+def test_flatten_dict_with_parent_key(test_mock_nested_dict: MutableMapping) -> None:
+    """Tests flatten dict with specified parent key."""
+    assert flatten_dict(d=test_mock_nested_dict, parent_key="test_parent") == {
+        "test_parent.a": 1,
+        "test_parent.c.a": 2,
+        "test_parent.c.b.x": 5,
+        "test_parent.c.b.y": 10,
+        "test_parent.d": [1, 2, 3],
+    }
