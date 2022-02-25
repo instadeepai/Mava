88585b5ad25b99d12b116219c8fb1a6f24b8d765
arnupretorius <arnupretorius@gmail.com>
2022-02-25 15:59:43 +0200

    feat: general builder class with hooks

--- mava/systems/jax/__init__.py
+++ mava/systems/jax/__init__.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# type: ignore
+
 """Jax-based Mava system implementation."""
 
 from mava.systems.jax.builder import Builder
--- mava/systems/jax/builder.py
+++ mava/systems/jax/builder.py
@@ -13,12 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO (Arnu): remove once we figure out the no attribute error.
+# type: ignore
+
 """Jax-based Mava system builder implementation."""
 
-from typing import Any, List
+from typing import Any, Dict, List
+
+from mava.core_jax import SystemBuilder
 
 
-class Builder:
+class Builder(SystemBuilder):
     def __init__(
         self,
         components: List[Any],
@@ -31,10 +36,185 @@ class Builder:
 
         self.callbacks = components
 
+        self.on_building_init_start()
+
+        self.on_building_init()
+
+        self.on_building_init_end()
+
+    def data_server(self) -> List[Any]:
+        """Data server to store and serve transition data from and to system.
+
+        Returns:
+            System data server
+        """
+
+        # start of make replay tables
+        self.on_building_tables_start()
+
+        # make adder signature
+        self.on_building_tables_adder_signature()
+
+        # make rate limiter
+        self.on_building_tables_rate_limiter()
+
+        # make tables
+        self.on_building_tables_make_tables()
+
+        # end of make replay tables
+        self.on_building_tables_end()
+
+        return self.system_data_server
+
+    def parameter_server(self, extra_nodes: Dict = {}) -> Any:
+        """Parameter server to store and serve system network parameters.
+
+        Args:
+            extra_nodes : additional nodes to add to a launchpad program build
+        Returns:
+            System parameter server
+        """
+
+        # Extra nodes
+        # TODO (dries): Remove this again if it is not necessary.
+        self._extra_nodes = extra_nodes
+
+        # start of make variable server
+        self.on_building_variable_server_start()
+
+        # make variable server
+        self.on_building_variable_server_make_variable_server()
+
+        # end of make variable server
+        self.on_building_variable_server_end()
+
+        return self.system_parameter_server
+
+    def executor(
+        self, executor_id: str, data_server_client: Any, parameter_server_client: Any
+    ) -> Any:
+        """Executor, a collection of agents in an environment to gather experience.
+
+        Args:
+            executor_id : id to identify the executor process for logging purposes
+            data_server_client : data server client for pushing transition data
+            parameter_server_client : parameter server client for pulling parameters
+        Returns:
+            System executor
+        """
+
+        self._executor_id = executor_id
+        self._data_server_client = data_server_client
+        self._parameter_server_client = parameter_server_client
+        self._evaluator = False
+
+        if self._executor_id == "evaluator":
+            self._evaluator = True
+        else:
+            # ADDER
+            # start of make adder
+            self.on_building_adder_start()
+
+            # make adder signature
+            self.on_building_adder_set_priority()
+
+            # make rate limiter
+            self.on_building_adder_make_adder()
+
+            # end of make adder
+            self.on_building_adder_end()
+
+        # EXECUTOR
+        # start of making the executor
+        self.on_building_executor_start()
+
+        # make executor logger
+        self.on_building_executor_logger()
+
+        # make executor parameter client
+        self.on_building_executor_parameter_client()
+
+        # make executor
+        self.on_building_executor_make_executor()
+
+        # make copy of environment
+        self.on_building_executor_environment()
+
+        # make train loop
+        self.on_building_executor_train_loop()
+
+        # end of making the executor
+        self.on_building_executor_end()
+
+        return self.system_executor
+
+    def trainer(
+        self, trainer_id: str, data_server_client: Any, parameter_server_client: Any
+    ) -> Any:
+        """Trainer, a system process for updating agent specific network parameters.
+
+        Args:
+            trainer_id : id to identify the trainer process for logging purposes
+            data_server_client : data server client for pulling transition data
+            parameter_server_client : parameter server client for pushing parameters
+        Returns:
+            System trainer
+        """
+
+        self._trainer_id = trainer_id
+        self._data_server_client = data_server_client
+        self._table_name = f"table_{trainer_id}"
+        self._parameter_server_client = parameter_server_client
+
+        # DATASET
+        # start of make dataset iterator
+        self.on_building_dataset_start()
+
+        # make dataset
+        self.on_building_dataset_make_dataset()
+
+        # end of make dataset iterator
+        self.on_building_dataset_end()
+
+        # TRAINER
+        # start of making the trainer
+        self.on_building_trainer_start()
+
+        # make trainer logger
+        self.on_building_trainer_logger()
+
+        # make dataset
+        self.on_building_trainer_dataset()
+
+        # make trainer parameter client
+        self.on_building_trainer_parameter_client()
+
+        # make trainer
+        self.on_building_trainer_make_trainer()
+
+        # end of making the trainer
+        self.on_building_trainer_end()
+
+        return self.system_trainer
+
     def build(self) -> None:
-        """Build the system."""
-        pass
+        """Construct program nodes."""
+        # start of system building
+        self.on_building_start()
+
+        # build program nodes
+        self.on_building_program_nodes()
+
+        # end of system building
+        self.on_building_end()
 
     def launch(self) -> None:
-        """Launch the system"""
-        pass
+        """Run the graph program."""
+        # start of system launch
+        self.on_building_launch_start()
+
+        # launch system
+        self.on_building_launch_distributor()
+
+        # end of system launch
+        self.on_building_launch_end()
