5473292cbdbc732d083ded8682c86bd58643361b
arnupretorius <arnupretorius@gmail.com>
2022-03-16 11:27:33 +0200

    feat: executor process component wip

---
+++ mava/components/jax/building/executor.py
@@ -0,0 +1,29 @@
+# python3
+# Copyright 2021 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Execution components for system builders"""
+
+from mava.components.jax.building.base import BaseExecutorProcess
+from mava.core_jax import SystemBuilder
+
+
+class ExecutorProcess(BaseExecutorProcess):
+    def on_building_executor(self, builder: SystemBuilder) -> None:
+        """_summary_
+
+        Args:
+            builder : _description_
+        """
+        # builder.executor_fn = Executor(builder.components)
