4b2b1b8dbdcc3af1050de6360eac6427d2f6f4af
Kale-ab <kaleabtessera@gmail.com>
2022-05-03 12:09:16 +0200

    chore: Updated function typing.

--- mava/utils/wrapper_utils.py
+++ mava/utils/wrapper_utils.py
@@ -116,7 +116,9 @@ def generate_zeros_from_spec(spec: specs.Array) -> np.ndarray:
     return np.zeros(spec.shape, spec.dtype)
 
 
-def convert_np_type(dtype: np.dtype, value: Union[int, float]) -> Union[int, float]:
+def convert_np_type(
+    dtype: Union[np.dtype, str], value: Union[int, float]
+) -> Union[int, float]:
     """Converts value to np dtype.
 
     Args:
