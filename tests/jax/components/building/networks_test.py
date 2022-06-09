2d7e15ae23f80927c2d31f0bc017023a3a36d54f
Matthew Morris <m.morris@instadeep.com>
2022-06-09 12:10:36 +0200

    test: network factory fixture

--- tests/jax/components/building/networks_test.py
+++ tests/jax/components/building/networks_test.py
@@ -1,10 +1,11 @@
-from typing import Any, Callable
+from typing import Any, Callable, Dict, List, Sequence
 
 import pytest
 
 from mava.components.jax.building import DefaultNetworks
 from mava.components.jax.building.networks import Networks, NetworksConfig
 from mava.core_jax import SystemBuilder
+from mava.specs import MAEnvironmentSpec
 from mava.systems.jax import Builder, mappo
 
 
@@ -18,9 +19,50 @@ class TestDefaultNetworks(DefaultNetworks):
 
 
 @pytest.fixture
-def test_network_factory() -> Callable:
+def test_mappo_network_factory() -> Callable:
     def network_factory(*args: Any, **kwargs: Any) -> Any:
         return mappo.make_default_networks(  # type: ignore
+            policy_layer_sizes=(254, 254, 254),
+            critic_layer_sizes=(512, 512, 256),
+            *args,
+            **kwargs,
+        )
+
+    return network_factory
+
+
+@pytest.fixture
+def test_network_factory() -> Callable:
+    def make_default_networks(
+        environment_spec: MAEnvironmentSpec,
+        agent_net_keys: Dict[str, str],
+        rng_key: List[int],
+        net_spec_keys: Dict[str, str] = {},
+        policy_layer_sizes: Sequence[int] = (
+            256,
+            256,
+            256,
+        ),
+        critic_layer_sizes: Sequence[int] = (512, 512, 256),
+    ) -> Dict[str, Any]:
+        net_keys = {"1", "2", "3"}
+        networks = {}
+
+        for net_key in net_keys:
+            networks[net_key] = {
+                "environment_spec": environment_spec,
+                "agent_net_keys": agent_net_keys,
+                "rng_key": rng_key,
+                "net_spec_keys": net_spec_keys,
+                "policy_layer_sizes": policy_layer_sizes,
+                "critic_layer_sizes": critic_layer_sizes,
+                "net_key": net_key,
+            }
+
+        return networks
+
+    def network_factory(*args: Any, **kwargs: Any) -> Any:
+        return make_default_networks(  # type: ignore
             policy_layer_sizes=(254, 254, 254),
             critic_layer_sizes=(512, 512, 256),
             *args,
