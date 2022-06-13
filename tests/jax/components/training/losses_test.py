5c8051340aee1d88140fd0f26ad5a4bdb4cbe93e
siddarthsingh1 <s.singh@instadeep.com>
2022-06-13 17:33:46 +0200

    fix: cleanup

--- tests/jax/components/training/losses_test.py
+++ tests/jax/components/training/losses_test.py
@@ -1,13 +1,18 @@
 from types import SimpleNamespace
-#from typing import Callable, List, Tuple
+
+# from typing import Callable, List, Tuple
 from typing import Any, Callable, Dict, Optional, Tuple
 
 import jax
 import jax.numpy as jnp
 import pytest
 
-from mava.components.jax.training.losses import MAPGTrustRegionClippingLossConfig, MAPGWithTrustRegionClippingLoss
+from mava.components.jax.training.losses import (
+    MAPGTrustRegionClippingLossConfig,
+    MAPGWithTrustRegionClippingLoss,
+)
 from mava.systems.jax.trainer import Trainer
+
 
 @pytest.fixture
 def mock_trainer() -> Trainer:
@@ -18,7 +23,8 @@ def mock_trainer() -> Trainer:
 
     return mock_trainer
 
-#@pytest.fixture
+
+# @pytest.fixture
 def mapg_trust_region_clipping_loss_config() -> MAPGTrustRegionClippingLossConfig:
     """Creates an MAPG loss config fixture with trust region and clipping"""
 
@@ -26,64 +32,38 @@ def mapg_trust_region_clipping_loss_config() -> MAPGTrustRegionClippingLossConfi
 
     return test_mapg_config
 
+
 @pytest.fixture
 def mapg_trust_region_clipping_loss() -> MAPGWithTrustRegionClippingLoss:
     """Creates an MAPG loss fixture with trust region and clipping"""
 
-    test_mapg = MAPGWithTrustRegionClippingLoss(config=MAPGTrustRegionClippingLossConfig())
+    test_mapg = MAPGWithTrustRegionClippingLoss(
+        config=MAPGTrustRegionClippingLossConfig()
+    )
 
     return test_mapg
 
-def test_mapg_creation(mock_trainer: Trainer, mapg_trust_region_clipping_loss:MAPGWithTrustRegionClippingLoss) -> None:
+
+def test_mapg_creation(
+    mock_trainer: Trainer,
+    mapg_trust_region_clipping_loss: MAPGWithTrustRegionClippingLoss,
+) -> None:
     """Test whether mapg function is successfully created"""
-    
+
     mapg_trust_region_clipping_loss.on_training_loss_fns(trainer=mock_trainer)
-    assert hasattr(mock_trainer.store,"grad_fn")
-    assert isinstance(mock_trainer.store.grad_fn, Callable) # type:ignore
+    assert hasattr(mock_trainer.store, "grad_fn")
+    assert isinstance(mock_trainer.store.grad_fn, Callable)  # type:ignore
+
 
 def test_mapg_config_creation() -> None:
     """Test whether mapg loss config variables are of correct type"""
-    
+
     mapg_config = mapg_trust_region_clipping_loss_config()
-    
-    assert isinstance(mapg_config.clipping_epsilon,float)
-    assert isinstance(mapg_config.clip_value,bool)
-    assert isinstance(mapg_config.entropy_cost,float)
-    assert isinstance(mapg_config.value_cost,float)
+
+    assert isinstance(mapg_config.clipping_epsilon, float)
+    assert isinstance(mapg_config.clip_value, bool)
+    assert isinstance(mapg_config.entropy_cost, float)
+    assert isinstance(mapg_config.value_cost, float)
 
 
-def mock_actions() -> Dict[str, jnp.ndarray]:
-    actions = jnp.array([1,1,1,1,1])
-    return {"agent_1":actions,"agent_2":actions,"agent_3":actions}
 
-def mock_behaviour_log_probs() -> Dict[str, jnp.ndarray]:
-    actions = jnp.array([-1.7,-1.7,-1.7,-1.7,-1.7])
-    return {"agent_1":actions,"agent_2":actions,"agent_3":actions}
-
-def mock_target_values() -> Dict[str, jnp.ndarray]:
-    actions = jnp.array([3,3,3,3,3])
-    return {"agent_1":actions,"agent_2":actions,"agent_3":actions}
-
-def mock_advantages() -> Dict[str, jnp.ndarray]:
-    actions = jnp.array([2,2,2,2,2])
-    return {"agent_1":actions,"agent_2":actions,"agent_3":actions}
-
-def mock_behavior_values() -> Dict[str, jnp.ndarray]:
-    actions = jnp.array([1,1,1,1,1])
-    return {"agent_1":actions,"agent_2":actions,"agent_3":actions}
-
-def test_mapg_loss(mock_trainer: Trainer,
-     mapg_trust_region_clipping_loss:MAPGWithTrustRegionClippingLoss,)-> None:
-    """Test whether mapg loss output is as expected"""
-    #https://github.com/deepmind/rlax/blob/master/rlax/_src/policy_gradients_test.py
-    mapg_trust_region_clipping_loss.on_training_loss_fns(trainer=mock_trainer)
-    grad_fn = mock_trainer.store.grad_fn
-
-    actions = mock_actions()
-    behaviour_log_probs = mock_behaviour_log_probs()
-    target_values = mock_target_values()
-    advantages = mock_advantages()
-    behavior_values = mock_behavior_values()
-
-    grads, loss_info = grad_fn(params=None,observations=None,actions=actions, behaviour_log_probs=behaviour_log_probs, target_values=target_values,advantages=advantages,behavior_values=behavior_values)
-    mapg_trust_region_clipping_loss.on_training_loss_fns 
