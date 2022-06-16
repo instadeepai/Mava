6c59e8f95f8884710f1b8bba6bcb3cf2f50b7dea
AsadJeewa <asad.jeewa@gmail.com>
2022-06-17 00:21:14 +0200

    simple adders tests

--- tests/jax/components/building/adders_test.py
+++ tests/jax/components/building/adders_test.py
@@ -15,13 +15,23 @@
 
 """Tests for config class for Jax-based Mava systems"""
 
-from dataclasses import dataclass, field
+from types import SimpleNamespace
 from typing import Callable, Dict, List, Optional, Tuple
 
 import numpy as np
+from mava.components.jax.executing.base import ExecutorInit
 import pytest
 from acme import specs
-
+from mava.components.jax.building.adders import (
+    ParallelSequenceAdder,
+    ParallelTransitionAdder,
+    ParallelSequenceAdderConfig,
+    ParallelTransitionAdderConfig,
+    ParallelTransitionAdderSignature,
+    ParallelSequenceAdderSignature,
+    UniformAdderPriority,
+)
+from mava.adders import reverb as reverb_adders
 from mava.components.jax import Component, building
 from mava.components.jax.building import adders
 from mava.components.jax.building.base import SystemInit
@@ -32,19 +42,11 @@ from mava.utils.wrapper_utils import parameterized_restart
 from tests.jax.mocks import (
     MockExecutorEnvironmentLoop,
     MockOnPolicyDataServer,
-    MockReverbDistributor,
-    make_fake_environment_factory,
+    MockDataServer,
+    MockDistributor,
     MockLogger,
 )
-
-agents = {"agent_0", "agent_1", "agent_2"}
-obs_first = {agent: np.array([0.0, 1.0]) for agent in agents}
-default_discount = {agent: 1.0 for agent in agents}
-env_restart = parameterized_restart(
-    reward={agent: 0.0 for agent in agents},
-    discount=default_discount,
-    observation=obs_first,
-)
+from mava.systems.jax.builder import Builder
 
 
 def make_fake_env_specs() -> MAEnvironmentSpec:
@@ -53,81 +55,6 @@ def make_fake_env_specs() -> MAEnvironmentSpec:
     Returns:
         _description_
     """
-    agents = ["agent_0", "agent_1"]
-    env_spec = {}
-    for agent in agents:
-        env_spec[agent] = specs.EnvironmentSpec(
-            observations=specs.Array(shape=(10, 5), dtype=np.float32),
-            actions=specs.DiscreteArray(num_values=3),
-            rewards=specs.Array(shape=(), dtype=np.float32),
-            discounts=specs.BoundedArray(
-                shape=(), dtype=np.float32, minimum=0.0, maximum=1.0
-            ),
-        )
-    return MAEnvironmentSpec(
-        environment=None,
-        specs=env_spec,
-        extra_specs={"extras": specs.Array(shape=(), dtype=np.float32)},
-    )
-
-
-@dataclass
-class DistributorDefaultConfig:
-    num_executors: int = 1
-    nodes_on_gpu: List[str] = field(default_factory=list)
-    multi_process: bool = True
-    name: str = "system"
-
-
-class MockDistributor(Component):
-    def __init__(
-        self, config: DistributorDefaultConfig = DistributorDefaultConfig()
-    ) -> None:
-        """Mock system distributor component.
-
-        Args:
-            config : dataclass configuration for setting component hyperparameters
-        """
-        self.config = config
-
-    @staticmethod
-    def name() -> str:
-        """Component type name, e.g. 'dataset' or 'executor'.
-
-        Returns:
-            Component type name
-        """
-        return "distributor"
-
-    @staticmethod
-    def config_class() -> Optional[Callable]:
-        """Config class used for component.
-
-        Returns:
-            config class/dataclass for component.
-        """
-        return DistributorDefaultConfig
-
-
-class TestSystemWithParallelSequenceAdder(System):
-    def design(self) -> Tuple[DesignSpec, Dict]:
-        """Mock system design with zero components.
-
-        Returns:
-            system callback components
-        """
-        components = DesignSpec(
-            environment_spec=EnvironmentSpec,
-            system_init=SystemInit,
-            data_server_adder_signature=adders.ParallelSequenceAdderSignature,
-            executor_adder=building.ParallelSequenceAdder,
-            adder_priority=adders.UniformAdderPriority,
-            data_server=MockOnPolicyDataServer,
-            distributor=MockReverbDistributor,
-            logger=MockLogger,
-            executor_environment_loop=MockExecutorEnvironmentLoop,
-        )
-        return components, {}
 
 
 @pytest.fixture
@@ -136,29 +63,117 @@ def test_system_parallel_sequence_adder() -> System:
     return TestSystemWithParallelSequenceAdder()
 
 
-# TODO Fix test.
-def test_adders(
-    test_system_parallel_sequence_adder: System,
+@pytest.fixture
+def mock_builder() -> Builder:
+    """Mock builder component.
+    Returns:
+        Builder
+    """
+    builder = Builder(components=[])
+    # store
+    adder = "paralleladder"
+    store = SimpleNamespace(
+        table_network_config={"table_0": "network_0"},
+        unique_net_keys=["network_0"],
+        data_server_client=MockDataServer,
+    )
+    builder.store = store
+    return builder
+
+
+@pytest.fixture
+def parallel_sequence_adder() -> ParallelSequenceAdder:
+    """Creates an MAPG loss fixture with trust region and clipping"""
+
+    adder = ParallelSequenceAdder(config=ParallelSequenceAdderConfig())
+
+    return adder
+
+
+@pytest.fixture
+def parallel_transition_adder() -> ParallelTransitionAdder:
+    """Creates an MAPG loss fixture with trust region and clipping"""
+
+    adder = ParallelTransitionAdder(config=ParallelTransitionAdderConfig())
+
+    return adder
+
+
+@pytest.fixture
+def parallel_sequence_adder_signature() -> ParallelSequenceAdderSignature:
+    """Creates an MAPG loss fixture with trust region and clipping"""
+
+    signature = ParallelSequenceAdderSignature()
+
+    return signature
+
+
+@pytest.fixture
+def parallel_transition_adder_signature() -> ParallelTransitionAdderSignature:
+    """Creates an MAPG loss fixture with trust region and clipping"""
+
+    signature = ParallelTransitionAdderSignature()
+
+    return signature
+
+
+@pytest.fixture
+def uniform_priority() -> UniformAdderPriority:
+    """Creates an MAPG loss fixture with trust region and clipping"""
+
+    priority = UniformAdderPriority()
+
+    return priority
+
+
+def test_sequence_adders(
+    mock_builder: Builder,
+    parallel_sequence_adder: ParallelSequenceAdder,
 ) -> None:
-    """Test if system builder instantiates processes as expected."""
-    # TODO Update once rewrite these tests.
-    test_system_parallel_sequence_adder.build(
-        environment_factory=make_fake_environment_factory()
+    parallel_sequence_adder.on_building_init_start(builder=mock_builder)
+    assert (
+        mock_builder.store.sequence_length
+        == parallel_sequence_adder.config.sequence_length
     )
-    test_system_parallel_sequence_adder._builder.data_server()  # to get client
-    # test_system_parallel_sequence_adder._builder.store.system_executor = None
-    test_system_parallel_sequence_adder._builder.executor(  # _builder.store.data_server_client assigned here
-        executor_id="executor",
-        data_server_client=test_system_parallel_sequence_adder._builder.store.system_build[
-            0
-        ],
-        parameter_server_client=None,
-    )
-    test_system_parallel_sequence_adder._builder.store.adder.add_first(env_restart)
-    # TODO test add and sample
-    # TODO test padding
-    # TODO test single_process distributor
-    # test_system_parallel_sequence_adder._builder.store.server.stop()
+    parallel_sequence_adder.on_building_executor_adder(builder=mock_builder)
+    assert type(mock_builder.store.adder) == reverb_adders.ParallelSequenceAdder
 
 
+def test_sequence_adders_signature(
+    mock_builder: Builder,
+    parallel_sequence_adder_signature: ParallelSequenceAdderSignature,
+) -> None:
+    parallel_sequence_adder_signature.on_building_data_server_adder_signature(
+        builder=mock_builder
+    )
+    assert mock_builder.store.adder_signature_fn
+
+
+def test_transition_adders(
+    mock_builder: Builder,
+    parallel_transition_adder: ParallelTransitionAdder,
+) -> None:
+    parallel_transition_adder.on_building_executor_adder(builder=mock_builder)
+    assert type(mock_builder.store.adder) == reverb_adders.ParallelNStepTransitionAdder
+
+
+def test_transition_adders_signature(
+    mock_builder: Builder,
+    parallel_transition_adder_signature: ParallelTransitionAdderSignature,
+) -> None:
+    parallel_transition_adder_signature.on_building_data_server_adder_signature(
+        builder=mock_builder
+    )
+    assert mock_builder.store.adder_signature_fn
+
+
+def test_uniform_priority(
+    mock_builder: Builder,
+    uniform_priority: UniformAdderPriority,
+) -> None:
+    uniform_priority.on_building_executor_adder_priority(builder=mock_builder)
+    assert mock_builder.store.priority_fns
+
+
+# TEST SIGNATURES
 # TODO (Kale-ab): test adder behaviour in more detail
