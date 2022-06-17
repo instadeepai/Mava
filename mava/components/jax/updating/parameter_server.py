b3ea5b28f11379f9aae128351534db791b4f0302
Matthew Morris <m.morris@instadeep.com>
2022-06-17 13:10:36 +0200

    fix: server set+add params do not account for names being string

--- mava/components/jax/updating/parameter_server.py
+++ mava/components/jax/updating/parameter_server.py
@@ -17,7 +17,7 @@
 import abc
 import time
 from dataclasses import dataclass
-from typing import Callable, Optional
+from typing import Any, Callable, Dict, Optional, Sequence, Union
 
 import numpy as np
 from acme.jax import savers
@@ -154,7 +154,7 @@ class DefaultParameterServer(ParameterServer):
     # Get
     def on_parameter_server_get_parameters(self, server: SystemParameterServer) -> None:
         """_summary_"""
-        names = server.store._param_names
+        names: Union[str, Sequence[str]] = server.store._param_names
 
         if type(names) == str:
             get_params = server.store.parameters[names]  # type: ignore
@@ -167,12 +167,8 @@ class DefaultParameterServer(ParameterServer):
     # Set
     def on_parameter_server_set_parameters(self, server: SystemParameterServer) -> None:
         """_summary_"""
-        params = server.store._set_params
+        params: Dict[str, Any] = server.store._set_params
         names = params.keys()
-
-        if type(names) == str:
-            params = {names: params}  # type: ignore
-            names = [names]  # type: ignore
 
         for var_key in names:
             assert var_key in server.store.parameters
@@ -189,12 +185,8 @@ class DefaultParameterServer(ParameterServer):
         self, server: SystemParameterServer
     ) -> None:
         """_summary_"""
-        params = server.store._add_to_params
+        params: Dict[str, Any] = server.store._add_to_params
         names = params.keys()
-
-        if type(names) == str:
-            params = {names: params}  # type: ignore
-            names = [names]  # type: ignore
 
         for var_key in names:
             assert var_key in server.store.parameters
