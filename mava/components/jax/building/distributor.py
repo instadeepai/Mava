997727b256d148bee85b3375db9b6ebc69cf3a8c
Matthew Morris <m.morris@instadeep.com>
2022-06-23 14:42:00 +0200

    feat: distributor dependencies

--- mava/components/jax/building/distributor.py
+++ mava/components/jax/building/distributor.py
@@ -15,9 +15,11 @@
 
 """Commonly used distributor components for system builders"""
 from dataclasses import dataclass
-from typing import Callable, List, Optional, Union
+from typing import Callable, List, Optional, Type, Union
 
+from mava.callbacks import Callback
 from mava.components.jax import Component
+from mava.components.jax.training.trainer import TrainerInit
 from mava.core_jax import SystemBuilder
 from mava.systems.jax.launcher import Launcher, NodeType
 
@@ -121,3 +123,14 @@ class Distributor(Component):
             config class/dataclass for component.
         """
         return DistributorConfig
+
+    @staticmethod
+    def required_components() -> List[Type[Callback]]:
+        """List of other Components required in the system for this Component to function.
+
+        TrainerInit required to set up builder.store.trainer_networks.
+
+        Returns:
+            List of required component classes.
+        """
+        return [TrainerInit]
