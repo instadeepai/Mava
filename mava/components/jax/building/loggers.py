3c4203cc97f2622c9ef4a6564bff8985e349bfee
Matthew Morris <m.morris@instadeep.com>
2022-06-23 15:01:17 +0200

    feat: logger dependencies

--- mava/components/jax/building/loggers.py
+++ mava/components/jax/building/loggers.py
@@ -16,8 +16,9 @@
 """Execution components for system builders"""
 
 from dataclasses import dataclass
-from typing import Any, Callable, Optional
+from typing import Any, Callable, List, Optional, Type
 
+from mava.callbacks import Callback
 from mava.components.jax import Component
 from mava.core_jax import SystemBuilder
 from mava.utils.loggers import MavaLogger
@@ -40,11 +41,13 @@ class Logger(Component):
     def on_building_executor_logger(self, builder: SystemBuilder) -> None:
         """[summary]"""
         logger_config = self.config.logger_config if self.config.logger_config else {}
+        # is_evaluator set by builder
         name = "executor" if not builder.store.is_evaluator else "evaluator"
 
         if self.config.logger_config and name in self.config.logger_config:
             logger_config = self.config.logger_config[name]
 
+        # executor_id set by builder
         builder.store.executor_logger = self.config.logger_factory(  # type: ignore
             builder.store.executor_id, **logger_config
         )
@@ -56,6 +59,7 @@ class Logger(Component):
         if self.config.logger_config and name in self.config.logger_config:
             logger_config = self.config.logger_config[name]
 
+        # trainer_id set by builder
         builder.store.trainer_logger = self.config.logger_factory(  # type: ignore
             builder.store.trainer_id, **logger_config
         )
@@ -73,3 +77,14 @@ class Logger(Component):
             config class/dataclass for component.
         """
         return LoggerConfig
+
+    @staticmethod
+    def required_components() -> List[Type[Callback]]:
+        """List of other Components required in the system for this Component to function.
+
+        None required.
+
+        Returns:
+            List of required component classes.
+        """
+        return []
