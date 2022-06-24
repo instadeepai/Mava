afa12d78dae883f4f8112bd6825515b618b38d63
RuanJohn <ruanjdekock@gmail.com>
2022-06-24 18:42:08 +0200

    fix: remove -v flag from tests.sh

--- bash_scripts/tests.sh
+++ bash_scripts/tests.sh
@@ -56,10 +56,10 @@ AutoROM -v
 N_CPU=$(grep -c ^processor /proc/cpuinfo)
 
 if [ "$integration" = "true" ]; then \
-    # Run all tests, TODO remove -v flag
-    pytest -n "${N_CPU}" tests -v ;
+    # Run all tests
+    pytest -n "${N_CPU}" tests ;
 else
-    # Run all unit tests (non integration tests). TODO remove -v flag
+    # Run all unit tests (non integration tests).
     pytest --durations=10 -n "${N_CPU}" tests --ignore-glob="*/*system_test.py" ;
 fi
 
