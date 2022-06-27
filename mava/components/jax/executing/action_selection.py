8754e225f710ad34c44812e6cf25c36660ea41f8
Matthew Morris <m.morris@instadeep.com>
2022-06-27 12:01:44 +0200

    feat: action selection dependencies

--- mava/components/jax/executing/action_selection.py
+++ mava/components/jax/executing/action_selection.py
@@ -17,11 +17,16 @@
 
 import abc
 from dataclasses import dataclass
+from typing import List, Type
 
 import jax
 from acme.jax import utils
 
+from mava.callbacks import Callback
 from mava.components.jax import Component
+from mava.components.jax.building.networks import Networks
+from mava.components.jax.building.system_init import BaseSystemInit
+from mava.components.jax.training.trainer import TrainerInit
 from mava.core_jax import SystemExecutor
 
 
@@ -64,6 +69,19 @@ class ExecutorSelectAction(Component):
         """
         return "executor_select_action"
 
+    @staticmethod
+    def required_components() -> List[Type[Callback]]:
+        """List of other Components required in the system for this Component to function.
+
+        TrainerInit required to set up executor.store.networks.
+        BaseSystemInit required to set up executor.store.agent_net_keys.
+        Networks required to set up executor.store.key.
+
+        Returns:
+            List of required component classes.
+        """
+        return [TrainerInit, BaseSystemInit, Networks]
+
 
 class FeedforwardExecutorSelectAction(ExecutorSelectAction):
     def __init__(
@@ -82,6 +100,7 @@ class FeedforwardExecutorSelectAction(ExecutorSelectAction):
         """Summary"""
         executor.store.actions_info = {}
         executor.store.policies_info = {}
+        # executor.store.observations set by Executor
         for agent, observation in executor.store.observations.items():
             action_info, policy_info = executor.select_action(agent, observation)
             executor.store.actions_info[agent] = action_info
@@ -91,11 +110,12 @@ class FeedforwardExecutorSelectAction(ExecutorSelectAction):
     def on_execution_select_action_compute(self, executor: SystemExecutor) -> None:
         """Summary"""
 
-        agent = executor.store.agent
+        agent = executor.store.agent  # Set by Executor
         network = executor.store.networks["networks"][
             executor.store.agent_net_keys[agent]
         ]
 
+        # executor.store.observation set by Executor
         observation = utils.add_batch_dim(executor.store.observation.observation)
         rng_key, executor.store.key = jax.random.split(executor.store.key)
 
