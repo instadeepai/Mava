6448926607b8c2a774e9f7940afd4e97ea9dfcdf
OmaymaMahjoub <omayma.mahjoub@ensi-uma.tn>
2022-06-27 13:25:17 +0100

    test: unit test of all components and methods within model_updating.py

--- tests/jax/components/training/model_updating_test.py
+++ tests/jax/components/training/model_updating_test.py
@@ -1,5 +1,22 @@
+# python3
+# Copyright 2021 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for MAPGMinibatchUpdate and MAPGEpochUpdate class for Jax-based Mava systems"""
+
 from types import SimpleNamespace
-from typing import Any, Dict, Tuple
+from typing import Any, Callable, Dict, Tuple
 
 import jax
 import jax.numpy as jnp
@@ -15,29 +32,45 @@ from mava.systems.jax.trainer import Trainer
 from mava.types import OLT
 
 
-def grad_fn(
-    params,
-    observations,
-    actions,
-    behavior_log_probs,
-    target_values,
-    advantages,
-    behavior_values,
-) -> Tuple:
+def fake_grad_fn(
+    params: Any,
+    observations: Any,
+    actions: Dict[str, jnp.ndarray],
+    behaviour_log_probs: Dict[str, jnp.ndarray],
+    target_values: Dict[str, jnp.ndarray],
+    advantages: Dict[str, jnp.ndarray],
+    behavior_values: Dict[str, jnp.ndarray],
+) -> Tuple[Dict, Dict]:
+    """fake grad function to be used in MockTrainer
+    Args:
+        params
+        observations
+        actions
+        behaviour_log_probs
+        target_values
+        advantages
+        behavior_values
+    Returns:
+        gradient: fake gradient
+        agent_metrics: fake metrics dictionary
+    """
     gradient = {
         "agent_0": jnp.array([5.0, 5.0, 5.0]),
         "agent_1": jnp.array([5.0, 5.0, 5.0]),
         "agent_2": jnp.array([5.0, 5.0, 5.0]),
     }
-    agent_metrics = {}
-    for agent_key, value in actions.items():
+
+    agent_metrics: Dict[str, Any] = {}
+    for agent_key in actions.keys():
         agent_metrics[agent_key] = {}
 
     return (gradient, agent_metrics)
 
 
 class MockTrainer(Trainer):
-    def __init__(self):
+    """Mock trainer component"""
+
+    def __init__(self) -> None:
         networks = {
             "networks": {
                 "network_agent_0": SimpleNamespace(params=jnp.array([0.0, 0.0, 0.0])),
@@ -53,7 +86,7 @@ class MockTrainer(Trainer):
         }
         self.store = SimpleNamespace(
             networks=networks,
-            grad_fn=grad_fn,
+            grad_fn=fake_grad_fn,
             trainer_agents=trainer_agents,
             trainer_agent_net_keys=trainer_agent_net_keys,
             full_batch_size=2,
@@ -62,34 +95,35 @@ class MockTrainer(Trainer):
 
 @pytest.fixture
 def mock_trainer() -> MockTrainer:
+    """Create mock trainer component"""
     return MockTrainer()
 
 
 class MockOptimizer:
+    """Mock optimizer configuration"""
+
     def __init__(self) -> None:
         self.test = "Done"
         pass
 
-    def init(self, params) -> str:
-        return str(params)
+    def init(self, params: Dict[str, Any]) -> list:
+        return list(params)
 
-    def update(self, gradient, opt_states):
+    def update(
+        self, gradient: Dict[str, Any], opt_states: Dict[str, Any]
+    ) -> Tuple[Dict, str]:
         return (gradient, "opt_states_after_update")
 
 
 @pytest.fixture
 def mock_optimizer() -> MockOptimizer:
+    """Create mock optimizer"""
     return MockOptimizer()
 
-def mock_minibatch_update(params, opt_states):
-    return (params, opt_states)
-
-@pytest.fixture
-def mock_minibatch_update_fn():
-    return mock_minibatch_update
 
 @pytest.fixture
 def fake_batch() -> Batch:
+    """Fake batch"""
     batch = Batch(
         observations={
             "agent_0": OLT(
@@ -122,9 +156,18 @@ def fake_batch() -> Batch:
 
 
 @pytest.fixture
-def fake_state(
+def fake_state_and_trainer(
     mock_trainer: MockTrainer, fake_batch: Batch, mock_optimizer: MockOptimizer
 ) -> Any:
+    """fake state dictionary and mock trainer component
+    Args:
+        mock_trainer: mock trainer
+        fake_batch: fake batch
+        mock_optimizer: mock optimizer
+    Returns:
+        state dictionary: include random_key, params, opt_states and batch
+        mock_trainer: mock trainer
+    """
     mini_batch_update = MAPGMinibatchUpdate()
     mini_batch_update.config.optimizer = mock_optimizer
     mini_batch_update.on_training_utility_fns(trainer=mock_trainer)
@@ -154,9 +197,46 @@ def fake_state(
     )
 
 
+def mock_minibatch_update(carry: Tuple[Any, Any], minibatches: Any) -> Tuple[Any, Any]:
+    """mock minibatch update to test model_update_epoch in MAPGEpochUpdate class
+    Args:
+        carry: tuple include params and opt_states
+        minibatches
+    Returns:
+        carry:same carry
+        metrics: to test metrics value in test of model_update_epoch
+    """
+    metrics = {
+        "agent_0": {
+            "norm_updates": jnp.array([2, 2, 2]),
+            "norm_grad": jnp.array([2, 2, 2]),
+        },
+        "agent_1": {
+            "norm_updates": jnp.array([2, 2, 2]),
+            "norm_grad": jnp.array([2, 2, 2]),
+        },
+        "agent_2": {
+            "norm_updates": jnp.array([2, 2, 2]),
+            "norm_grad": jnp.array([2, 2, 2]),
+        },
+    }
+    return carry, metrics
+
+
+@pytest.fixture
+def mock_minibatch_update_fn() -> Callable:
+    """create mock minibatch_update function"""
+    return mock_minibatch_update
+
+
 def test_on_training_utility_fns_empty_config_optimizer(
     mock_trainer: MockTrainer,
 ) -> None:
+    """Test on_training_utility_fns from MAPGMinibatchUpdate with MAPGMinibatchUpdateConfig does not include optimizer
+
+    Args:
+        mock_trainer: Trainer
+    """
     mini_batch_update = MAPGMinibatchUpdate()
     mini_batch_update.config.optimizer = None
     mini_batch_update.on_training_utility_fns(trainer=mock_trainer)
@@ -169,7 +249,6 @@ def test_on_training_utility_fns_empty_config_optimizer(
         "network_agent_1",
         "network_agent_2",
     ]
-
     for net_key in mock_trainer.store.networks["networks"].keys():
         assert mock_trainer.store.opt_states[net_key][0] == optax.EmptyState()
         assert isinstance(
@@ -195,6 +274,12 @@ def test_on_training_utility_fns_empty_config_optimizer(
 def test_on_training_utility_fns(
     mock_trainer: MockTrainer, mock_optimizer: MockOptimizer
 ) -> None:
+    """Test on_training_utility_fns from MAPGMinibatchUpdate
+
+    Args:
+        mock_trainer: Trainer
+        mock_optimizer: Optimizer
+    """
     mini_batch_update = MAPGMinibatchUpdate()
     mini_batch_update.config.optimizer = mock_optimizer
     mini_batch_update.on_training_utility_fns(trainer=mock_trainer)
@@ -202,36 +287,42 @@ def test_on_training_utility_fns(
     assert mock_trainer.store.optimizer.test == "Done"
 
     for net_key in mock_trainer.store.networks["networks"].keys():
-        assert mock_trainer.store.opt_states[net_key] == str(
+        assert mock_trainer.store.opt_states[net_key] == list(
             mock_trainer.store.networks["networks"][net_key].params
         )
 
     assert mock_trainer.store.minibatch_update_fn
 
 
-def test_minibatch_update_fn(fake_state: Any) -> None:
-    state = fake_state[0]
-    mock_trainer = fake_state[1]
-    carry = [state["params"], state["opt_states"]]
+def test_minibatch_update_fn(
+    fake_state_and_trainer: Tuple[Dict[str, Any], MockTrainer]
+) -> None:
+    """Test on_minibatch_update_fn
 
+    Args:
+        fake_state_and_trainer: tuple include fake state and mock trainer
+    """
+    state = fake_state_and_trainer[0]
+    mock_trainer = fake_state_and_trainer[1]
+    carry = [state["params"], state["opt_states"]]
     (
-        state["params"],
-        state["opt_state"],
+        new_params,
+        new_opt_states,
     ), metrics = mock_trainer.store.minibatch_update_fn(
         carry=carry, minibatch=state["batch"]
     )
 
-    assert list(state["params"].keys()) == [
+    assert list(new_params.keys()) == [
         "network_agent_0",
         "network_agent_1",
         "network_agent_2",
     ]
-    assert list(state["params"]["network_agent_0"]) == [5.0, 5.0, 5.0]
-    assert list(state["params"]["network_agent_1"]) == [6.0, 6.0, 6.0]
-    assert list(state["params"]["network_agent_2"]) == [7.0, 7.0, 7.0]
+    assert list(new_params["network_agent_0"]) == [5.0, 5.0, 5.0]
+    assert list(new_params["network_agent_1"]) == [6.0, 6.0, 6.0]
+    assert list(new_params["network_agent_2"]) == [7.0, 7.0, 7.0]
 
-    for net_key in state["opt_state"].keys():
-        assert state["opt_state"][net_key] == "opt_states_after_update"
+    for net_key in new_opt_states.keys():
+        assert new_opt_states[net_key] == "opt_states_after_update"
 
     assert list(metrics.keys()) == ["agent_0", "agent_1", "agent_2"]
     for agent in metrics.keys():
@@ -245,6 +336,11 @@ def test_minibatch_update_fn(fake_state: Any) -> None:
 
 
 def test_on_training_utility_fns_epoch(mock_trainer: MockTrainer) -> None:
+    """Test on_training_utility_fns from MAPGEpochUpdate
+
+    Args:
+        mock_trainer: trainer
+    """
     mini_epoch_update = MAPGEpochUpdate()
     mini_epoch_update.on_training_utility_fns(trainer=mock_trainer)
 
@@ -255,13 +351,44 @@ def test_on_training_utility_fns_epoch(mock_trainer: MockTrainer) -> None:
     assert mock_trainer.store.epoch_update_fn
 
 
-def test_epoch_update_fn(fake_state:Any, mock_minibatch_update_fn: Any)->None:
-    mock_trainer= fake_state[1]
-    mini_epoch_update= MAPGEpochUpdate()
+def test_epoch_update_fn(
+    fake_state_and_trainer: Tuple[Dict[str, Any], MockTrainer],
+    mock_minibatch_update_fn: Callable,
+) -> None:
+    """Test epoch_update_fn function
+
+    Args:
+        fake_state_and_trainer: tuple include fake state and mock trainer
+        mock_minibatch_update_fn
+    """
+    mock_trainer = fake_state_and_trainer[1]
+    mini_epoch_update = MAPGEpochUpdate()
     mini_epoch_update.on_training_utility_fns(trainer=mock_trainer)
-    mock_trainer.store.minibatch_update_fn=mock_minibatch_update_fn
-    state= fake_state[0]
-    state["params"]=jnp.array([0,0,0])
-    state["opt_states"]=jnp.array([0,0,0])
-    carry= [state["random_key"], state["params"], state["opt_states"], state["batch"]]
-    mock_trainer.store.epoch_update_fn(carry=carry, unused_t=None)
+    mock_trainer.store.minibatch_update_fn = mock_minibatch_update_fn
+
+    state = fake_state_and_trainer[0]
+    # update params and opt_state to function in jax.lax.scan function
+    state["params"] = jnp.array([0, 0, 0])
+    state["opt_states"] = jnp.array([1, 1, 1])
+
+    carry = [state["random_key"], state["params"], state["opt_states"], state["batch"]]
+    (
+        new_key,
+        new_params,
+        new_opt_states,
+        batch,
+    ), metrics = mock_trainer.store.epoch_update_fn(carry=carry, unused_t=None)
+
+    assert list(new_key) == list(jax.random.split(state["random_key"])[0])
+
+    assert list(new_params) == [0, 0, 0]
+
+    assert list(new_opt_states) == [1, 1, 1]
+
+    assert batch == state["batch"]
+
+    assert list(metrics.keys()) == ["agent_0", "agent_1", "agent_2"]
+    for agent in metrics.keys():
+        assert list(metrics[agent].keys()) == ["norm_grad", "norm_updates"]
+        assert list(metrics[agent]["norm_grad"][0]) == [2, 2, 2]
+        assert list(metrics[agent]["norm_updates"][0]) == [2, 2, 2]
