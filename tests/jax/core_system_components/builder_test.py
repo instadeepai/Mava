7d88b544de796e950544c2f2befade4f68f7a7ee
Matthew Morris <m.morris@instadeep.com>
2022-06-30 15:50:18 +0200

    test: executor_store_when_is_evaluator

--- tests/jax/core_system_components/builder_test.py
+++ tests/jax/core_system_components/builder_test.py
@@ -4,7 +4,7 @@ from typing import List
 import pytest
 
 from mava.callbacks import Callback
-from mava.systems.jax import Builder, ParameterServer
+from mava.systems.jax import Builder, Executor, ParameterServer
 from tests.jax.hook_order_tracking import HookOrderTracking
 
 
@@ -19,6 +19,8 @@ class TestBuilder(HookOrderTracking, Builder):
 
         super().__init__(components=components, global_config=global_config)
         self.store.data_tables = ["data_table_1", "data_table_2"]
+        self.store.system_executor = "system_executor"
+        self.store.adder = "adder"
 
 
 @pytest.fixture
@@ -45,3 +47,27 @@ def test_parameter_server_store(test_builder: TestBuilder) -> None:
     parameter_server = test_builder.parameter_server()
     assert isinstance(parameter_server, ParameterServer)
     assert parameter_server.store == test_builder.store
+
+
+def test_executor_store_when_is_evaluator(test_builder: TestBuilder) -> None:
+    """Test that store is handled correctly in executor() when it's an evaluator."""
+    executor_id = "evaluator"
+    data_server_client = "data_server_client"
+    parameter_server_client = "parameter_server_client"
+    assert (
+        test_builder.executor(
+            executor_id=executor_id,
+            data_server_client=data_server_client,
+            parameter_server_client=parameter_server_client,
+        )
+        == "system_executor"
+    )
+
+    assert test_builder.store.executor_id == executor_id
+    assert test_builder.store.data_server_client == data_server_client
+    assert test_builder.store.parameter_server_client == parameter_server_client
+    assert test_builder.store.is_evaluator
+    assert test_builder.store.adder is None
+
+    assert isinstance(test_builder.store.executor, Executor)
+    assert test_builder.store.executor.store == test_builder.store
