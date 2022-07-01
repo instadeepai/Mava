ab34836b62f84b0b9321dac6b87067cfe3546798
AsadJeewa <asad.jeewa@gmail.com>
2022-07-01 10:33:00 +0200

    remove comment

--- tests/jax/components/building/adders_test.py
+++ tests/jax/components/building/adders_test.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for config class for Jax-based Mava systems"""
-
 from types import SimpleNamespace
 
 import pytest
