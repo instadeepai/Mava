eafe4e1319b836e2697223bf4fde437d0afe0778
Matthew Morris <m.morris@instadeep.com>
2022-07-07 14:57:01 +0200

    refactor: copyright. Better param value for trainer_id

--- tests/jax/core_system_components/builder_test.py
+++ tests/jax/core_system_components/builder_test.py
@@ -1,3 +1,18 @@
+# python3
+# Copyright 2022 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from types import SimpleNamespace
 from typing import List
 
@@ -104,7 +119,7 @@ def test_executor_store_when_executor(test_builder: TestBuilder) -> None:
 
 def test_trainer_store(test_builder: TestBuilder) -> None:
     """Test that store is handled correctly in trainer()."""
-    trainer_id = "executor"
+    trainer_id = "trainer"
     data_server_client = "data_server_client"
     parameter_server_client = "parameter_server_client"
 
