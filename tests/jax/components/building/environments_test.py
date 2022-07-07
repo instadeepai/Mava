27fe4b20b37e81cf85fce72bbd0e79ea3816e39d
Matthew Morris <m.morris@instadeep.com>
2022-07-07 12:09:30 +0200

    test: abstract executor environment loop class

--- tests/jax/components/building/environments_test.py
+++ tests/jax/components/building/environments_test.py
@@ -1,4 +1,5 @@
 import functools
+from types import SimpleNamespace
 
 import numpy as np
 import pytest
@@ -7,11 +8,21 @@ from mava import MAEnvironmentSpec, specs
 from mava.components.jax.building.environments import (
     EnvironmentSpec,
     EnvironmentSpecConfig,
+    ExecutorEnvironmentLoop,
+    ExecutorEnvironmentLoopConfig,
 )
 from mava.core_jax import SystemBuilder
 from mava.systems.jax import Builder
 from mava.utils.environments import debugging_utils
 from mava.utils.sort_utils import sort_str_num
+
+
+class AbstractExecutorEnvironmentLoop(ExecutorEnvironmentLoop):
+    """Implement abstract methods to allow testing of class"""
+
+    def on_building_executor_environment_loop(self, builder: SystemBuilder) -> None:
+        """Do nothing. Just implement abstract method"""
+        pass
 
 
 @pytest.fixture
@@ -30,9 +41,21 @@ def test_environment_spec() -> EnvironmentSpec:
 
 
 @pytest.fixture
+def test_executor_environment_loop() -> ExecutorEnvironmentLoop:
+    """Pytest fixture for executor environment loop"""
+    config = ExecutorEnvironmentLoopConfig(should_update=False)
+    return AbstractExecutorEnvironmentLoop(config=config)
+
+
+@pytest.fixture
 def test_builder() -> SystemBuilder:
     """Pytest fixture for system builder."""
-    system_builder = Builder(components=[])
+
+    def environment_factory(evaluation: bool) -> str:
+        return "environment_eval_" + ("true" if evaluation else "false")
+
+    global_config = SimpleNamespace(environment_factory=environment_factory)
+    system_builder = Builder(components=[], global_config=global_config)
     return system_builder
 
 
@@ -93,3 +116,22 @@ class TestEnvironmentSpec:
 
         # Extras spec created
         assert test_builder.store.extras_spec == {}
+
+
+class TestExecutorEnvironmentLoop:
+    """Tests for abstract ExecutorEnvironmentLoop"""
+
+    def test_init(
+        self, test_executor_environment_loop: ExecutorEnvironmentLoop
+    ) -> None:
+        """Test that class loads config properly"""
+        assert not test_executor_environment_loop.config.should_update
+
+    def test_on_building_executor_environment(
+        self,
+        test_executor_environment_loop: ExecutorEnvironmentLoop,
+        test_builder: SystemBuilder,
+    ) -> None:
+        """Test by manually calling the hook and checking the store"""
+        test_executor_environment_loop.on_building_executor_environment(test_builder)
+        assert test_builder.store.executor_environment == "environment_eval_false"
