95af2b6c2e6be7bd08041e02b0f1a8288190adbe
Siddarth <s.singh@instadeep.com>
2022-07-17 20:18:56 +0200

    first commit

--- examples/jax/debugging/simple_spread/feedforward/decentralised/run_mappo.py
+++ examples/jax/debugging/simple_spread/feedforward/decentralised/run_mappo.py
@@ -75,7 +75,7 @@ def main(_: Any) -> None:
     logger_factory = functools.partial(
         logger_utils.make_logger,
         directory=FLAGS.base_dir,
-        to_terminal=True,
+        to_terminal=False,
         to_tensorboard=True,
         time_stamp=FLAGS.mava_id,
         time_delta=log_every,
--- mava/systems/jax/mappo/networks.py
+++ mava/systems/jax/mappo/networks.py
@@ -22,19 +22,61 @@ import haiku as hk  # type: ignore
 import jax
 import jax.numpy as jnp
 import numpy as np
+import tensorflow_probability as tfp
 from acme import specs
 from acme.jax import networks as networks_lib
 from acme.jax import utils
+from acme.jax.networks.rescaling import TanhToSpec
 from dm_env import specs as dm_specs
 from jax import jit
 
 from mava import specs as mava_specs
 from mava.utils.jax_training_utils import action_mask_categorical_policies
 
+tfd = tfp.distributions
 Array = dm_specs.Array
 BoundedArray = dm_specs.BoundedArray
 DiscreteArray = dm_specs.DiscreteArray
 EntropyFn = Callable[[Any], jnp.ndarray]
+
+
+class ClippedGaussianDistribution:
+    def __init__(self, guassian_dist: Any, action_specs: Any):
+        self._guassian_dist = guassian_dist
+        self.clip_fn = TanhToSpec(action_specs)
+
+    def entropy(self):
+        # Note (dries): This calculates the approximate entropy of the
+        # clipped Gaussian distribution by setting it to the
+        # unclipped guassian entropy.
+        return self._guassian_dist.entropy()
+
+    def sample(self, seed):
+        # print(self._guassian_dist)
+        # return self.clip_fn(self._guassian_dist).sample(seed=seed)
+        return self._guassian_dist.sample(seed=seed)
+
+    def log_prob(self, action):
+        # return self.clip_fn(self._guassian_dist).log_prob(action)
+        return self._guassian_dist.log_prob(action)
+
+    def batch_reshape(self, dims, name: str = "reshaped") -> None:
+        self._guassian_dist = tfd.BatchReshape(
+            self._guassian_dist, batch_shape=dims, name=name
+        )
+
+
+class ClippedGaussianHead(hk.Module):
+    def __init__(
+        self,
+        action_specs: Any,
+        name: Optional[str] = None,
+    ):
+        super().__init__(name=name)
+        self._action_specs = action_specs
+
+    def __call__(self, x: Any) -> ClippedGaussianDistribution:
+        return ClippedGaussianDistribution(x, action_specs=self._action_specs)
 
 
 @dataclasses.dataclass
@@ -56,7 +98,7 @@ class PPONetworks:
         self.entropy = entropy
         self.sample = sample
 
-        @jit
+        # @jit
         def forward_fn(
             params: Dict[str, jnp.ndarray],
             observations: networks_lib.Observation,
@@ -67,12 +109,14 @@ class PPONetworks:
             # The parameters of the network might change. So it has to
             # be fed into the jitted function.
             distribution, _ = self.network.apply(params, observations)
-            if mask is not None:
-                distribution = action_mask_categorical_policies(distribution, mask)
-
+            # if mask is not None:
+            # distribution = action_mask_categorical_policies(distribution, mask)
+            # print(distribution)
+            # exit()
             actions = jax.numpy.squeeze(distribution.sample(seed=key))
+            print(actions)
             log_prob = distribution.log_prob(actions)
-
+            # print(log_prob)
             return actions, log_prob
 
         self.forward_fn = forward_fn
@@ -85,7 +129,9 @@ class PPONetworks:
     ) -> Tuple[np.ndarray, Dict]:
         """TODO: Add description here."""
         actions, log_prob = self.forward_fn(self.params, observations, key, mask)
-        actions = np.array(actions, dtype=np.int64)
+        # actions = np.array(actions, dtype=np.int64)
+        # Continuous actions a re floats
+        actions = np.array(actions, dtype=np.float64)
         log_prob = np.squeeze(np.array(log_prob, dtype=np.float32))
         return actions, {"log_prob": log_prob}
 
@@ -130,10 +176,12 @@ def make_networks(
         )
 
     else:
-        raise NotImplementedError(
-            "Continuous networks not implemented yet."
-            + "See: https://github.com/deepmind/acme/blob/"
-            + "master/acme/agents/jax/ppo/networks.py"
+        return make_continuous_networks(
+            environment_spec=spec,
+            key=key,
+            policy_layer_sizes=policy_layer_sizes,
+            critic_layer_sizes=critic_layer_sizes,
+            observation_network=observation_network,
         )
 
 
@@ -162,6 +210,61 @@ def make_discrete_networks(
             ]
         )
         return policy_value_network(inputs)
+
+    # Transform into pure functions.
+    forward_fn = hk.without_apply_rng(hk.transform(forward_fn))
+
+    dummy_obs = utils.zeros_like(environment_spec.observations.observation)
+    dummy_obs = utils.add_batch_dim(dummy_obs)  # Dummy 'sequence' dim.
+
+    network_key, key = jax.random.split(key)
+    params = forward_fn.init(network_key, dummy_obs)  # type: ignore
+
+    # Create PPONetworks to add functionality required by the agent.
+    return make_ppo_network(network=forward_fn, params=params)
+
+
+def make_continuous_networks(
+    environment_spec: specs.EnvironmentSpec,
+    key: networks_lib.PRNGKey,
+    policy_layer_sizes: Sequence[int],
+    critic_layer_sizes: Sequence[int],
+    observation_network: Callable = utils.batch_concat,
+    # default behaviour is to flatten observations
+) -> PPONetworks:
+    """TODO: Add description here."""
+    specs = environment_spec.actions
+    # agent_environment_specs
+    # Get total number of action dimensions from action spec.
+    num_dimensions = np.prod(environment_spec.actions.shape, dtype=int)
+
+    def forward_fn(inputs: jnp.ndarray) -> networks_lib.FeedForwardNetwork:
+        policy_network = hk.Sequential(
+            [
+                utils.batch_concat,
+                observation_network,
+                hk.nets.MLP(policy_layer_sizes, activation=jax.nn.relu),
+                networks_lib.MultivariateNormalDiagHead(num_dimensions),
+                ClippedGaussianHead(specs),
+            ]
+        )
+
+        value_network = hk.Sequential(
+            [
+                utils.batch_concat,
+                observation_network,
+                hk.nets.MLP(critic_layer_sizes, activate_final=True),
+                hk.Linear(1),
+                lambda x: jnp.squeeze(x, axis=-1),
+            ]
+        )
+
+        action_distribution = policy_network(inputs)
+        value = value_network(inputs)
+        # print("*****ACTION*****")
+        # print(action_distribution)
+        # exit()
+        return (action_distribution, value)
 
     # Transform into pure functions.
     forward_fn = hk.without_apply_rng(hk.transform(forward_fn))
