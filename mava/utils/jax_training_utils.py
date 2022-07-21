c49e6c26a34492cbdd21bd69622c29659077a99f
Siddarth <s.singh@instadeep.com>
2022-07-21 13:33:26 +0200

    fix: linting errors

--- mava/utils/jax_training_utils.py
+++ mava/utils/jax_training_utils.py
@@ -1,11 +1,13 @@
-import jax.numpy as jnp
-import tensorflow_probability.substrates.jax.distributions as tfd 
-import tensorflow_probability.substrates.jax.bijectors as tfb
-from chex import Array
-from acme import specs
-import tensorflow as tf
-from typing import Union
 import dataclasses
+from typing import Union
+
+import jax.numpy as jnp
+import tensorflow as tf
+import tensorflow_probability.substrates.jax.bijectors as tfb
+import tensorflow_probability.substrates.jax.distributions as tfd
+from acme import specs
+from chex import Array
+
 
 def action_mask_categorical_policies(
     distribution: tfd.Categorical, mask: Array
@@ -18,15 +20,20 @@ def action_mask_categorical_policies(
     )
     return tfd.Categorical(logits=masked_logits)
 
+
 @dataclasses.dataclass
 class TanhToSpec:
-  """Squashes real-valued inputs to match a BoundedArraySpec."""
-  spec: specs.BoundedArray
+    """Squashes real-valued inputs to match a BoundedArraySpec."""
 
-  def __call__(self, inputs: Union[tf.Tensor, tfd.Distribution]) -> Union[tf.Tensor, tfd.Distribution]:
-    scale = self.spec.maximum - self.spec.minimum
-    offset = self.spec.minimum
-    inputs = tfb.Tanh()(inputs)
-    inputs = tfb.ScaleMatvecDiag(0.5 * scale)(inputs)
-    output = tfb.Shift(offset + 0.5 * scale)(inputs)
-    return output
+    spec: specs.BoundedArray
+
+    def __call__(
+        self, inputs: Union[tf.Tensor, tfd.Distribution]
+    ) -> Union[tf.Tensor, tfd.Distribution]:
+        """Call for TanhToSpec."""
+        scale = self.spec.maximum - self.spec.minimum
+        offset = self.spec.minimum
+        inputs = tfb.Tanh()(inputs)
+        inputs = tfb.ScaleMatvecDiag(0.5 * scale)(inputs)
+        output = tfb.Shift(offset + 0.5 * scale)(inputs)
+        return output
