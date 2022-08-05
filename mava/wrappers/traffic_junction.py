edff35d668dbfbaa2ef228e143ad90b8a032df63
Matthew Morris <m.morris@instadeep.com>
2022-08-05 12:34:32 +0200

    feat(tj): reset wrapper

--- mava/wrappers/traffic_junction.py
+++ mava/wrappers/traffic_junction.py
@@ -1,9 +1,10 @@
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Union
 
 import dm_env
 import numpy as np
 
 from mava.utils.debugging.environments.traffic_junction import TrafficJunctionEnv
+from mava.utils.wrapper_utils import convert_np_type, parameterized_restart
 from mava.wrappers import PettingZooParallelEnvWrapper
 
 
@@ -11,6 +12,7 @@ class TrafficJunctionWrapper(PettingZooParallelEnvWrapper):
     """Environment wrapper for Traffic Junction MARL environment."""
 
     _step_type: dm_env.StepType  # For tracking step type during step
+    _discounts: Dict[str, Union[int, float]]  # Agent discounts
 
     def __init__(self, environment: TrafficJunctionEnv):
         super().__init__(environment=environment)
@@ -32,6 +34,17 @@ class TrafficJunctionWrapper(PettingZooParallelEnvWrapper):
             self.environment.action_spaces[agent_id] = environment.action_space
             self.environment.observation_spaces[agent_id] = environment.observation_space
 
+        # Compute discounts
+        discount_spec = self.discount_spec()
+        self._discounts = {
+            agent: convert_np_type(discount_spec[agent].dtype, 1)
+            for agent in self._environment.possible_agents
+        }
+
+        # Track number of episodes in env
+        self.n_episodes = 0
+
+        # Reset the env
         self.reset()
 
     def step(self, actions: Dict[str, np.ndarray]) -> Tuple[dm_env.TimeStep, np.ndarray]:
@@ -41,11 +54,12 @@ class TrafficJunctionWrapper(PettingZooParallelEnvWrapper):
             self.reset()
 
         observations, rewards, episode_over, extras = self._environment.step(actions)
-        dones = {agent_id: False for agent_id in self.agent_ids}  # No agents are ever fully done
-        communication_graph = extras['env_graph']
 
-        if observations:
-            observations = self._convert_observations(observations, dones)
+        dones = {agent_id: False for agent_id in self.agent_ids}  # No agents are ever fully done
+        observations = self._observation_dict_from_tuple(observations)
+        observations = self._convert_observations(observations, dones)
+
+        communication_graph = extras['env_graph']
 
         if episode_over:
             self._step_type = dm_env.StepType.LAST
@@ -67,3 +81,25 @@ class TrafficJunctionWrapper(PettingZooParallelEnvWrapper):
         """Resets the episode."""
         self._reset_next_step = False
         self._step_type = dm_env.StepType.FIRST
+        self.n_episodes += 1
+
+        observations, communication_graph = self.environment.reset(epoch=self.n_episodes)
+        observations = self._observation_dict_from_tuple(observations)
+        observations = self._convert_observations(
+            observations, {agent_id: False for agent_id in self.agent_ids}
+        )
+
+        rewards_spec = self.reward_spec()
+        rewards = {
+            agent: convert_np_type(rewards_spec[agent].dtype, 0)
+            for agent in self.possible_agents
+        }
+
+        return (
+            parameterized_restart(rewards, self._discounts, observations),
+            communication_graph,
+        )
+
+    def _observation_dict_from_tuple(self, observation_tuple):
+        return {agent_id: observation_tuple[self.agent_ids.index(agent_id)]
+                for agent_id in self.agent_ids}
