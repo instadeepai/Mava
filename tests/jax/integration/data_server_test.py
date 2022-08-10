b6a5d773b2594829de172dfb5aa76aac795fced5
OmaymaMahjoub <omayma.mahjoub@ensi-uma.tn>
2022-08-10 13:36:01 +0100

    test: integration test of the Data Server

--- tests/jax/integration/data_server_test.py
+++ tests/jax/integration/data_server_test.py
@@ -1,102 +1,197 @@
-# python3
-# Copyright 2021 InstaDeep Ltd. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import functools
+from datetime import datetime
+from typing import Any
 
-"""Tests for config class for Jax-based Mava systems"""
-from typing import Any, Dict
-
+import jax.numpy as jnp
+import launchpad as lp
+import optax
 import pytest
-from reverb import item_selectors, pybind, reverb_types
+import reverb
+from tensorflow.python.data.ops import dataset_ops
 
-from mava.utils import enums
-from tests.jax.mocks import return_test_system
-from tests.jax.utils import assert_if_value_is_not_none
-
-from .data_server_test_data import transition_adder_data_server_test_cases
+from mava.systems.jax import System, mappo
+from mava.utils.environments import debugging_utils
+from mava.utils.loggers import logger_utils
 
 
-@pytest.mark.parametrize(
-    "components",
-    transition_adder_data_server_test_cases,
-)
-class TestDataServer:
-    # Adapted from
-    # https://github.com/deepmind/reverb/blob/c5ea7c37118d0de4ff2320bf5519ba79ad1d4284/reverb/server_test.py#L102
-    def _check_selector_proto(
-        self, expected_selector: reverb_types.SelectorType, proto_msg: Any
-    ) -> None:
-        if isinstance(expected_selector, item_selectors.Uniform):
-            assert proto_msg.HasField("uniform")
-        elif isinstance(expected_selector, item_selectors.Prioritized):
-            assert proto_msg.HasField("prioritized")
-        elif isinstance(expected_selector, pybind.HeapSelector):
-            assert proto_msg.HasField("heap")
-        elif isinstance(expected_selector, item_selectors.Fifo):
-            assert proto_msg.HasField("fifo")
-        elif isinstance(expected_selector, item_selectors.Lifo):
-            assert proto_msg.HasField("lifo")
-        else:
-            raise ValueError(f"Unknown selector: {expected_selector}")
+@pytest.fixture
+def test_system() -> System:
+    """Mappo system"""
+    return mappo.MAPPOSystem()
 
-    def test_data_server(
-        self,
-        components: Dict[str, Dict],
-    ) -> None:
-        """Test if system builder instantiates processes as expected."""
-        # Get specified component
-        test_system = return_test_system(components["component"])
-        system_config = components["system_config"]
-        test_system.build(**system_config)
 
-        # Manually add needed parameter to global config
-        test_system._builder.store.global_config.network_sampling_setup_type = (
-            enums.NetworkSampler.fixed_agent_networks
+def get_dataset(data_server: reverb.client.Client) -> dataset_ops.DatasetV1Adapter:
+    """Batches 2 sequences to get samples"""
+    dataset = reverb.TrajectoryDataset.from_table_signature(
+        server_address=data_server._server_address,
+        table="trainer",
+        max_in_flight_samples_per_worker=10,
+    )
+    dataset = dataset.batch(2)
+    return dataset
+
+
+def test_data_server(test_system: System) -> None:
+    """Test if the data server instantiates processes as expected."""
+
+    # Environment.
+    environment_factory = functools.partial(
+        debugging_utils.make_environment,
+        env_name="simple_spread",
+        action_space="discrete",
+    )
+
+    # Networks.
+    def network_factory(*args: Any, **kwargs: Any) -> Any:
+        return mappo.make_default_networks(  # type: ignore
+            policy_layer_sizes=(32, 32),
+            critic_layer_sizes=(64, 64),
+            *args,
+            **kwargs,
         )
 
-        test_system._builder.data_server()
+    # Checkpointer appends "Checkpoints" to checkpoint_dir.
+    base_dir = "~/mava"
+    mava_id = str(datetime.now())
+    checkpoint_subpath = f"{base_dir}/{mava_id}"
+    # Log every [log_every] seconds.
+    log_every = 1
+    logger_factory = functools.partial(
+        logger_utils.make_logger,
+        directory=base_dir,
+        to_terminal=True,
+        to_tensorboard=True,
+        time_stamp=mava_id,
+        time_delta=log_every,
+    )
+    # Optimizer.
+    optimizer = optax.chain(
+        optax.clip_by_global_norm(40.0),
+        optax.adam(1e-4),
+    )
 
-        # Assuming a single table for now
-        table = test_system._builder.store.data_tables[0]
-        table_info = table.info
+    # Build the test_system
+    test_system.build(
+        environment_factory=environment_factory,
+        network_factory=network_factory,
+        logger_factory=logger_factory,
+        experiment_path=checkpoint_subpath,
+        optimizer=optimizer,
+        executor_parameter_update_period=10,
+        multi_process=False,
+        run_evaluator=True,
+        num_executors=1,
+        use_next_extras=False,
+        max_queue_size=5000,
+        sample_batch_size=2,
+        max_in_flight_samples_per_worker=4,
+        num_workers_per_iterator=-1,
+        rate_limiter_timeout_ms=-1,
+        checkpoint=True,
+        nodes_on_gpu=[],
+        lp_launch_type=lp.LaunchType.LOCAL_MULTI_PROCESSING,
+        sequence_length=4,
+        period=4,
+    )
 
-        # Testing table is set correctly
-        assert (
-            table_info.name
-            == list(test_system._builder.store.table_network_config.keys())[
-                0
-            ]  # noqa:E501
-        ), f"{table_info.name} {list(test_system._builder.store.table_network_config.keys())[0]}"  # noqa: E501
+    (
+        data_server,
+        parameter_server,
+        executor,
+        evaluator,
+        trainer,
+    ) = test_system._builder.store.system_build
 
-        # Off policy params
-        assert_if_value_is_not_none(system_config.get("max_size"), table_info.max_size)
-        assert_if_value_is_not_none(
-            system_config.get("max_times_sampled"), table_info.max_times_sampled
-        )
-        assert_if_value_is_not_none(
-            system_config.get("min_data_server_size"),
-            table_info.rate_limiter_info.min_size_to_sample,
-        )
-        if system_config.get("sampler"):
-            self._check_selector_proto(
-                system_config["sampler"], table_info.sampler_options
-            )
-        if system_config.get("remover"):
-            self._check_selector_proto(
-                system_config["remover"], table_info.remover_options
-            )
+    table_trainer = data_server.server_info()["trainer"]
+    assert table_trainer.name == "trainer"
+    assert table_trainer.max_size == 5000  # max_queue_size
+    assert table_trainer.max_times_sampled == 1
 
-        # On policy params
-        assert_if_value_is_not_none(
-            system_config.get("max_queue_size"), table_info.max_size
-        )
+    rate_limiter = table_trainer.rate_limiter_info
+    assert rate_limiter.samples_per_insert == 1
+    assert rate_limiter.max_diff == 5000
+    assert rate_limiter.min_size_to_sample == 1
+
+    signature = table_trainer.signature
+    assert sorted(list(signature.observations.keys())) == [
+        "agent_0",
+        "agent_1",
+        "agent_2",
+    ]
+    for observation in signature.observations.values():
+        assert observation.observation
+        assert observation.legal_actions
+        assert observation.terminal
+    assert sorted(list(signature.actions.keys())) == ["agent_0", "agent_1", "agent_2"]
+    assert sorted(list(signature.rewards.keys())) == ["agent_0", "agent_1", "agent_2"]
+    assert sorted(list(signature.discounts.keys())) == ["agent_0", "agent_1", "agent_2"]
+    assert sorted(list(signature.extras.keys())) == ["network_keys", "policy_info"]
+    assert sorted(list(signature.extras["network_keys"].keys())) == [
+        "agent_0",
+        "agent_1",
+        "agent_2",
+    ]
+    assert sorted(list(signature.extras["policy_info"].keys())) == [
+        "agent_0",
+        "agent_1",
+        "agent_2",
+    ]
+
+    assert table_trainer.num_episodes == 0
+    assert table_trainer.num_unique_samples == 0
+
+    # Run episodes
+    for _ in range(0, 5):
+        executor.run_episode()
+
+    table_trainer = data_server.server_info()["trainer"]
+    assert table_trainer.num_episodes == 5  # 5 episodes
+    assert table_trainer.num_unique_samples != 0
+
+    # dataset added by the exevutor via the adders
+    dataset = get_dataset(data_server)
+    for sample in dataset.take(1):
+        assert sorted(list(sample.data.observations.keys())) == [
+            "agent_0",
+            "agent_1",
+            "agent_2",
+        ]
+        for observation in sample.data.observations.values():
+            assert jnp.size(observation.observation) != 0
+            assert jnp.size(observation.legal_actions) != 0
+            assert jnp.size(observation.terminal) != 0
+        assert sorted(list(sample.data.actions.keys())) == [
+            "agent_0",
+            "agent_1",
+            "agent_2",
+        ]
+        for agent_action in sample.data.actions.values():
+            assert jnp.size(agent_action) != 0
+        assert sorted(list(sample.data.rewards.keys())) == [
+            "agent_0",
+            "agent_1",
+            "agent_2",
+        ]
+        for agent_reward in sample.data.rewards.values():
+            assert jnp.size(agent_reward) != 0
+        assert sorted(list(sample.data.discounts.keys())) == [
+            "agent_0",
+            "agent_1",
+            "agent_2",
+        ]
+        for agent_discount in sample.data.discounts.values():
+            assert jnp.size(agent_discount) != 0
+        assert sorted(list(sample.data.extras.keys())) == [
+            "network_keys",
+            "policy_info",
+        ]
+        assert sorted(list(sample.data.extras["network_keys"].keys())) == [
+            "agent_0",
+            "agent_1",
+            "agent_2",
+        ]
+        assert sorted(list(sample.data.extras["policy_info"].keys())) == [
+            "agent_0",
+            "agent_1",
+            "agent_2",
+        ]
