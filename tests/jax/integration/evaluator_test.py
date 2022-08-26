3cf03da8361445d980056663688db9ef8d6e28b1
OmaymaMahjoub <omayma.mahjoub@ensi-uma.tn>
2022-08-26 10:09:53 +0100

    test: minor changes

--- tests/jax/integration/evaluator_test.py
+++ tests/jax/integration/evaluator_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Integration test of The Executor for Jax-based Mava systems"""
+"""Integration test of The Evaluatorfor Jax-based Mava systems"""
 
 import functools
 
