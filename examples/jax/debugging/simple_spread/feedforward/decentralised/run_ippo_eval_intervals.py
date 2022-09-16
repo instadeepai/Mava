07a83dff943132ad1c1b5a70eabfced79c499ff1
RuanJohn <ruanjdekock@gmail.com>
2022-09-16 08:57:04 +0200

    fix: optimiser fixes to eval interval examples

--- examples/jax/debugging/simple_spread/feedforward/decentralised/run_ippo_eval_intervals.py
+++ examples/jax/debugging/simple_spread/feedforward/decentralised/run_ippo_eval_intervals.py
@@ -81,8 +81,12 @@ def main(_: Any) -> None:
         time_delta=log_every,
     )
 
-    # Optimizer.
-    optimizer = optax.chain(
+    # Optimiser.
+    policy_optimiser = optax.chain(
+        optax.clip_by_global_norm(40.0), optax.scale_by_adam(), optax.scale(-1e-4)
+    )
+
+    critic_optimiser = optax.chain(
         optax.clip_by_global_norm(40.0), optax.scale_by_adam(), optax.scale(-1e-4)
     )
 
@@ -95,7 +99,8 @@ def main(_: Any) -> None:
         network_factory=network_factory,
         logger_factory=logger_factory,
         experiment_path=experiment_path,
-        optimizer=optimizer,
+        policy_optimiser=policy_optimiser,
+        critic_optimiser=critic_optimiser,
         run_evaluator=True,
         sample_batch_size=5,
         num_epochs=15,
