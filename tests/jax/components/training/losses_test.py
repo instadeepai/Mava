379c19d89e3f6c94504870bdb71f5ed5ba681cc5
Dries <dries.epos@gmail.com>
2022-09-23 13:54:17 +0200

    fix: Tests.

--- tests/jax/components/training/losses_test.py
+++ tests/jax/components/training/losses_test.py
@@ -35,10 +35,10 @@ class MockPolicyNet:
         """Mock function to apply the network to training data"""
         if len(observation_state) == 2:
             # Recurrent case
-            return observation_state[0][0], observation_state[1]  # noqa: E501
+            return observation_state[0][0], observation_state[1] # type: ignore
         else:
             # Feedforward case
-            return observation_state[0]  # noqa: E501
+            return observation_state[0]  # type: ignore
 
 
 class MockCriticNet:
