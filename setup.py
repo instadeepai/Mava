cd2a9537decfc241c916763b2a593043995814e1
OmaymaMahjoub <omaymamahjoub98@gmail.com>
2022-09-28 07:06:55 +0100

    fix: type-six version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ testing_formatting_requirements = [
     "pytest-cov",
     "interrogate",
     "pydocstyle",
-    "types-six==1.16.20",
+    "types-six",
 ]
 
 record_episode_requirements = ["array2gif", "pyglet"]
