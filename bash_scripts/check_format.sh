b87cf458a359536b2666dc7ea95bfaf25d6a157e
arnupretorius <arnupretorius@gmail.com>
2022-10-04 10:54:05 +0200

    chore: update CI to verfiy uploader

--- bash_scripts/check_format.sh
+++ bash_scripts/check_format.sh
@@ -33,6 +33,16 @@ curl -Os https://uploader.codecov.io/latest/linux/codecov
 chmod +x codecov
 ./codecov
 
+# Integrity checking the uploader
+curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+shasum -a 256 -c codecov.SHA256SUM
+chmod +x codecov
+./codecov -t ${CODECOV_TOKEN}
+
 # Set up a virtual environment.
 pip install virtualenv
 virtualenv mava_testing
