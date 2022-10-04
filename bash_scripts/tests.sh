177d048988e6d1548308da830743298b9eb26c0e
arnupretorius <arnupretorius@gmail.com>
2022-10-04 11:51:37 +0200

    chore: just use action

--- bash_scripts/tests.sh
+++ bash_scripts/tests.sh
@@ -63,21 +63,6 @@ else
     pytest --durations=10 -n "${N_CPU}" tests --ignore-glob="*/*system_test.py" ;
 fi
 
-# Code coverage
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-chmod +x codecov
-./codecov
-
-# Integrity checking the uploader
-curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
-curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
-gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
-shasum -a 256 -c codecov.SHA256SUM
-chmod +x codecov
-./codecov -t ${CODECOV_TOKEN}
-
 # Clean-up.
 deactivate
 rm -rf mava_testing/
