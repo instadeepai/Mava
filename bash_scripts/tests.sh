f7262a8a8b43d09bdbdda4f141f9a6f86a05fd45
arnupretorius <arnupretorius@gmail.com>
2022-10-05 16:13:23 +0200

    fix: testing

--- bash_scripts/tests.sh
+++ bash_scripts/tests.sh
@@ -63,6 +63,8 @@ else
     pytest --cov=./ --cov-report=xml --durations=10 -n "${N_CPU}" tests --ignore-glob="*/*system_test.py" ;
 fi
 
+ls
+
 # Code coverage
 curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
 curl -Os https://uploader.codecov.io/latest/linux/codecov
@@ -71,7 +73,7 @@ curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
 gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
 shasum -a 256 -c codecov.SHA256SUM
 chmod +x codecov
-./codecov
+./codecov --rootDir=./ --file=coverage.xml
 
 # Clean-up.
 deactivate
