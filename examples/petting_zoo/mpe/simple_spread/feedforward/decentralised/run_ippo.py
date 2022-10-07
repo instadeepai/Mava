4afb4c0e28a63fbb0a9fce95812f251f5ccd81c9
RuanJohn <ruanjdekock@gmail.com>
2022-10-07 09:10:54 +0200

    chore: MPE example docstring fix.

--- examples/petting_zoo/mpe/simple_spread/feedforward/decentralised/run_ippo.py
+++ examples/petting_zoo/mpe/simple_spread/feedforward/decentralised/run_ippo.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Example running IPPO on debug MPE environments."""
+"""Example running IPPO on MPE environments."""
 import functools
 from datetime import datetime
 from typing import Any
