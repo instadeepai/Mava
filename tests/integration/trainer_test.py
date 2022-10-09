463703dad500d92b1f45269537782eaf20c71bfd
ulricharmel <mulricharmel@gmail.com>
2022-10-09 13:24:32 +0200

    added a trainer integration test for features normalisation

--- tests/integration/trainer_test.py
+++ tests/integration/trainer_test.py
@@ -18,14 +18,24 @@
 import jax.numpy as jnp
 import pytest
 
+from mava import constants
 from mava.systems import System
-from tests.systems.systems_test_data import ippo_system_single_process
+from tests.systems.systems_test_data import (
+    ippo_system_single_process,
+    ippo_system_single_process_norm,
+)
 
 
 @pytest.fixture
 def test_system_sp() -> System:
     """A single process built system"""
     return ippo_system_single_process()
+
+
+@pytest.fixture
+def test_system_sp_norm() -> System:
+    """A single process built system with feature and value normlisation set to true"""
+    return ippo_system_single_process_norm()
 
 
 def test_trainer_single_process(test_system_sp: System) -> None:
@@ -70,3 +80,64 @@ def test_trainer_single_process(test_system_sp: System) -> None:
         for categorical_value_head in mu.values():
             assert not jnp.all(categorical_value_head["b"] == 0)
             assert not jnp.all(categorical_value_head["w"] == 0)
+
+
+def test_trainer_single_process_norm(test_system_sp_norm: System) -> None:
+    """Test if the trainer instantiates processes as expected."""
+    # extract the nodes
+    (
+        data_server,
+        parameter_server,
+        executor,
+        evaluator,
+        trainer,
+    ) = test_system_sp_norm._builder.store.system_build
+
+    executor.run_episode()
+
+    # Before run step method
+    for net_key in trainer.store.networks.keys():
+        # Policy
+        mu = trainer.store.policy_opt_states[net_key]["opt_state"][1].mu  # network
+        for categorical_value_head in mu.values():
+            assert jnp.all(categorical_value_head["b"] == 0)
+            assert jnp.all(categorical_value_head["w"] == 0)
+
+        # Critic
+        mu = trainer.store.critic_opt_states[net_key]["opt_state"][1].mu  # network
+        for categorical_value_head in mu.values():
+            assert jnp.all(categorical_value_head["b"] == 0)
+            assert jnp.all(categorical_value_head["w"] == 0)
+
+    # Check normalisation parameters
+    for agent in trainer.store.trainer_agent_net_keys.keys():
+        obs_norm_params = trainer.store.obs_norm_params[
+            constants.OBS_NORM_STATE_DICT_KEY
+        ][agent]
+        assert jnp.all(obs_norm_params["mean"] == 0)
+        assert jnp.all(obs_norm_params["var"] == 0)
+        assert jnp.all(obs_norm_params["count"] == 1e-4)
+
+    trainer.step()
+
+    # After run step method
+    for net_key in trainer.store.networks.keys():
+        # Policy
+        mu = trainer.store.policy_opt_states[net_key]["opt_state"][1].mu  # network
+        for categorical_value_head in mu.values():
+            assert not jnp.all(categorical_value_head["b"] == 0)
+            assert not jnp.all(categorical_value_head["w"] == 0)
+
+        # Critic
+        mu = trainer.store.critic_opt_states[net_key]["opt_state"][1].mu  # network
+        for categorical_value_head in mu.values():
+            assert not jnp.all(categorical_value_head["b"] == 0)
+            assert not jnp.all(categorical_value_head["w"] == 0)
+
+    for agent in trainer.store.trainer_agent_net_keys.keys():
+        obs_norm_params = trainer.store.obs_norm_params[
+            constants.OBS_NORM_STATE_DICT_KEY
+        ][agent]
+        assert not jnp.all(obs_norm_params["mean"] == 0)
+        assert not jnp.all(obs_norm_params["var"] == 0)
+        assert not jnp.all(obs_norm_params["count"] == 1e-4)
