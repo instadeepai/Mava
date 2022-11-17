fafdbab30151d3929bb1ed8417d519f42322fccd
RuanJohn <ruanjdekock@gmail.com>
2022-11-17 10:10:25 +0200

    fix: pin jax and jaxlib versions.

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ tf_requirements = [
 
 jax_requirements = [
     "chex",
-    "jax~=0.3.20",
-    "jaxlib~=0.3.20",
+    "jax==0.3.24",
+    "jaxlib==0.3.24",
     "dm-haiku==0.0.8",
     "flax",
     "optax",
