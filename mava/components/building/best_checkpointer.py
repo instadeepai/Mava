77e817ba733fa0ed284ac5e3c8043e14e8711e79
Sasha Abramowitz <reallysasha@gmail.com>
2022-11-25 12:50:36 +0200

    feat: best checkpointer own component

---
+++ mava/components/building/best_checkpointer.py
@@ -0,0 +1,74 @@
+from copy import deepcopy
+from dataclasses import dataclass
+from typing import Any, Dict, Optional, Tuple, Union
+
+from optax import Params
+
+from mava.components.component import Component
+from mava.core_jax import SystemBuilder, SystemParameterServer
+
+
+@dataclass
+class BestCheckpointerConfig:
+    checkpointing_metric: Tuple = ("mean_episode_return",)
+    checkpoint_best_perf: bool = False
+
+
+class BestCheckpointer(Component):
+    def __init__(
+        self, config: BestCheckpointerConfig = BestCheckpointerConfig()
+    ) -> None:
+        """Initialise BestCheckpointer"""
+        super().__init__(config)
+
+    def on_building_init(self, builder: SystemBuilder) -> None:
+        """Store checkpointing params in the store"""
+        # Save list of metrics attached with their best performance
+        builder.store.checkpointing_metric: Dict[str, Any] = {}  # type: ignore
+        for metric in list(self.config.checkpointing_metric):
+            builder.store.checkpointing_metric[metric] = None
+
+    def on_building_executor_start(self, builder: SystemBuilder) -> None:
+        """Initialises the store for best model checkpointing"""
+        if not (builder.store.is_evaluator and self.config.checkpoint_best_perf):
+            return
+
+        builder.store.best_checkpoint = self.init_checkpointing_params(builder)
+
+    def on_parameter_server_init(self, server: SystemParameterServer) -> None:
+        """Adding checkpointing parameters to parameter server"""
+        if not self.config.checkpoint_best_perf:
+            return
+
+        server.store.parameters.update(self.init_checkpointing_params(server))
+
+    def init_checkpointing_params(
+        self, system: Union[SystemParameterServer, SystemBuilder]
+    ) -> Dict[str, Any]:
+        """Initialises the parameters used to checkpointing the best models"""
+        params: Dict[str, Dict[str, Optional[Union[float, Params]]]] = {}
+        networks = system.store.networks
+
+        # Create a dictionary of all parameters to save
+        for metric in system.store.checkpointing_metrics:
+            params[metric] = {}
+            params[metric]["best_performance"] = None
+            for agent_net_key in system.store.networks.keys():
+                policy_params = deepcopy(networks[agent_net_key].policy_params)
+                params[metric][f"policy_network-{agent_net_key}"] = policy_params
+
+                critic_params = deepcopy(networks[agent_net_key].critic_params)
+                params[metric][f"critic_network-{agent_net_key}"] = critic_params
+
+                policy_opt = deepcopy(system.store.policy_opt_states[agent_net_key])
+                params[metric][f"policy_opt_state-{agent_net_key}"] = policy_opt
+
+                critic_opt = deepcopy(system.store.critic_opt_states[agent_net_key])
+                params[metric][f"critic_opt_state-{agent_net_key}"] = critic_opt
+
+        return params
+
+    @staticmethod
+    def name() -> str:
+        """Returns the name of the component"""
+        return "best_checkpointer"
--- mava/components/updating/parameter_server.py
+++ mava/components/updating/parameter_server.py
@@ -15,16 +15,15 @@
 
 """Parameter server Component for Mava systems."""
 import abc
-import copy
 from dataclasses import dataclass
-from typing import Any, Dict, List, Optional, Sequence, Tuple, Type, Union
+from typing import Any, Dict, List, Optional, Sequence, Type, Union
 
 import numpy as np
 
 from mava.callbacks import Callback
 from mava.components.building.networks import Networks
 from mava.components.component import Component
-from mava.core_jax import SystemBuilder, SystemParameterServer
+from mava.core_jax import SystemParameterServer
 from mava.utils.lp_utils import termination_fn
 
 
@@ -33,9 +32,6 @@ class ParameterServerConfig:
     non_blocking_sleep_seconds: int = 10
     experiment_path: str = "~/mava/"
     json_path: Optional[str] = None
-    # Saves networks based on this list of metrics
-    checkpointing_metric: Tuple = ("mean_episode_return",)
-    checkpoint_best_perf: bool = False
 
 
 class ParameterServer(Component):
@@ -104,16 +100,6 @@ class DefaultParameterServer(ParameterServer):
         """
         self.config = config
 
-    def on_building_init(self, builder: SystemBuilder) -> None:
-        """Store checkpointing params in the store"""
-        # Save list of metrics attached with their best performance
-        builder.store.checkpointing_metric: Dict[str, Any] = {}  # type: ignore
-        for metric in list(self.config.checkpointing_metric):
-            builder.store.checkpointing_metric[metric] = None
-
-        # Save the flag of best checkpointing or not
-        builder.store.checkpoint_best_perf = self.config.checkpoint_best_perf
-
     def on_parameter_server_init_start(self, server: SystemParameterServer) -> None:
         """Register parameters and network params to track.
 
@@ -161,28 +147,6 @@ class DefaultParameterServer(ParameterServer):
 
         # Interrupt the system in case all the executors failed
         server.store.parameters["num_executor_failed"] = 0
-
-        # Initiate best performance network values
-        if server.store.checkpoint_best_perf:
-            server.store.parameters["best_checkpoint"] = {}
-            for metric in server.store.checkpointing_metric:
-                server.store.parameters["best_checkpoint"][metric] = {}
-                server.store.parameters["best_checkpoint"][metric][
-                    "best_performance"
-                ] = None
-                for agent_net_key in networks.keys():
-                    server.store.parameters["best_checkpoint"][metric][
-                        f"policy_network-{agent_net_key}"
-                    ] = copy.deepcopy(networks[agent_net_key].policy_params)
-                    server.store.parameters["best_checkpoint"][metric][
-                        f"critic_network-{agent_net_key}"
-                    ] = copy.deepcopy(networks[agent_net_key].critic_params)
-                    server.store.parameters["best_checkpoint"][metric][
-                        f"policy_opt_state-{agent_net_key}"
-                    ] = copy.deepcopy(server.store.policy_opt_states[agent_net_key])
-                    server.store.parameters["best_checkpoint"][metric][
-                        f"critic_opt_state-{agent_net_key}"
-                    ] = copy.deepcopy(server.store.critic_opt_states[agent_net_key])
 
     # Get
     def on_parameter_server_get_parameters(self, server: SystemParameterServer) -> None:
