9f268d7a318926fbfe17912d056891c2d61eebf6
Kale-ab <kaleabtessera@gmail.com>
2023-01-18 14:31:23 +0200

    fix: Make base_dir configurable.

--- educational/bash_scripts/random_agent_8m.sh
+++ educational/bash_scripts/random_agent_8m.sh
@@ -1,2 +1,5 @@
 # Script to run random agent on 8m
-python systems/random_agent.py --env_type smac --env_name 8m --max_total_steps=2000000
+# Optional param to change base_dir - mainly for tensorboard. 
+# Usage ./bash_scripts/random_agent_8m.sh ~/custom_folder
+base_dir=${1:-'~/mava'}
+python systems/random_agent.py --env_type smac --env_name 8m --max_total_steps=2000000 --base_dir $base_dir
