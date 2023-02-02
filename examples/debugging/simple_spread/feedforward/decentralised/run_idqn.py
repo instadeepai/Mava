6bfdc384753c9f40f0e2808c4e07007451fff476
Sasha Abramowitz <reallysasha@gmail.com>
2023-02-02 14:49:12 +0200

    fix: epsilon going to 0 too fast

--- examples/debugging/simple_spread/feedforward/decentralised/run_idqn.py
+++ examples/debugging/simple_spread/feedforward/decentralised/run_idqn.py
@@ -85,7 +85,7 @@ def main(_: Any) -> None:
     policy_optimiser = optax.chain(
         optax.clip_by_global_norm(40.0), optax.scale_by_adam(), optax.scale(-1e-4)
     )
-    epsilon_scheduler = LinearEpsilonScheduler(1.0, 0.1, 10_000)
+    epsilon_scheduler = LinearEpsilonScheduler(1.0, 0.1, 100_000)
 
     # Create the system.
     system = idqn.IDQNSystem()
