5f55c48b1adf03e6c8340208c2d95fd43b21ff59
RuanJohn <ruanjdekock@gmail.com>
2023-07-21 13:10:22 +0200

    feat: working on cartpole anakin example.

--- jax_distribution/wrappers/purejaxrl.py
+++ jax_distribution/wrappers/purejaxrl.py
@@ -80,7 +80,7 @@ class LogWrapper(GymnaxWrapper):
         self, key: chex.PRNGKey, params: Optional[environment.EnvParams] = None
     ) -> Tuple[chex.Array, environment.EnvState]:
         obs, env_state = self._env.reset(key, params)
-        state = LogEnvState(env_state, 0, 0, 0, 0, 0)
+        state = LogEnvState(env_state, 0.0, 0, 0.0, 0, 0)
         return obs, state
 
     @partial(jax.jit, static_argnums=(0,))
