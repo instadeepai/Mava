444c98a39bda3c8a924e510eaee93291c8c69490
RuanJohn <ruanjdekock@gmail.com>
2023-07-28 13:32:36 +0200

    feat: moved transition to types file

---
+++ jax_distribution/types.py
@@ -0,0 +1,13 @@
+from typing import NamedTuple
+
+import jax.numpy as jnp
+
+
+class Transition(NamedTuple):
+    done: jnp.ndarray
+    action: jnp.ndarray
+    value: jnp.ndarray
+    reward: jnp.ndarray
+    log_prob: jnp.ndarray
+    obs: jnp.ndarray
+    info: dict
