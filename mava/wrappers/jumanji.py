7bd7873b1566033cf5ceedcfbbe7f4666245e8ca
Wiem Khlifi <w.khlifi@instadeep.com>
2023-12-01 16:52:14 +0100

    Merge pull request #952 from instadeepai/feat/lbf-dev
    - docs: small edits based on reviews
    - Update mava/configs/env/scenario/2s-8x8-2p-2f-coop.yaml
    - small edits on configs
    - feat: edit configs +jumanji wrapper
    - docs: fix typos
    - feat: seperate lbf and rware wrappers + edit configs
    - docs: add details in config file+fix merge conflict
    - Merge branch origin/develop into feat/lbf-dev
    - feat: edit configs
    - Merge branch develop into lbf-dev + small edits
    - feat: change config files
    - docs: modify docs +types+naming
    - fix: fix types
    - fix: fix types
    - feat: changes based on review
    - revert: revert the added systems for lbf except ff_ippo
    - feat: add a flag to choose to use individual reward or not+remove discount repeat
    - feat: create lbf wrapper+lbf config files

--- mava/wrappers/jumanji.py
+++ mava/wrappers/jumanji.py
@@ -17,6 +17,8 @@ from typing import Tuple
 import chex
 import jax.numpy as jnp
 from jumanji import specs
+from jumanji.env import Environment
+from jumanji.environments.routing.lbf import LevelBasedForaging
 from jumanji.environments.routing.robot_warehouse import RobotWarehouse
 from jumanji.types import TimeStep
 from jumanji.wrappers import Wrapper
@@ -24,41 +26,94 @@ from jumanji.wrappers import Wrapper
 from mava.types import Observation, State
 
 
-class RwareWrapper(Wrapper):
-    """Multi-agent wrapper for the Robotic Warehouse environment."""
-
-    def __init__(self, env: RobotWarehouse):
+class MultiAgentWrapper(Wrapper):
+    def __init__(self, env: Environment):
         super().__init__(env)
-        self._env: RobotWarehouse
+        self._num_agents = self._env.num_agents
+        self.time_limit = self._env.time_limit
 
     def modify_timestep(self, timestep: TimeStep) -> TimeStep[Observation]:
-        n_agents = self._env.num_agents
-        observation = Observation(
-            agents_view=timestep.observation.agents_view,
-            action_mask=timestep.observation.action_mask,
-            step_count=jnp.repeat(timestep.observation.step_count, n_agents),
-        )
-        reward = jnp.repeat(timestep.reward, n_agents)
-        discount = jnp.repeat(timestep.discount, n_agents)
-        return timestep.replace(observation=observation, reward=reward, discount=discount)
+        """Modify the timestep for `step` and `reset`."""
+        pass
 
     def reset(self, key: chex.PRNGKey) -> Tuple[State, TimeStep]:
-        """Reset the environment. Updates the step count."""
+        """Reset the environment."""
         state, timestep = self._env.reset(key)
         return state, self.modify_timestep(timestep)
 
     def step(self, state: State, action: chex.Array) -> Tuple[State, TimeStep]:
-        """Step the environment. Updates the step count."""
+        """Step the environment."""
         state, timestep = self._env.step(state, action)
         return state, self.modify_timestep(timestep)
 
     def observation_spec(self) -> specs.Spec[Observation]:
-        """Specification of the observation of the `RobotWarehouse` environment."""
+        """Specification of the observation of the environment."""
         step_count = specs.BoundedArray(
-            (self._env.num_agents,),
+            (self._num_agents,),
             jnp.int32,
-            [0] * self._env.num_agents,
-            [self._env.time_limit] * self._env.num_agents,
+            [0] * self._num_agents,
+            [self.time_limit] * self._num_agents,
             "step_count",
         )
         return self._env.observation_spec().replace(step_count=step_count)
+
+
+class RwareWrapper(MultiAgentWrapper):
+    """Multi-agent wrapper for the Robotic Warehouse environment."""
+
+    def __init__(self, env: RobotWarehouse):
+        super().__init__(env)
+
+    def modify_timestep(self, timestep: TimeStep) -> TimeStep[Observation]:
+        """Modify the timestep for the Robotic Warehouse environment."""
+        observation = Observation(
+            agents_view=timestep.observation.agents_view,
+            action_mask=timestep.observation.action_mask,
+            step_count=jnp.repeat(timestep.observation.step_count, self._num_agents),
+        )
+        reward = jnp.repeat(timestep.reward, self._num_agents)
+        discount = jnp.repeat(timestep.discount, self._num_agents)
+        return timestep.replace(observation=observation, reward=reward, discount=discount)
+
+
+class LbfWrapper(MultiAgentWrapper):
+    """
+     Multi-agent wrapper for the Level-Based Foraging environment.
+
+    Args:
+        env (Environment): The base environment.
+        use_individual_rewards (bool): If true each agent gets a separate reward,
+        sum reward otherwise.
+    """
+
+    def __init__(self, env: LevelBasedForaging, use_individual_rewards: bool = False):
+        super().__init__(env)
+        self._env: LevelBasedForaging
+        self._use_individual_rewards = use_individual_rewards
+
+    def aggregate_rewards(
+        self, timestep: TimeStep, observation: Observation
+    ) -> TimeStep[Observation]:
+        """Aggregate individual rewards across agents."""
+        team_reward = jnp.sum(timestep.reward)
+
+        # Repeat the aggregated reward for each agent.
+        reward = jnp.repeat(team_reward, self._num_agents)
+        return timestep.replace(observation=observation, reward=reward)
+
+    def modify_timestep(self, timestep: TimeStep) -> TimeStep[Observation]:
+        """Modify the timestep for Level-Based Foraging environment and update
+        the reward based on the specified reward handling strategy."""
+
+        # Create a new observation with adjusted step count
+        modified_observation = Observation(
+            agents_view=timestep.observation.agents_view,
+            action_mask=timestep.observation.action_mask,
+            step_count=jnp.repeat(timestep.observation.step_count, self._num_agents),
+        )
+        if self._use_individual_rewards:
+            # The environment returns a list of individual rewards and these are used as is.
+            return timestep.replace(observation=modified_observation)
+
+        # Aggregate the list of individual rewards and use a single team_reward.
+        return self.aggregate_rewards(timestep, modified_observation)
