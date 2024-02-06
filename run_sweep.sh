46aa68991b6a4a9c5b099919101d2066aaa3a50d
WiemKhlifi <w.khlifi@instadeep.com>
2024-02-06 10:15:59 +0100

    feat: set a sweep of exp

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,30 +1,27 @@
-# python mava/systems/ff_ippo_continuous.py  system.total_timesteps=300000000 system.seed=42,43,44 env.env_name=ant_4x2,halfcheetah_6x1,hopper_3x1,"humanoid_9|8",walker2d_2x3 -m && \
-# python mava/systems/ff_mappo_continuous.py  system.total_timesteps=300000000 system.seed=42,43,44 env.env_name=ant_4x2,halfcheetah_6x1,hopper_3x1,"humanoid_9|8",walker2d_2x3 -m && \
-# python mava/systems/rec_ippo_continuous.py  system.total_timesteps=300000000 system.seed=42,43,44 env.env_name=ant_4x2,halfcheetah_6x1,hopper_3x1,"humanoid_9|8",walker2d_2x3 -m && \
-# python mava/systems/rec_mappo_continuous.py  system.total_timesteps=300000000 system.seed=42,43,44 env.env_name=ant_4x2,halfcheetah_6x1,hopper_3x1,"humanoid_9|8",walker2d_2x3 -m
+# Before running this script, excute the "chmod u+x run_sweep.sh" then run the script using the tmux terminal.
+# python mava/systems/ff_ippo_continuous.py system.seed=42,43,44 env.env_name=ant_4x2,halfcheetah_6x1,hopper_3x1,"humanoid_9|8",walker2d_2x3 -m && \
 
-envs=(walker2d_2x3 ant_4x2 halfcheetah_6x1 hopper_3x1 "humanoid_9|8")
+
+envs=(ant_4x2 halfcheetah_6x1 walker2d_2x3) # hopper_3x1,"humanoid_9|8
+# seeds= (42 12)
 
 for e in "${envs[@]}"
 do
-    if [ "$e" == "ant_4x2" ] || [ "$e" == "walker2d_2x3" ]
-    then
-        for seed in {0..5}
-        do
-            python mava/systems/ff_ippo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m &&
-            python mava/systems/ff_mappo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m &&
-            python mava/systems/rec_ippo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m &&
-            python mava/systems/rec_mappo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m
-        done
-    else
-        for seed in {0..5}
-        do
-            python mava/systems/ff_ippo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e"  &&
-            python mava/systems/ff_mappo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e"  &&
-            python mava/systems/rec_ippo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e"  &&
-            python mava/systems/rec_mappo_continuous.py system.seed="$seed" system.total_timesteps=400000000 env.env_name="$e"  env.scenario.task_name="$e"
-        done
-    fi
+    # if [ "$e" == "ant_4x2" ] || [ "$e" == "walker2d_2x3" ]
+    # then
+    #     for seed in {42..44}
+    #     do
+    #         python mava/systems/ff_ippo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m &&
+    #         python mava/systems/ff_mappo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m # &&
+    #         # python mava/systems/rec_ippo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m &&
+    #         # python mava/systems/rec_mappo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e" env.kwargs.homogenisation_method=None -m
+    #     done
+    # else
+    for seed in {42..44}
+    do
+        python mava/systems/ff_ippo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e"  &&
+        python mava/systems/ff_mappo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e" &&
+        python mava/systems/rec_ippo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e"
+        # python mava/systems/rec_mappo_continuous.py system.seed="$seed" env.env_name="$e"  env.scenario.task_name="$e"
+    done
 done
-
-python mava/systems/ff_ippo_continuous.py env.env_name=ant_4x2 env.kwargs.homogenisation_method=None
