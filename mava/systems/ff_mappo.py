deb172386f1c2e3ebe1e67e7b227073bbacf82af
Liam Clark <l.clark@instadeep.com>
2024-02-14 15:22:47 +0200

    Add return metrics for experiments and corresponding Hydra config option
    Modifies the run_experiment method and hydra_entry_point in the various systems to return a metric to be used for hyperparameter tuning through Hydra (using something like OpTuna). The Hydra config option, config.env.eval_metric,is used to select the metric (episode_return or win_rate) for an environment.

--- mava/systems/ff_mappo.py
+++ mava/systems/ff_mappo.py
@@ -456,7 +456,7 @@ def learner_setup(
     return learn, actor_network, init_learner_state
 
 
-def run_experiment(_config: DictConfig) -> None:
+def run_experiment(_config: DictConfig) -> float:
     """Runs experiment."""
     config = copy.deepcopy(_config)
 
@@ -591,17 +591,21 @@ def run_experiment(_config: DictConfig) -> None:
     # Stop the logger.
     logger.stop()
 
+    # Return the evaluation performance.
+    eval_performance = float(jnp.mean(evaluator_output.episode_metrics[config.env.eval_metric]))
+    return eval_performance
+
 
 @hydra.main(config_path="../configs", config_name="default_ff_mappo.yaml", version_base="1.2")
-def hydra_entry_point(cfg: DictConfig) -> None:
+def hydra_entry_point(cfg: DictConfig) -> float:
     """Experiment entry point."""
     # Allow dynamic attributes.
     OmegaConf.set_struct(cfg, False)
 
     # Run experiment.
-    run_experiment(cfg)
-
+    eval_performance = run_experiment(cfg)
     print(f"{Fore.CYAN}{Style.BRIGHT}MAPPO experiment completed{Style.RESET_ALL}")
+    return eval_performance
 
 
 if __name__ == "__main__":
