34efa2f93e36b9a04209bd1c48cd64f5df436d1c
Sasha Abramowitz <reallysasha@gmail.com>
2024-03-04 16:00:24 +0200

    chore: ignore ep completed for logging explore metrics

--- mava/systems/sac/ff_masac.py
+++ mava/systems/sac/ff_masac.py
@@ -515,11 +515,12 @@ def run_experiment(cfg: DictConfig) -> float:
     # Log explore metrics.
     t = int(jnp.sum(learner_state.t))
     sps = t / (time.time() - start_time)
-    final_metrics, ep_completed = episode_metrics.get_final_step_metrics(metrics)
+    # Don't mind if episode isn't completed here, nice to have the graphs start near 0.
+    # So we ignore the second return value.
+    final_metrics, _ = episode_metrics.get_final_step_metrics(metrics)
 
     logger.log({"step": t, "steps_per_second": sps}, t, 0, LogEvent.MISC)
-    if ep_completed:
-        logger.log(final_metrics, cfg.system.explore_steps, 0, LogEvent.ACT)
+    logger.log(final_metrics, cfg.system.explore_steps, 0, LogEvent.ACT)
 
     # Main loop:
     # We want start to align with the final step of the first pmaped_learn,
