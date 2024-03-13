fix: small timestep calculation fix

mava/systems/q_learning/rec_iql.py
@@ -93,10 +93,10 @@ def init(
     num_agents = env.num_agents
 
     key, q_key = jax.random.split(key, 2)
-# Shape legend: 
-# T: Time 
-# B: Batch
-# A: Agent
+    # Shape legend:
+    # T: Time
+    # B: Batch
+    # A: Agent
     # Make dummy inputs to init recurrent Q network -> need shape (T, B, A, ...)
     init_obs = env.observation_spec().generate_value()  # (A, ...)
     init_obs_batched = jax.tree_util.tree_map(
@@ -173,7 +173,7 @@ def init(
         axis_name="device",
     )(reset_keys)
     first_obs = first_timestep.observation
-    first_done = first_timestep.last()[..., jnp.newaxis] 
+    first_done = first_timestep.last()[..., jnp.newaxis]
 
     # Initialise env steps and training steps
     t0 = jnp.zeros((cfg.arch.n_devices, cfg.system.update_batch_size), dtype=int)
@@ -529,6 +529,14 @@ def run_experiment(cfg: DictConfig) -> float:
     cfg.arch.n_devices = len(jax.devices())
 
     # Number of env steps before evaluating/logging.
+    cfg.system.total_timesteps = int(
+        cfg.system.rollout_length
+        * cfg.system.update_batch_size
+        * cfg.arch.num_envs
+        * cfg.arch.n_devices
+        * cfg.system.num_updates
+    )
+
     steps_per_rollout = int(cfg.system.total_timesteps // cfg.arch.num_evaluation)
     # Multiplier for a single env/learn step in an anakin system
     anakin_steps = cfg.arch.n_devices * cfg.system.update_batch_size