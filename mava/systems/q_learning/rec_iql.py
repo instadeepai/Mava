f57c99f0a437204162791e1b9acbd0bf91b83f65
lbeyers <louise@beyers.co.za>
2024-03-13 14:32:18 +0200

    Update mava/systems/q_learning/rec_iql.py
    Co-authored-by: Sasha <reallysasha@gmail.com>

--- mava/systems/q_learning/rec_iql.py
+++ mava/systems/q_learning/rec_iql.py
@@ -153,8 +153,8 @@ def init(
 
     # Initialise trajectory buffer
     rb = fbx.make_trajectory_buffer(
-        sample_sequence_length=cfg.system.recurrent_chunk_size
-        + 1,  # n transitions gives n-1 full data points
+        # n transitions gives n-1 full data points
+        sample_sequence_length=cfg.system.recurrent_chunk_size + 1,
         period=1,  # sample any unique trajectory
         add_batch_size=cfg.arch.num_envs,
         sample_batch_size=cfg.system.sample_batch_size,
