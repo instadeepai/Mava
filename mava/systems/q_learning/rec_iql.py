589abcbed68c6b39f725808babaa0978eac89fa0
lbeyers <louise@beyers.co.za>
2024-03-13 09:44:06 +0200

    Update mava/systems/q_learning/rec_iql.py
    Co-authored-by: RuanJohn <33461981+RuanJohn@users.noreply.github.com>

--- mava/systems/q_learning/rec_iql.py
+++ mava/systems/q_learning/rec_iql.py
@@ -105,7 +105,7 @@ def init(
     )  # (B, A, ...)
     init_obs_batched = jax.tree_util.tree_map(
         lambda x: x[jnp.newaxis, ...], init_obs_batched
-    )  # add time dim (1,B,A,x)
+    )  # add time dim (1, B, A, ...)
     init_done = jnp.zeros((1, cfg.arch.num_envs, 1), dtype=bool)  # (1,B,1)
     init_x = (init_obs_batched, init_done)  # pack the RNN dummy inputs
     init_hidden_state = ScannedRNN.initialize_carry(
