7fe4b5dc5c2e086dd29370c026b419e28dbe5ea5
Sasha Abramowitz <reallysasha@gmail.com>
2024-07-10 12:25:33 +0200

    fix: protocol explicitly names methods instead of inheriting

--- mava/types.py
+++ mava/types.py
@@ -15,6 +15,7 @@
 from typing import Any, Callable, Dict, Generic, Protocol, Tuple, TypeVar
 
 import chex
+import jumanji.specs as specs
 from flax.core.frozen_dict import FrozenDict
 from jumanji.env import Environment
 from jumanji.types import TimeStep
@@ -29,7 +30,7 @@ HiddenState: TypeAlias = chex.Array
 State: TypeAlias = Any
 
 
-class MarlEnv(Environment, Protocol):
+class MarlEnv(Protocol):
     """The API used by mava for environments.
 
     A mava environment simply uses the Jumanji env API with a few added attributes.
@@ -40,6 +41,70 @@ class MarlEnv(Environment, Protocol):
     num_agents: int
     time_limit: int
     action_dim: int
+
+    def reset(self, key: chex.PRNGKey) -> Tuple[State, TimeStep]:
+        """Resets the environment to an initial state.
+
+        Args:
+            key: random key used to reset the environment.
+
+        Returns:
+            state: State object corresponding to the new state of the environment,
+            timestep: TimeStep object corresponding the first timestep returned by the environment,
+        """
+        ...
+
+    def step(self, state: State, action: chex.Array) -> Tuple[State, TimeStep]:
+        """Run one timestep of the environment's dynamics.
+
+        Args:
+            state: State object containing the dynamics of the environment.
+            action: Array containing the action to take.
+
+        Returns:
+            state: State object corresponding to the next state of the environment,
+            timestep: TimeStep object corresponding the timestep returned by the environment,
+        """
+        ...
+
+    def observation_spec(self) -> specs.Spec:
+        """Returns the observation spec.
+
+        Returns:
+            observation_spec: a NestedSpec tree of spec.
+        """
+        ...
+
+    def action_spec(self) -> specs.Spec:
+        """Returns the action spec.
+
+        Returns:
+            action_spec: a NestedSpec tree of spec.
+        """
+        ...
+
+    def reward_spec(self) -> specs.Array:
+        """Describes the reward returned by the environment. By default, this is assumed to be a
+        single float.
+
+        Returns:
+            reward_spec: a `specs.Array` spec.
+        """
+        ...
+
+    def discount_spec(self) -> specs.BoundedArray:
+        """Describes the discount returned by the environment. By default, this is assumed to be a
+        single float between 0 and 1.
+
+        Returns:
+            discount_spec: a `specs.BoundedArray` spec.
+        """
+        ...
+
+    @property
+    def unwrapped(self) -> Environment:
+        """Retuns: the innermost environment (without any wrappers applied)."""
+        ...
 
 
 class Observation(NamedTuple):
