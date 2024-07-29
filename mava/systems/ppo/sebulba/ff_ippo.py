5a5e542c6b135bcc86d2d40c06ac6905e5f7b435
Louay-Ben-nessir <louaybennessir@gmail.com>
2024-07-29 11:53:22 +0100

    chore: config file changes

--- mava/systems/ppo/sebulba/ff_ippo.py
+++ mava/systems/ppo/sebulba/ff_ippo.py
@@ -697,7 +697,7 @@ def run_experiment(_config: DictConfig) -> float:
 
 
 @hydra.main(
-    config_path="../../../configs", config_name="default_ff_ippo_seb.yaml", version_base="1.2"
+    config_path="../../../configs", config_name="default_ff_ippo_sebulba.yaml", version_base="1.2"
 )
 def hydra_entry_point(cfg: DictConfig) -> float:
     """Experiment entry point."""
