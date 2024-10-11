47b8e036f57722d7a2b98d4d0801bdd186a77c1f
Sasha Abramowitz <reallysasha@gmail.com>
2024-10-11 09:51:59 +0200

    fix: update configs to match latest mava

--- mava/systems/ppo/sebulba/ff_ippo.py
+++ mava/systems/ppo/sebulba/ff_ippo.py
@@ -704,7 +704,9 @@ def run_experiment(_config: DictConfig) -> float:
 
 
 @hydra.main(
-    config_path="../../../configs", config_name="default_ff_ippo_sebulba.yaml", version_base="1.2"
+    config_path="../../../configs/default/",
+    config_name="ff_ippo_sebulba.yaml",
+    version_base="1.2",
 )
 def hydra_entry_point(cfg: DictConfig) -> float:
     """Experiment entry point."""
