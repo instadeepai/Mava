7482c8f47d651e10dd1cccb800f726cfa30d8ba9
Ruan de Kock <33461981+RuanJohn@users.noreply.github.com>
2024-10-23 14:23:35 +0200

    Update mava/systems/q_learning/anakin/rec_qmix.py
    Co-authored-by: Sasha Abramowitz <reallysasha@gmail.com>

--- mava/systems/q_learning/anakin/rec_qmix.py
+++ mava/systems/q_learning/anakin/rec_qmix.py
@@ -623,12 +623,7 @@ def run_experiment(cfg: DictConfig) -> float:
         final_metrics, ep_completed = episode_metrics.get_final_step_metrics(metrics)
         final_metrics["steps_per_second"] = steps_per_rollout / elapsed_time
         loss_metrics = losses
-        logger.log(
-            {"timestep": t, "epsilon": eps},
-            t,
-            eval_idx,
-            LogEvent.MISC,
-        )
+        logger.log({"timestep": t, "epsilon": eps}, t, eval_idx, LogEvent.MISC)
         if ep_completed:
             logger.log(final_metrics, t, eval_idx, LogEvent.ACT)
         logger.log(loss_metrics, t, eval_idx, LogEvent.TRAIN)
