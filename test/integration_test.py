a4df0e734c60a3a1388b32d1f4299839a7c357f2
Ruan de Kock <ruanjdekock@gmail.com>
2024-10-30 11:00:41 +0200

    Merge pull request #1107 from instadeepai/feat/implement-mat
    - feat: merge in main
    - feat: swiglu documentation
    - feat: execution and training apply types
    - chore: rename embed dim
    - chore: remove obs dim in MAT network class
    - chore: config comments and reverts
    - chore: pre-commit
    - Merge branch develop into feat/implement-mat
    - chore: set correct number of keys
    - chore: better action encoder init
    - chore: add dim on new line
    - Merge branch develop into feat/implement-mat
    - Merge branch develop into feat/implement-mat
    - chore: more lightweight network configs
    - chore: corect shape names in the comments
    - feat: add MAT network config type
    - feat: use network for MAT network init
    - test: add mat to integration tests
    - Merge branch develop into feat/implement-mat
    - Merge branch develop into feat/implement-mat
    - feat: use model params and optimiser state directly instead of named tuples
    - feat: use .at[].set() with drop instead of jax.lax.cond to update shifted actions
    - feat: use make mlp method
    - chore: output projection
    - chore: use capital letters for dimensions
    - chore: todo about using einops in the future
    - feat: rename dimensions
    - chore: pass in less seeds
    - feat: split less keys
    - chore: linter
    - feat: pass in full observation object to network
    - chore: use marlenv type
    - chore: dont check action space type on strings
    - chore: rename v_loc to value
    - chore: move SwiGLU network to torsos file
    - chore: expand mask dims without reshape
    - chore: remove old comments
    - feat: chore pre-commit
    - chore: add license
    - chore: remove unused network file
    - chore: duplicate whole info dict at the same time
    - feat: move decoding functions to network utils
    - feat: use get_action_head util instead of manually setting action space type
    - Merge branch develop into feat/implement-mat
    - feat: infer batch size and num agents from obs rep instead of manually passing in
    - chore: comment cleanup
    - feat: continuous actions training
    - feat: pass key through trainer to prepare for continuous action spaces
    - feat: squeeze inside of network and not in system run file
    - feat: use jax.tree.map instead of deprecated jax.tree_map
    - chore: remove redundant obs being passed around
    - feat: prepare to starting using mava discrete action head
    - feat: merge in network refactor
    - feat: use tfp instead of distrax
    - feat: remove autoregressive scans
    - feat: use MAT types
    - feat: remove value norm
    - feat: remove huber loss
    - feat: add discrete MAT and training on rware

--- test/integration_test.py
+++ test/integration_test.py
@@ -36,6 +36,7 @@ ppo_systems = [
 
 sac_systems = ["sac.anakin.ff_isac", "sac.anakin.ff_masac", "sac.anakin.ff_hasac"]
 q_learning_systems = ["q_learning.anakin.rec_iql", "q_learning.anakin.rec_qmix"]
+transformer_systems = ["mat.anakin.mat"]
 
 discrete_envs = ["gigastep", "lbf", "matrax", "rware", "smax"]
 cnn_envs = ["cleaner", "connector"]
@@ -94,6 +95,19 @@ def test_sac_system(fast_config: dict, system_path: str) -> None:
     """Test all SAC systems on random envs."""
     _, _, system_name = system_path.split(".")
     env = random.choice(continuous_envs)
+
+    with initialize(version_base=None, config_path=config_path):
+        cfg = compose(config_name=f"{system_name}", overrides=[f"env={env}"])
+        cfg = _get_fast_config(cfg, fast_config)
+
+    _run_system(system_path, cfg)
+
+
+@pytest.mark.parametrize("system_path", transformer_systems)
+def test_transformer_system(fast_config: dict, system_path: str) -> None:
+    """Test transformer systems on random envs."""
+    _, _, system_name = system_path.split(".")
+    env = random.choice(continuous_envs + discrete_envs)
 
     with initialize(version_base=None, config_path=config_path):
         cfg = compose(config_name=f"{system_name}", overrides=[f"env={env}"])
