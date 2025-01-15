8430987f392d76b557c4d2def53ab0ea429daff8
SimonDuToit <fratinokek@gmail.com>
2025-01-15 10:44:26 +0200

    lbf scenario

--- mava/__init__.py
+++ mava/__init__.py
@@ -12,3 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 __version__ = "0.2.0"
+
+from gymnasium import register
+
+
+register(
+    id="Foraging-2s-15x15-4p-5f-coop",
+    entry_point="lbforaging.foraging:ForagingEnv",
+    kwargs={
+        "players": 4,
+        "max_player_level": 2,
+        "field_size": (15, 15),
+        "max_food": 5,
+        "sight": 2,
+        "max_episode_steps": 100,
+        "force_coop": True,
+    },
+)
