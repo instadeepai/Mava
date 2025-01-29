06d71a7c1571dcb2eef83ab7d8a4047bcd36b84f
Simon Du Toit <90381208+SimonDuToit@users.noreply.github.com>
2025-01-29 12:41:36 +0200

    Nitpick
    Co-authored-by: Sasha Abramowitz <reallysasha@gmail.com>

--- mava/utils/sebulba.py
+++ mava/utils/sebulba.py
@@ -45,9 +45,7 @@ class ThreadLifetime:
 
 
 @jax.jit
-def _stack_trajectory(
-    trajectory: List[MavaTransition],
-) -> MavaTransition:
+def _stack_trajectory(trajectory: List[MavaTransition]) -> MavaTransition:
     """Stack a list of parallel_env transitions into a single
     transition of shape [rollout_len, num_envs, ...]."""
     return tree.map(lambda *x: jnp.stack(x, axis=0).swapaxes(0, 1), *trajectory)  # type: ignore
