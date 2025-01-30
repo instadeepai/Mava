406ece5af378524e9c8a73e053b3e7b64c2281dd
DriesSmit <dries.epos@gmail.com>
2025-01-30 09:48:19 +0200

    feat: test assistant implementation

--- mava/networks/sable_network.py
+++ mava/networks/sable_network.py
@@ -86,8 +86,8 @@ class Encoder(nn.Module):
     memory_config: DictConfig
     n_agents: int
     num_atoms: int = 101
-    v_min: float = 0.0
-    v_max: float = 50.0
+    v_min: float = -20.0
+    v_max: float = 20.0
 
     def setup(self) -> None:
         self.ln = nn.RMSNorm()
@@ -102,24 +102,12 @@ class Encoder(nn.Module):
             ],
         )
         self.head = nn.Sequential(
-            [
-                nn.Dense(self.net_config.embed_dim, kernel_init=orthogonal(jnp.sqrt(2))),
-                nn.gelu,
-                nn.RMSNorm(),
-                nn.Dense(self.num_atoms, kernel_init=orthogonal(0.01)),
-            ],
+            nn.Dense(self.net_config.embed_dim, kernel_init=orthogonal(jnp.sqrt(2))),
+            nn.relu,
+            nn.RMSNorm(),
+            nn.Dense(self.num_atoms, kernel_init=orthogonal(0.01)),
         )
         self.support = jnp.linspace(self.v_min, self.v_max, self.num_atoms)
-
-        # Delete this: Old head
-        # self.old_head = nn.Sequential(
-        #     [
-        #         nn.Dense(self.net_config.embed_dim, kernel_init=orthogonal(jnp.sqrt(2))),
-        #         nn.gelu,
-        #         nn.RMSNorm(),
-        #         nn.Dense(1, kernel_init=orthogonal(0.01)),
-        #     ],
-        # )
 
         self.blocks = [
             EncodeBlock(
