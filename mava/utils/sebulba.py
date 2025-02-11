f58e4c5d3d786d1bc22a824fadcb9367231ce863
SimonDuToit <fratinokek@gmail.com>
2025-02-11 16:04:21 +0200

    the real fix

--- mava/utils/sebulba.py
+++ mava/utils/sebulba.py
@@ -95,9 +95,8 @@ class Pipeline(threading.Thread):
     def put(
         self,
         traj: Sequence[MavaTransition],
-        timestep: TimeStep,
         metrics: Tuple[Dict, List[Dict]],
-        hstates: HiddenStates = None
+        final_timestep: Tuple[TimeStep, HiddenStates],
     ) -> None:
         """Put a trajectory on the queue to be consumed by the learner."""
         start_condition, end_condition = (threading.Condition(), threading.Condition())
@@ -105,6 +104,8 @@ class Pipeline(threading.Thread):
             self.tickets_queue.put((start_condition, end_condition))
             start_condition.wait()  # wait to be allowed to start
 
+        timestep, hstates = final_timestep
+        
         # [Transition(num_envs)] * rollout_len -> Transition[done=(num_envs, rollout_len, ...)]
         traj = _stack_trajectory(traj)
         traj, timestep = jax.device_put((traj, timestep), device=self.sharding)
@@ -122,7 +123,7 @@ class Pipeline(threading.Thread):
         # We use a try-finally so the lock is released even if an exception is raised.
         try:
             self._queue.put(
-                (traj, timestep, time_dict, episode_metrics, hstates),
+                (traj, time_dict, episode_metrics, (timestep, hstates)),
                 block=True,
                 timeout=QUEUE_PUT_TIMEOUT,
             )
@@ -141,7 +142,7 @@ class Pipeline(threading.Thread):
 
     def get(
         self, block: bool = True, timeout: Union[float, None] = None
-    ) -> Tuple[MavaTransition, TimeStep, Dict, Metrics]:
+    ) -> Tuple[MavaTransition, Dict, Metrics, Tuple[TimeStep, HiddenStates]]:
         """Get a trajectory from the pipeline."""
         return self._queue.get(block, timeout)  # type: ignore
 
