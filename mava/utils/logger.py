50524db0dcbb522ad618ea55096acb5d0627e72d
Ruan de Kock <ruanjdekock@gmail.com>
2025-04-02 15:54:13 +0200

    feat: always set gorup tags

--- mava/utils/logger.py
+++ mava/utils/logger.py
@@ -160,8 +160,7 @@ class NeptuneLogger(BaseLogger):
         self.logger = neptune.init_run(project=project, tags=tags, mode=mode)
 
         self.logger["config"] = stringify_unsupported(cfg)
-        if cfg.logger.kwargs.neptune_group_tag:
-            self.logger["sys/group_tags"].add(list(cfg.logger.kwargs.neptune_group_tag))
+        self.logger["sys/group_tags"].add(list(cfg.logger.kwargs.neptune_group_tag))
 
         self.detailed_logging = cfg.logger.kwargs.detailed_neptune_logging
 
