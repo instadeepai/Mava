c1f4f10b77ee5388eac55d847c9d6477a0a8e766
Ruan de Kock <ruanjdekock@gmail.com>
2025-04-02 15:03:36 +0200

    feat: set different system name

--- mava/systems/sable/anakin/rec_sable.py
+++ mava/systems/sable/anakin/rec_sable.py
@@ -527,7 +527,7 @@ def learner_setup(
 
 def run_experiment(_config: DictConfig) -> float:
     """Runs experiment."""
-    _config.logger.system_name = "rec_sable"
+    _config.logger.system_name = "rec_sable_numer_stable"
     config = copy.deepcopy(_config)
 
     n_devices = len(jax.devices())
