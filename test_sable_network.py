1767c818dcb517bfc12267734a3788f46e07aa37
Ruan de Kock <ruanjdekock@gmail.com>
2025-04-16 17:48:01 +0200

    feat: add jit to sable network test

--- test_sable_network.py
+++ test_sable_network.py
@@ -27,6 +27,7 @@
 # limitations under the License
 
 import copy
+from functools import partial
 
 import jax
 
@@ -35,11 +36,11 @@ import jax.numpy as jnp
 from omegaconf import DictConfig
 
 from mava.networks.sable_network import SableNetwork
-from mava.systems.sable.types import HiddenStates, Scales
+from mava.systems.sable.types import HiddenStates
 from mava.types import Observation
 
 base_seed = 2
-num_time_steps = 128
+num_time_steps = 512
 bsz = 16
 num_agents = 4
 obs_dim = 11
@@ -49,7 +50,7 @@ seq_len = num_agents * num_time_steps
 retnet_embed_dim = 32
 retnet_num_heads = 2
 retnet_num_blocks = 2
-num_chunks = 1
+num_chunks = 8
 
 act_dim = 5
 
@@ -102,8 +103,6 @@ step_counts = jnp.arange(num_time_steps)
 step_counts = step_counts[None, ...].repeat(bsz, axis=0)[..., None].repeat(num_agents, axis=-1)
 step_counts = step_counts.reshape(bsz, seq_len)
 
-init_scale = jnp.ones((bsz, retnet_num_heads, retnet_num_blocks, 1, 1))
-
 key, init_key = jax.random.split(key)
 
 observation = Observation(
@@ -116,40 +115,40 @@ init_hstates = HiddenStates(
     decoder_self_retn=copy.deepcopy(init_hstate),
     decoder_cross_retn=copy.deepcopy(init_hstate),
 )
-init_scales = Scales(
-    encoder=copy.deepcopy(init_scale),
-    decoder_self_retn=copy.deepcopy(init_scale),
-    decoder_cross_retn=copy.deepcopy(init_scale),
-)
 
 params = network.init(
     init_key,
     observation=jax.tree.map(lambda x: x[0:1, 0:num_agents, ...], observation),
     hstates=jax.tree.map(lambda x: x[0:1, ...], init_hstates),
-    scales=jax.tree.map(lambda x: x[0:1, ...], init_scales),
     key=init_key,
     method="get_actions",
 )
+
+JIT_FUNCTIONS = True
+
+if JIT_FUNCTIONS:
+    inf_apply = jax.jit(partial(network.apply, method="get_actions"))
+    train_apply = jax.jit(network.apply)
+else:
+    inf_apply = partial(network.apply, method="get_actions")
+    train_apply = network.apply
 
 inference_actions = []
 inference_log_probs = []
 inference_values = []
 
 hstates = copy.deepcopy(init_hstates)
-scales = copy.deepcopy(init_scales)
 
 for step in range(num_time_steps):
     key, step_key = jax.random.split(key)
     obs_i = jax.tree.map(
         lambda x, step=step: x[:, step * num_agents : (step + 1) * num_agents, ...], observation
     )
-    action_i, log_prob_i, value_i, hstates, scales = network.apply(
+    action_i, log_prob_i, value_i, hstates = inf_apply(
         params,
         obs_i,
         hstates,
-        scales,
         step_key,
-        method="get_actions",
     )
     inference_actions.append(action_i)
     inference_log_probs.append(log_prob_i)
@@ -159,26 +158,27 @@ inference_actions = jnp.concatenate(inference_actions, axis=1)
 inference_log_probs = jnp.concatenate(inference_log_probs, axis=1)
 inference_values = jnp.concatenate(inference_values, axis=1)
 
-print(f"Inference actions: {inference_actions.shape}")
-print(f"Inference log probs: {inference_log_probs.shape}")
-print(f"Inference values: {inference_values.shape}")
+print(f"Inference actions shape: {inference_actions.shape}")
+print(f"Inference log probs shape: {inference_log_probs.shape}")
+print(f"Inference values shape: {inference_values.shape}")
 
 hstates = copy.deepcopy(init_hstates)
-scales = copy.deepcopy(init_scales)
 
-train_value, train_log_prob, train_entropy = network.apply(
+train_value, train_log_prob, train_entropy = train_apply(
     params,
     observation,
     inference_actions,
     hstates,
-    scales,
     dones,
     key,
 )
 
-print(f"Train value: {train_value.shape}")
-print(f"Train log prob: {train_log_prob.shape}")
-print(f"Train entropy: {train_entropy.shape}")
+print(f"Train value shape: {train_value.shape}")
+print(f"Train log prob shape: {train_log_prob.shape}")
+print(f"Train entropy shape: {train_entropy.shape}")
 
-print(f"Value error: {jnp.mean(jnp.abs(train_value - inference_values))}")
-print(f"Log prob error: {jnp.mean(jnp.abs(train_log_prob - inference_log_probs))}")
+print()
+print(f"Value mean absolute error: {jnp.mean(jnp.abs(train_value - inference_values))}")
+print(f"Value max absolute error: {jnp.max(jnp.abs(train_value - inference_values))}")
+print(f"Log prob mean absolute error: {jnp.mean(jnp.abs(train_log_prob - inference_log_probs))}")
+print(f"Log prob max absolute error: {jnp.max(jnp.abs(train_log_prob - inference_log_probs))}")
