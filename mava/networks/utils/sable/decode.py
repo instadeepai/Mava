230f736444d36af2feab63f5feac481c88a629ec
liyheng <86578139+liyheng@users.noreply.github.com>
2025-08-27 16:13:52 +0800

    Update decode.py

--- mava/networks/utils/sable/decode.py
+++ mava/networks/utils/sable/decode.py
@@ -44,6 +44,7 @@ def discrete_train_decoder_fn(
     n_agents: int,
     chunk_size: int,
     rng_key: Optional[chex.PRNGKey] = None,
+    return_dist: bool = False,
 ) -> Tuple[chex.Array, chex.Array]:
     """Parallel action sampling for discrete action spaces."""
     # Delete `rng_key` since it is not used in discrete action space
@@ -79,8 +80,10 @@ def discrete_train_decoder_fn(
 
     distribution = distrax.Categorical(logits=masked_logits)
     action_log_prob = distribution.log_prob(action)
-
-    return action_log_prob, distribution.entropy()
+    if return_dist:
+        return action_log_prob, distribution.entropy(), distribution
+    else:
+        return action_log_prob, distribution.entropy()
 
 
 def get_shifted_discrete_actions(
@@ -165,6 +168,7 @@ def continuous_train_decoder_fn(
     chunk_size: int,
     action_dim: int,
     rng_key: Optional[chex.PRNGKey] = None,
+    return_dist: bool = False,
 ) -> Tuple[chex.Array, chex.Array]:
     """Parallel action sampling for discrete action spaces."""
     # Delete `legal_actions` since it is not used in continuous action space
@@ -204,8 +208,10 @@ def continuous_train_decoder_fn(
     action_log_prob = distribution.log_prob(action)
     entropy = distribution.entropy(seed=rng_key)
 
-    return action_log_prob, entropy
-
+    if return_dist:
+        return action_log_prob, entropy, distribution
+    else:
+        return action_log_prob, entropy
 
 def get_shifted_continuous_actions(
     action: chex.Array, action_dim: int, n_agents: int
