9a75b9a4c9dcabed356321a2158a6d6ac0e00589
liyheng <86578139+liyheng@users.noreply.github.com>
2025-09-01 08:14:43 +0800

    Update rec_sable.py

--- mava/systems/sable/sebulba/rec_sable.py
+++ mava/systems/sable/sebulba/rec_sable.py
@@ -251,7 +251,7 @@ def get_learner_step_fn(
                 ) -> Tuple:
                     """Calculate Sable loss."""
                     # Rerun network
-                    value, log_prob, entropy = sable_apply_fn(  # type: ignore
+                    value, log_prob, entropy, _ = sable_apply_fn(  # type: ignore
                         params,
                         traj_batch.obs,
                         traj_batch.action,
