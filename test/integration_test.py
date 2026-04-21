f1acd356a6af4cfc3d55b700244f76a5c88b9c63
Sasha Abramowitz <reallysasha@gmail.com>
2026-04-21 12:53:37 +0200

    feat: add mlflow and remove neptune

--- test/integration_test.py
+++ test/integration_test.py
@@ -48,9 +48,9 @@ def _run_system(system_name: str, cfg: DictConfig) -> float:
     """Runs a system."""
     OmegaConf.set_struct(cfg, False)
     # we never want to log these tests anywhere
-    cfg.logger.use_neptune = False
-    cfg.logger.use_tb = False
-    cfg.logger.use_json = False
+    cfg.logger.loggers.mlflow.enabled = False
+    cfg.logger.loggers.tensorboard.enabled = False
+    cfg.logger.loggers.json.enabled = False
 
     system = importlib.import_module(f"mava.systems.{system_name}")
     eval_perf = system.run_experiment(cfg)
