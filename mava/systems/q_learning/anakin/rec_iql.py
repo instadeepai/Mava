c6893be5d674aabb95b614af4adf7a3a691f7dd4
quinnarnold <qarnold005@gmail.com>
2026-08-26 04:58:00 -0400

    fix: report Anakin learner throughput separately (#1212)
    * fix: include learner updates in Anakin throughput
    
    * fix: log learner update throughput separately

--- mava/systems/q_learning/anakin/rec_iql.py
+++ mava/systems/q_learning/anakin/rec_iql.py
@@ -504,6 +504,8 @@ def run_experiment(cfg: DictConfig) -> float:
     anakin_act_steps = anakin_steps * cfg.arch.num_envs * cfg.system.rollout_length
     # Number of steps to do in the scanned update method (how many anakin steps).
     cfg.system.scan_steps = int(steps_per_rollout / anakin_act_steps)
+    # Number of gradient steps performed between evaluations.
+    learn_steps_per_rollout = anakin_steps * cfg.system.epochs * cfg.system.scan_steps
 
     # Initialise system and make learning/evaluation functions
     (env, eval_env), q_net, opt, rb, learner_state, logger, key = init(cfg)
@@ -558,16 +560,15 @@ def run_experiment(cfg: DictConfig) -> float:
         jax.block_until_ready(learner_state)
 
         # Log:
-        # Add learn steps here because anakin steps per second is learn + act steps
-        # But we also want to make sure we're counting env steps correctly so
-        # learn steps is not included in the loop counter.
         elapsed_time = time.time() - start_time
         eps = jnp.maximum(
             cfg.system.eps_min, 1 - (t / cfg.system.eps_decay) * (1 - cfg.system.eps_min)
         )
         final_metrics, ep_completed = episode_metrics.get_final_step_metrics(metrics)
         final_metrics["steps_per_second"] = steps_per_rollout / elapsed_time
-        loss_metrics = losses
+        loss_metrics = losses | {
+            "learner_updates_per_second": learn_steps_per_rollout / elapsed_time
+        }
 
         logger.log({"timestep": t, "epsilon": eps}, t, eval_idx, LogEvent.MISC)
         if ep_completed:
